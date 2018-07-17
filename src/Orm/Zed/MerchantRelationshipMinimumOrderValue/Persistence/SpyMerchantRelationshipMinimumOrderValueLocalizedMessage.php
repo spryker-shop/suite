763d658e6bbae7b832cb3661373ba5197329d71d
Artem Kuznetsov <wizardphp@gmail.com>
2018-07-17 08:56:43 +0000

    ps-1118

---
+++ src/Orm/Zed/MerchantRelationshipMinimumOrderValue/Persistence/SpyMerchantRelationshipMinimumOrderValueLocalizedMessage.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\MerchantRelationshipMinimumOrderValue\Persistence;
+
+use Orm\Zed\MerchantRelationshipMinimumOrderValue\Persistence\Base\SpyMerchantRelationshipMinimumOrderValueLocalizedMessage as BaseSpyMerchantRelationshipMinimumOrderValueLocalizedMessage;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_merchant_relationship_min_order_value_localized_message' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyMerchantRelationshipMinimumOrderValueLocalizedMessage extends BaseSpyMerchantRelationshipMinimumOrderValueLocalizedMessage
+{
+}
