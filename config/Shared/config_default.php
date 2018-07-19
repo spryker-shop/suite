48c9802b9f673171512773b8d96cec0c1d7e2d07
Evhen Odokienko <evhen.odokienko@spryker.com>
2018-07-19 18:47:51 +0200

    Zed API logging of sensitive data included in ApiRequestTransfer
    PS-2557 [support #208] Zed API: (potential) logging of sensitive data
    included in ApiRequestTransfer

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -430,5 +430,5 @@ $config[OauthConstants::PUBLIC_KEY_PATH] = 'file://';
 $config[OauthConstants::ENCRYPTION_KEY] = '';
 
 // ----------- Api
-$config[\Spryker\Shared\Api\ApiConstants::SERVER_VARIABLE_FILTER_STRATEGY] = \Spryker\Shared\Api\ApiConstants::SERVER_VARIABLE_FILTER_STRATEGY_WHITELIST;
-$config[\Spryker\Shared\Api\ApiConstants::SERVER_VARIABLE_WHITELIST] = ['REQUEST_URI'];
+$config[\Spryker\Shared\Api\ApiConstants::API_ENV_SERVER_VARIABLE_FILTER_STRATEGY] = \Spryker\Shared\Api\ApiConstants::API_ENV_SERVER_VARIABLE_FILTER_STRATEGY_WHITELIST;
+$config[\Spryker\Shared\Api\ApiConstants::API_ENV_SERVER_VARIABLE_WHITELIST] = ['REQUEST_URI'];
