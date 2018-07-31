09ed640c3d4b7b706bce8de0aea4c3093d6fa0c0
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-07-31 12:33:03 +0300

    PS-2890: Created CompanyUserStatusChangePermissionPlugin and added it to CompanyRoleConfig::getAdminRolePermissions()

---
+++ src/Pyz/Zed/CompanyRole/CompanyRoleConfig.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\CompanyRole;
+
+use Spryker\Zed\CompanyRole\CompanyRoleConfig as SprykerCompanyRoleConfig;
+use SprykerShop\Shared\CompanyPage\Plugin\CompanyUserStatusChangePermissionPlugin;
+
+class CompanyRoleConfig extends SprykerCompanyRoleConfig
+{
+    /**
+     * @return \Spryker\Shared\PermissionExtension\Dependency\Plugin\PermissionPluginInterface[]
+     */
+    public function getAdminRolePermissions(): string
+    {
+        return [
+            new CompanyUserStatusChangePermissionPlugin(),
+        ];
+    }
+}
