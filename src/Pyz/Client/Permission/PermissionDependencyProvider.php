b98723a89e0c9c19c6658c8c2dc39985da4f1285
Ruslan Ivanov <ruslan.ivanov@spryker.com>
2018-08-03 19:42:06 +0300

    PS-2604: added new keys to glossary, added new plugins of permission to PermissionDependencyProvider

--- src/Pyz/Client/Permission/PermissionDependencyProvider.php
+++ src/Pyz/Client/Permission/PermissionDependencyProvider.php
@@ -8,12 +8,17 @@
 namespace Pyz\Client\Permission;
 
 use Spryker\Client\CompanyRole\Plugin\PermissionStoragePlugin;
+use Spryker\Client\CompanyUser\Plugin\AddCompanyUserPermissionPlugin;
 use Spryker\Client\CompanyUserInvitation\Plugin\ManageCompanyUserInvitationPermissionPlugin;
 use Spryker\Client\Permission\PermissionDependencyProvider as SprykerPermissionDependencyProvider;
 use Spryker\Client\SharedCart\Plugin\ReadSharedCartPermissionPlugin;
 use Spryker\Client\SharedCart\Plugin\WriteSharedCartPermissionPlugin;
 use Spryker\Client\ShoppingList\Plugin\WriteShoppingListPermissionPlugin;
 use Spryker\Zed\ShoppingList\Communication\Plugin\ReadShoppingListPermissionPlugin;
+use SprykerShop\Client\CheckoutPage\Plugin\PlaceOrderWithAmountUpToPermissionPlugin;
+use SprykerShop\Shared\CartPage\Plugin\AddCartItemPermissionPlugin;
+use SprykerShop\Shared\CartPage\Plugin\ChangeCartItemPermissionPlugin;
+use SprykerShop\Shared\CartPage\Plugin\RemoveCartItemPermissionPlugin;
 
 class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
 {
@@ -38,6 +43,11 @@ class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
             new ReadShoppingListPermissionPlugin(), #ShoppingListFeature
             new WriteShoppingListPermissionPlugin(), #ShoppingListFeature
             new ManageCompanyUserInvitationPermissionPlugin(), #CompanyUserInvitationFeature
+            new AddCompanyUserPermissionPlugin(), #ManagingUsersFeature
+            new AddCartItemPermissionPlugin(), #ManagingUsersFeature
+            new ChangeCartItemPermissionPlugin(), #ManagingUsersFeature
+            new RemoveCartItemPermissionPlugin(), #ManagingUsersFeature
+            new PlaceOrderWithAmountUpToPermissionPlugin(), #ManagingUsersFeature
         ];
     }
 }
