63e2ad0e38c11a97ae000cef3cd621e4e67ea9e3
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-08-07 16:32:56 +0200

    PS-1118: Add MinimumOrderValueCartPostSavePlugin and MinimumOrderValueCheckoutPreConditionPlugin

--- src/Pyz/Zed/Checkout/CheckoutDependencyProvider.php
+++ src/Pyz/Zed/Checkout/CheckoutDependencyProvider.php
@@ -14,6 +14,7 @@ use Spryker\Zed\Customer\Communication\Plugin\Checkout\CustomerOrderSavePlugin;
 use Spryker\Zed\Customer\Communication\Plugin\CustomerPreConditionCheckerPlugin;
 use Spryker\Zed\Discount\Communication\Plugin\Checkout\DiscountOrderSavePlugin;
 use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\MinimumOrderValue\Communication\Plugin\Checkout\MinimumOrderValueCheckoutPreConditionPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentOrderSaverPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentPostCheckPlugin;
 use Spryker\Zed\Payment\Communication\Plugin\Checkout\PaymentPreCheckPlugin;
@@ -46,6 +47,7 @@ class CheckoutDependencyProvider extends SprykerCheckoutDependencyProvider
             new ShipmentCheckoutPreCheckPlugin(),
             new ProductDiscontinuedCheckoutPreConditionPlugin(), #ProductDiscontinuedFeature
             new AmountAvailabilityCheckoutPreConditionPlugin(),
+            new MinimumOrderValueCheckoutPreConditionPlugin(),
         ];
     }
 
