c5827f0e1120ba155e6b110be3b8adc54ebb94f2
Vitaliy Kirichenko <vitaly.kirichenko@spryker.com>
2018-08-08 15:24:25 +0300

    PS-2604: Disabled all new permissions by default

--- src/Pyz/Client/Permission/PermissionDependencyProvider.php
+++ src/Pyz/Client/Permission/PermissionDependencyProvider.php
@@ -8,17 +8,11 @@
 namespace Pyz\Client\Permission;
 
 use Spryker\Client\CompanyRole\Plugin\PermissionStoragePlugin;
-use Spryker\Client\CompanyUser\Plugin\AddCompanyUserPermissionPlugin;
-use Spryker\Client\CompanyUserInvitation\Plugin\ManageCompanyUserInvitationPermissionPlugin;
 use Spryker\Client\Permission\PermissionDependencyProvider as SprykerPermissionDependencyProvider;
 use Spryker\Client\SharedCart\Plugin\ReadSharedCartPermissionPlugin;
 use Spryker\Client\SharedCart\Plugin\WriteSharedCartPermissionPlugin;
 use Spryker\Client\ShoppingList\Plugin\WriteShoppingListPermissionPlugin;
 use Spryker\Zed\ShoppingList\Communication\Plugin\ReadShoppingListPermissionPlugin;
-use SprykerShop\Client\CheckoutPage\Plugin\PlaceOrderWithAmountUpToPermissionPlugin;
-use SprykerShop\Shared\CartPage\Plugin\AddCartItemPermissionPlugin;
-use SprykerShop\Shared\CartPage\Plugin\ChangeCartItemPermissionPlugin;
-use SprykerShop\Shared\CartPage\Plugin\RemoveCartItemPermissionPlugin;
 
 class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
 {
@@ -42,12 +36,6 @@ class PermissionDependencyProvider extends SprykerPermissionDependencyProvider
             new WriteSharedCartPermissionPlugin(), #SharedCartFeature
             new ReadShoppingListPermissionPlugin(), #ShoppingListFeature
             new WriteShoppingListPermissionPlugin(), #ShoppingListFeature
-            new ManageCompanyUserInvitationPermissionPlugin(), #CompanyUserInvitationFeature
-            new AddCompanyUserPermissionPlugin(), #CompanyUserFeature
-            new AddCartItemPermissionPlugin(), #CartFeature
-            new ChangeCartItemPermissionPlugin(), #CartFeature
-            new RemoveCartItemPermissionPlugin(), #CartFeature
-            new PlaceOrderWithAmountUpToPermissionPlugin(), #CheckoutFeature
         ];
     }
 }
