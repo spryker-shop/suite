3877791c349ce28a9e9291f7ed777367a8d63f4f
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-08-10 09:51:25 +0300

    PS-3277: Refactoring

--- src/Orm/Zed/ShoppingListNote/Persistence/SpyShoppingListItemNoteQuery.php
+++ src/Orm/Zed/ShoppingListNote/Persistence/SpyShoppingListItemNoteQuery.php
@@ -1,8 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\ShoppingListNote\Persistence;
 
-use Orm\Zed\ShoppingListNote\Persistence\Base\SpyShoppingListItemNoteQuery as BaseSpyShoppingListItemNoteQuery;
+use Spryker\Zed\ShoppingListNote\Persistence\Propel\AbstractSpyShoppingListItemNoteQuery as BaseSpyShoppingListItemNoteQuery;
 
 /**
  * Skeleton subclass for performing query and update operations on the 'spy_shopping_list_item_note' table.
@@ -10,11 +15,10 @@ use Orm\Zed\ShoppingListNote\Persistence\Base\SpyShoppingListItemNoteQuery as Ba
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyShoppingListItemNoteQuery extends BaseSpyShoppingListItemNoteQuery
 {
-
 }
