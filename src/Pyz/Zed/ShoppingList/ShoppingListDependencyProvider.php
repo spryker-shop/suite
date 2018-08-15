7c6b4d8feddaedafca47feed8624f1ba906856ba
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2018-08-15 16:15:26 +0300

    PS-3098: add comment

--- src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
+++ src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
@@ -40,7 +40,7 @@ class ShoppingListDependencyProvider extends SprykerShoppingListDependencyProvid
     protected function getItemExpanderPlugins(): array
     {
         return [
-            new ShoppingListItemNoteExpanderPlugin(),
+            new ShoppingListItemNoteExpanderPlugin(), #ShoppingListNoteFeature
         ];
     }
 }
