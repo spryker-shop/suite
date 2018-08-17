e74371daab206a15360626a196d5f1181a396e36
Mahmoud Ali <mahmoud.ali@spryker.com>
2018-08-17 19:40:04 +0200

    PS-1118 Move the installer plugin into Installer namespace

--- src/Pyz/Zed/Installer/InstallerDependencyProvider.php
+++ src/Pyz/Zed/Installer/InstallerDependencyProvider.php
@@ -13,7 +13,7 @@ use Spryker\Zed\Country\Communication\Plugin\CountryInstallerPlugin;
 use Spryker\Zed\Glossary\Communication\Plugin\GlossaryInstallerPlugin;
 use Spryker\Zed\Installer\InstallerDependencyProvider as SprykerInstallerDependencyProvider;
 use Spryker\Zed\Locale\Communication\Plugin\LocaleInstallerPlugin;
-use Spryker\Zed\MinimumOrderValue\Communication\Plugin\MinimumOrderValueTypeInstallerPlugin;
+use Spryker\Zed\MinimumOrderValue\Communication\Plugin\Installer\MinimumOrderValueTypeInstallerPlugin;
 use Spryker\Zed\Newsletter\Communication\Plugin\NewsletterInstallerPlugin;
 use Spryker\Zed\OauthCustomerConnector\Communication\Plugin\Installer\OauthCustomerInstallerPlugin;
 use Spryker\Zed\PriceProduct\Communication\Plugin\PriceInstallerPlugin;
