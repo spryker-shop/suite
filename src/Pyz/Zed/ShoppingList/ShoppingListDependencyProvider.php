7fd5c4428243c382541d3eea1511c6f0478a45d4
laktionova <helen.laktionova@spryker.com>
2018-08-20 13:55:08 +0300

    PS-3822: As a customer I want to see products with product options on my shopping list view page
    * Implemented widget;
    * Added 'ShoppingListItem' expander;

--- src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
+++ src/Pyz/Zed/ShoppingList/ShoppingListDependencyProvider.php
@@ -10,6 +10,7 @@ namespace Pyz\Zed\ShoppingList;
 use Spryker\Zed\ProductBundle\Communication\Plugin\ShoppingList\ReplaceBundledQuoteItemsPreConvertPlugin;
 use Spryker\Zed\ProductDiscontinued\Communication\Plugin\ShoppingList\ProductDiscontinuedAddItemPreCheckPlugin;
 use Spryker\Zed\ShoppingList\ShoppingListDependencyProvider as SprykerShoppingListDependencyProvider;
+use Spryker\Zed\ShoppingListProductOption\Communication\Plugin\ShoppingList\ShoppingListItemProductOptionExpanderPlugin;
 
 class ShoppingListDependencyProvider extends SprykerShoppingListDependencyProvider
 {
@@ -30,6 +31,16 @@ class ShoppingListDependencyProvider extends SprykerShoppingListDependencyProvid
     {
         return [
             new ReplaceBundledQuoteItemsPreConvertPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\ShoppingListExtension\Dependency\Plugin\ItemExpanderPluginInterface[]
+     */
+    protected function getItemExpanderPlugins(): array
+    {
+        return [
+            new ShoppingListItemProductOptionExpanderPlugin(),
         ];
     }
 }
