cb5b9abc553e8fb1909370297ba8b8dc27b63c6f
Ruslan Ivanov <ruslan.ivanov@spryker.com>
2018-09-14 10:51:37 +0300

    PS-3672: refactored code after AA review

--- src/Pyz/Client/MultiCart/MultiCartConfig.php
+++ src/Pyz/Client/MultiCart/MultiCartConfig.php
@@ -15,9 +15,9 @@ class MultiCartConfig extends SprykerMultiCartConfig
     /**
      * @return string[]
      */
-    public function getQuoteFieldsAllowedForDuplicateQuote(): array
+    public function getQuoteFieldsAllowedForQuoteDuplicate(): array
     {
-        return array_merge(parent::getQuoteFieldsAllowedForDuplicateQuote(), [
+        return array_merge(parent::getQuoteFieldsAllowedForQuoteDuplicate(), [
             QuoteTransfer::BUNDLE_ITEMS,
             QuoteTransfer::CART_NOTE, #CartNoteFeature
         ]);
