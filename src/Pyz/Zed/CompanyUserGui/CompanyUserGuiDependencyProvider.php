58f08060ddf35f97c411dc2a9e2bbd2c7136a8c7
Dima Aseev <dmitriy.aseev@spryker.com>
2018-09-28 13:50:20 +0300

    PS-3930: Fixed travis issue.

--- src/Pyz/Zed/CompanyUserGui/CompanyUserGuiDependencyProvider.php
+++ src/Pyz/Zed/CompanyUserGui/CompanyUserGuiDependencyProvider.php
@@ -7,12 +7,12 @@
 
 namespace Pyz\Zed\CompanyUserGui;
 
-use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserBusinessUnitFieldPlugin;
-use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserRoleFieldPlugin;
 use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyBusinessUnitCompanyUserTableConfigExpanderPlugin;
 use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyBusinessUnitCompanyUserTablePrepareDataExpanderPlugin;
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\CompanyUserBusinessUnitFieldPlugin;
 use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyRoleCompanyUserTableConfigExpanderPlugin;
 use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyRoleCompanyUserTablePrepareDataExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\CompanyUserRoleFieldPlugin;
 use Spryker\Zed\CompanyUserGui\CompanyUserGuiDependencyProvider as SprykerCompanyUserGuiDependencyProvider;
 
 class CompanyUserGuiDependencyProvider extends SprykerCompanyUserGuiDependencyProvider
