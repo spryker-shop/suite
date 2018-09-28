6f8b1306004cafdeee75c980d6fef173f50e9eac
AntonKhabiuk <brotherosirus@gmail.com>
2018-09-28 13:14:10 +0300

    GLUE-909 AA review fix

--- src/Pyz/Zed/RestApiDocumentationGenerator/RestApiDocumentationGeneratorConfig.php
+++ src/Pyz/Zed/RestApiDocumentationGenerator/RestApiDocumentationGeneratorConfig.php
@@ -7,31 +7,22 @@
 
 namespace Pyz\Zed\RestApiDocumentationGenerator;
 
-use Spryker\Shared\Kernel\KernelConstants;
 use Spryker\Zed\RestApiDocumentationGenerator\RestApiDocumentationGeneratorConfig as SprykerRestApiDocumentationGeneratorConfig;
 
 class RestApiDocumentationGeneratorConfig extends SprykerRestApiDocumentationGeneratorConfig
 {
-    protected const APPLICATION_SOURCE_ANNOTATIONS_SOURCE_DIRECTORY_PATTERN = '/Glue/%1$s/Controller/';
     protected const APPLICATION_VENDOR_ANNOTATIONS_SOURCE_DIRECTORY_PATTERN = '/spryker/spryker/Bundles/%1$s/src/Spryker/Glue/%1$s/Controller/';
 
     /**
+     * @project Only needed internal non-split project, not in public split project.
+     *
      * @return array
      */
     protected function getCoreAnnotationsSourceDirectoryPatterns(): array
     {
-        return [
-            APPLICATION_VENDOR_DIR . static::APPLICATION_VENDOR_ANNOTATIONS_SOURCE_DIRECTORY_PATTERN,
-        ];
-    }
+        $directoryPatterns = parent::getCoreAnnotationsSourceDirectoryPatterns();
+        $directoryPatterns[] = APPLICATION_VENDOR_DIR . static::APPLICATION_VENDOR_ANNOTATIONS_SOURCE_DIRECTORY_PATTERN;
 
-    /**
-     * @return array
-     */
-    protected function getApplicationAnnotationsSourceDirectoryPattern(): array
-    {
-        return [
-            APPLICATION_SOURCE_DIR . '/' . $this->get(KernelConstants::PROJECT_NAMESPACE) . static::APPLICATION_SOURCE_ANNOTATIONS_SOURCE_DIRECTORY_PATTERN,
-        ];
+        return $directoryPatterns;
     }
 }
