20fcf2b1a9620e0ac4e47158d4c4a692cbab667e
AntonKhabiuk <brotherosirus@gmail.com>
2018-10-04 10:57:37 +0300

    GLUE-1128 Fix console command naming [skip ci]

--- src/Pyz/Zed/Console/ConsoleDependencyProvider.php
+++ src/Pyz/Zed/Console/ConsoleDependencyProvider.php
@@ -79,7 +79,7 @@ use Spryker\Zed\Propel\Communication\Console\PropelSchemaValidatorConsole;
 use Spryker\Zed\Propel\Communication\Plugin\ServiceProvider\PropelServiceProvider;
 use Spryker\Zed\Queue\Communication\Console\QueueTaskConsole;
 use Spryker\Zed\Queue\Communication\Console\QueueWorkerConsole;
-use Spryker\Zed\Quote\Communication\Console\CleanExpiredGuestCartConsoleConsole;
+use Spryker\Zed\Quote\Communication\Console\CleanExpiredGuestCartConsole;
 use Spryker\Zed\RabbitMq\Communication\Console\DeleteAllExchangesConsole;
 use Spryker\Zed\RabbitMq\Communication\Console\DeleteAllQueuesConsole;
 use Spryker\Zed\RabbitMq\Communication\Console\PurgeAllQueuesConsole;
@@ -268,7 +268,7 @@ class ConsoleDependencyProvider extends SprykerConsoleDependencyProvider
             new WishlistsUuidWriterConsole(),
 
             new ProductTaxSetsRestApiConsole(),
-            new CleanExpiredGuestCartConsoleConsole(),
+            new CleanExpiredGuestCartConsole(),
         ];
 
         $propelCommands = $container->getLocator()->propel()->facade()->getConsoleCommands();
