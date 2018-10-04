1e8fc70810e7bf1e1eb9751458225671938211ba
Andrey Tkachenko <andrey.tkachenko@spryker.com>
2018-10-04 17:34:11 +0200

    SUITE-718 fixed sniff issues

--- src/Pyz/Zed/Payment/Communication/Plugin/Payment/SkipFirstPaymentMethodFilterPlugin.php
+++ src/Pyz/Zed/Payment/Communication/Plugin/Payment/SkipFirstPaymentMethodFilterPlugin.php
@@ -1,7 +1,13 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Pyz\Zed\Payment\Communication\Plugin\Payment;
 
+use ArrayObject;
 use Generated\Shared\Transfer\PaymentMethodsTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
 use Spryker\Zed\Payment\Dependency\Plugin\Payment\PaymentMethodFilterPluginInterface;
@@ -21,7 +27,7 @@ class SkipFirstPaymentMethodFilterPlugin implements PaymentMethodFilterPluginInt
      */
     public function filterPaymentMethods(PaymentMethodsTransfer $paymentMethodsTransfer, QuoteTransfer $quoteTransfer)
     {
-        $paymentMethodsTransfer->setMethods(new \ArrayObject(array_slice($paymentMethodsTransfer->getMethods()->getArrayCopy(), 1)));
+        $paymentMethodsTransfer->setMethods(new ArrayObject(array_slice($paymentMethodsTransfer->getMethods()->getArrayCopy(), 1)));
 
         return $paymentMethodsTransfer;
     }
