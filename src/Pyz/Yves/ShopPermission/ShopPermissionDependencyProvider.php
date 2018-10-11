db2261d34bdedc1219bbcbb70a4360dfd08e3135
René Klatt <rene.klatt@spryker.com>
2018-10-11 11:53:22 +0000

    te-250 update composer

--- src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
+++ src/Pyz/Yves/ShopPermission/ShopPermissionDependencyProvider.php
@@ -8,21 +8,10 @@
 namespace Pyz\Yves\ShopPermission;
 
 use SprykerShop\Yves\ShopPermission\Plugin\Twig\PermissionTwigExtensionPlugin;
-use SprykerShop\Yves\ShopPermission\Plugin\Twig\PermissionTwigFunctionPlugin;
 use SprykerShop\Yves\ShopPermission\ShopPermissionDependencyProvider as SprykerShopPermissionDependencyProvider;
 
 class ShopPermissionDependencyProvider extends SprykerShopPermissionDependencyProvider
 {
-    /**
-     * @return \Spryker\Yves\Twig\Plugin\TwigFunctionPluginInterface[]
-     */
-    protected function getPermissionTwigFunctionPlugins()
-    {
-        return [
-            new PermissionTwigFunctionPlugin(),
-        ];
-    }
-
     /**
      * @return \Spryker\Yves\Twig\Plugin\AbstractTwigExtensionPlugin[]
      */
