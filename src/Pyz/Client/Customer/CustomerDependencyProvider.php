eeeecbc788f29e8322ceb3c175b0554c068913a7
Anton Zubariev <anton.zubariev@spryker.com>
2018-10-18 14:56:07 +0300

    [PS-5021] Method renamed

--- src/Pyz/Client/Customer/CustomerDependencyProvider.php
+++ src/Pyz/Client/Customer/CustomerDependencyProvider.php
@@ -10,7 +10,7 @@ namespace Pyz\Client\Customer;
 use Spryker\Client\Cart\Plugin\CustomerChangeCartUpdatePlugin;
 use Spryker\Client\Customer\CustomerDependencyProvider as SprykerCustomerDependencyProvider;
 use Spryker\Client\Customer\Plugin\CustomerAddressSessionUpdatePlugin;
-use Spryker\Client\Customer\Plugin\CustomerTransferSessionGetPlugin;
+use Spryker\Client\Customer\Plugin\CustomerTransferSessionRefreshPlugin;
 use Spryker\Client\MultiCart\Plugin\GuestCartSaveCustomerSessionSetPlugin;
 use Spryker\Client\PersistentCart\Plugin\GuestCartUpdateCustomerSessionSetPlugin;
 
@@ -22,7 +22,7 @@ class CustomerDependencyProvider extends SprykerCustomerDependencyProvider
     protected function getCustomerSessionGetPlugins()
     {
         return [
-            new CustomerTransferSessionGetPlugin(),
+            new CustomerTransferSessionRefreshPlugin(),
         ];
     }
 
