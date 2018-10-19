d032874e5da818fd9617abd7e6c5e14e0562632c
eugenia.poidenko <eugenia.poidenko@spryker.com>
2018-10-19 11:44:30 +0000

    GLUE-391 updated composer.lock, moved the payment methods config to project level [skip ci]

---
+++ src/Pyz/Glue/CheckoutRestApi/CheckoutRestApiConfig.php
@@ -0,0 +1,33 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Glue\CheckoutRestApi;
+
+use Spryker\Glue\CheckoutRestApi\CheckoutRestApiConfig as SprykerCheckoutRestApiConfig;
+
+class CheckoutRestApiConfig extends SprykerCheckoutRestApiConfig
+{
+    protected const PAYMENT_REQUIRED_DATA_COMMON = [
+        'paymentMethod' => '',
+        'paymentProvider' => '',
+        'paymentSelection' => '',
+    ];
+
+    protected const PAYMENT_REQUIRED_DATA = [
+        'dummyPaymentInvoice' => [
+            'dateOfBirth' => '',
+        ],
+        'dummyPaymentCreditCard' => [
+            'cardType' => '',
+            'cardNumber' => '',
+            'nameOnCard' => '',
+            'cardExpiresMonth' => '',
+            'cardExpiresYear' => '',
+            'cardSecurityCode' => '',
+        ],
+    ];
+}
