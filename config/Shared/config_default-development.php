ab7882d89252bddc8948a072b4afed99e95df94b
Ross Crawford-d'Heureuse <ross.crawford@mindcurv.com>
2018-11-02 15:36:11 +0100

    base

--- config/Shared/config_default-development.php
+++ config/Shared/config_default-development.php
@@ -44,25 +44,25 @@ $config[ApplicationConstants::YVES_SSL_ENABLED] = false;
 // ---------- Propel
 $config[PropelConstants::PROPEL_DEBUG] = false;
 $config[PropelOrmConstants::PROPEL_SHOW_EXTENDED_EXCEPTION] = true;
-$config[PropelConstants::ZED_DB_USERNAME] = 'development';
-$config[PropelConstants::ZED_DB_PASSWORD] = 'mate20mg';
-$config[PropelConstants::ZED_DB_HOST] = '127.0.0.1';
-$config[PropelConstants::ZED_DB_PORT] = 5432;
+$config[PropelConstants::ZED_DB_USERNAME] = getenv('DB_USERNAME');
+$config[PropelConstants::ZED_DB_PASSWORD] = getenv('DB_PASSWORD');
+$config[PropelConstants::ZED_DB_HOST] = getenv('DB_HOST');
+$config[PropelConstants::ZED_DB_PORT] = getenv('DB_PORT', 5432);
 $config[PropelConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
 $config[PropelQueryBuilderConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
 
 // ---------- Redis
 $config[StorageConstants::STORAGE_REDIS_PROTOCOL] = 'tcp';
-$config[StorageConstants::STORAGE_REDIS_HOST] = '127.0.0.1';
-$config[StorageConstants::STORAGE_REDIS_PORT] = '10009';
+$config[StorageConstants::STORAGE_REDIS_HOST] = getenv('REDIS_HOST', 'redis');
+$config[StorageConstants::STORAGE_REDIS_PORT] = getenv('REDIS_PORT', '10009');
 $config[StorageConstants::STORAGE_REDIS_PASSWORD] = false;
-$config[StorageConstants::STORAGE_REDIS_DATABASE] = 0;
+$config[StorageConstants::STORAGE_REDIS_DATABASE] = getenv('REDIS_DATABASE', 0);
 
 // ---------- RabbitMQ
-$config[RabbitMqEnv::RABBITMQ_API_HOST] = 'localhost';
-$config[RabbitMqEnv::RABBITMQ_API_PORT] = '15672';
-$config[RabbitMqEnv::RABBITMQ_API_USERNAME] = 'admin';
-$config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = 'mate20mg';
+$config[RabbitMqEnv::RABBITMQ_API_HOST] = getenv('RABBIT_HOST', 'rabbit');
+$config[RabbitMqEnv::RABBITMQ_API_PORT] = getenv('RABBIT_PORT', '15672');
+$config[RabbitMqEnv::RABBITMQ_API_USERNAME] = getenv('RABBIT_USERNAME', 'admin');
+$config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = getenv('RABBIT_PASSWORD', 'mate20mg');
 
 // ---------- Session
 $config[SessionConstants::YVES_SESSION_COOKIE_SECURE] = false;
