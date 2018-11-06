3ea855ca48493dc763fb4f1c20d90ebab8279a2e
Evhen Odokienko <evhen.odokienko@spryker.com>
2018-11-06 12:24:59 +0200

    CC-1433 Cleanup of deprecated modules

--- src/Pyz/Zed/CompanyRole/CompanyRoleConfig.php
+++ src/Pyz/Zed/CompanyRole/CompanyRoleConfig.php
@@ -8,6 +8,7 @@
 namespace Pyz\Zed\CompanyRole;
 
 use Generated\Shared\Transfer\CompanyRoleTransfer;
+use Spryker\Shared\Checkout\Plugin\Permission\PlaceOrderWithAmountUpToPermissionPlugin;
 use Spryker\Shared\CompanyUser\Plugin\AddCompanyUserPermissionPlugin;
 use Spryker\Shared\CompanyUserInvitation\Plugin\ManageCompanyUserInvitationPermissionPlugin;
 use Spryker\Zed\CompanyRole\CompanyRoleConfig as SprykerCompanyRoleConfig;
@@ -29,6 +30,7 @@ class CompanyRoleConfig extends SprykerCompanyRoleConfig
             AddCompanyUserPermissionPlugin::KEY,
             ManageCompanyUserInvitationPermissionPlugin::KEY,
             CompanyUserStatusChangePermissionPlugin::KEY,
+            PlaceOrderWithAmountUpToPermissionPlugin::KEY,
         ];
     }
 
