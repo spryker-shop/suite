7abd5177ce3150175a1a9cedf8adaeb875b5530d
Ruslan Ivanov <ruslan.ivanov@spryker.com>
2018-11-16 18:17:04 +0200

    PS-6105: added dependency provider to Pyz

---
+++ src/Pyz/Zed/BusinessOnBehalfGui/BusinessOnBehalfGuiDependencyProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\BusinessOnBehalfGui;
+
+use Spryker\Zed\BusinessOnBehalfGui\BusinessOnBehalfGuiDependencyProvider as SprykerBusinessOnBehalfGuiDependencyProvider;
+use Spryker\Zed\CompanyBusinessUnitGui\Communication\Plugin\BusinessOnBehalfGui\CompanyBusinessUnitCustomerBusinessUnitAttachFormExpanderPlugin;
+use Spryker\Zed\CompanyRoleGui\Communication\Plugin\BusinessOnBehalfGui\CompanyRoleCustomerBusinessUnitAttachFormExpanderPlugin;
+
+/**
+ * @method \Spryker\Zed\BusinessOnBehalfGui\BusinessOnBehalfGuiConfig getConfig()
+ */
+class BusinessOnBehalfGuiDependencyProvider extends SprykerBusinessOnBehalfGuiDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\CompanyUserGuiExtension\Dependency\Plugin\CompanyUserAttachCustomerFormExpanderPluginInterface[]
+     */
+    protected function getCustomerBusinessUnitAttachFormExpanderPlugins(): array
+    {
+        return [
+            new CompanyBusinessUnitCustomerBusinessUnitAttachFormExpanderPlugin(),
+            new CompanyRoleCustomerBusinessUnitAttachFormExpanderPlugin(),
+        ];
+    }
+}
