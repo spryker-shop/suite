1f2e05ee1cef301062326f451fa9103aee3a9962
Sergey <sergeirybalko@gmail.com>
2018-12-27 12:34:55 +0200

    DFIB-2: Change Travis CI

--- frontend/lint.js
+++ frontend/lint.js
@@ -9,6 +9,7 @@ stylelint.lint({
     if (data.errored) {
         const messages = JSON.parse(JSON.stringify(data.output));
         process.stdout.write(messages);
+        process.exit(1);
     }
 }).catch(function(error) {
     console.error(error.stack);
