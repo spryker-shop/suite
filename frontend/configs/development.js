a5351db0fa9a7a7250e0fb876a333eac9750f20f
Sergey <sergeirybalko@gmail.com>
2018-12-27 11:22:36 +0200

    DFIB-2: Change stylelint rules and tools

--- frontend/configs/development.js
+++ frontend/configs/development.js
@@ -4,6 +4,7 @@ const autoprefixer = require('autoprefixer');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 const appSettings = require('../settings');
 const finder = require('../libs/finder');
 const alias = require('../libs/alias');
@@ -121,6 +122,12 @@ module.exports = {
             root: path.join(appSettings.context, appSettings.paths.public),
             verbose: true,
             beforeEmit: true
+        }),
+
+        new StyleLintPlugin({
+            context: process.cwd(),
+            files: [`${appSettings.paths.core.modules}/**/*.scss`, `${appSettings.paths.project.modules}/**/*.scss`],
+            syntax: 'scss',
         }),
 
         new CopyWebpackPlugin([
--- frontend/lint.js
+++ frontend/lint.js
@@ -2,11 +2,13 @@ const stylelint = require('stylelint');
 const appSettings = require('./settings');
 
 stylelint.lint({
-    files: `${appSettings.paths.core.modules}/**/*.scss`,
-    syntax: "scss"
+    files: [`${appSettings.paths.core.modules}/**/*.scss`, `${appSettings.paths.project.modules}/**/*.scss`],
+    syntax: "scss",
+    formatter: "string",
 }).then(function(data) {
     if (data.errored) {
-        process.stdout.write(JSON.stringify(data.output));
+        const messages = JSON.parse(JSON.stringify(data.output));
+        process.stdout.write(messages);
     }
 }).catch(function(error) {
     console.error(error.stack);
