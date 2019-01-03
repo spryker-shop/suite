9e3a3031c5157ee3e08a186050627dd2ad2f452b
Alexander Veselov <alexander.veselov@spryker.com>
2019-01-03 16:41:52 +0000

    CMS-84: CMS-85: composer updated, enabled contentStorage

--- src/Pyz/Zed/Synchronization/SynchronizationDependencyProvider.php
+++ src/Pyz/Zed/Synchronization/SynchronizationDependencyProvider.php
@@ -16,6 +16,7 @@ use Spryker\Zed\CmsBlockProductStorage\Communication\Plugin\Synchronization\CmsB
 use Spryker\Zed\CmsBlockStorage\Communication\Plugin\Synchronization\CmsBlockSynchronizationDataPlugin;
 use Spryker\Zed\CmsPageSearch\Communication\Plugin\Synchronization\CmsPageSynchronizationDataPlugin;
 use Spryker\Zed\CmsStorage\Communication\Plugin\Synchronization\CmsSynchronizationDataPlugin;
+use Spryker\Zed\ContentStorage\Communication\Plugin\Synchronization\ContentStorageSynchronizationDataPlugin;
 use Spryker\Zed\CustomerAccessStorage\Communication\Plugin\Synchronization\CustomerAccessSynchronizationDataPlugin;
 use Spryker\Zed\GlossaryStorage\Communication\Plugin\Synchronization\GlossarySynchronizationDataPlugin;
 use Spryker\Zed\NavigationStorage\Communication\Plugin\Synchronization\NavigationSynchronizationDataPlugin;
@@ -105,6 +106,7 @@ class SynchronizationDependencyProvider extends SprykerSynchronizationDependency
             new ProductDiscontinuedSynchronizationDataPlugin(),
             new CustomerAccessSynchronizationDataPlugin(),
             new ProductConcretePageSynchronizationDataPlugin(),
+            new ContentStorageSynchronizationDataPlugin(),
         ];
     }
 }
