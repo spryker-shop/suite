b7a615a2b185f91c281c18cf8e4545836c7c41a4
galitsky <eugen.galitsky@spryker.com>
2019-01-03 16:12:14 +0200

    DFIB-4: travis tslint

--- frontend/libs/tslint.js
+++ frontend/libs/tslint.js
@@ -8,32 +8,40 @@ class TSSprykerLinter {
             fix: false,
             formatter: this.outputFormatter,
         };
-        
+
         this.program = Linter.createProgram("tsconfig.json", appSettings.context);
         this.configurationFilename = path.join(appSettings.context, 'tslint.json');
         this.linter = new Linter(this.options, this.program);
         this.files = Linter.getFileNames(this.program);
     }
-    
+
     lintFiles() {
         this.files.forEach((file) => {
             const fileContents = this.program.getSourceFile(file).getFullText();
             const configuration = Configuration.findConfiguration(this.configurationFilename, file).results;
             this.linter.lint(file, fileContents, configuration);
         });
-        
+
         this.showLintOutput();
     }
-    
+
     showLintOutput() {
         const lintingResult = this.linter.getResult();
-        
+
         console.log(
             lintingResult.output,
             `\x1b[36mErrors count: ${lintingResult.errorCount}\x1b[0m`
         );
+
+        this.exitProcess(lintingResult.errorCount);
     }
-    
+
+    exitProcess(errorCount) {
+        if(errorCount > 0 && process.env.NODE_ENV !== 'development') {
+            process.exit(1);
+        }
+    }
+
     get outputFormatter() {
         const formatterName = process.argv.slice(2)[0];
         return formatterName ? formatterName : 'codeFrame';
