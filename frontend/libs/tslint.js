d9fcef94dc0862e39b55ee329a8598876a0edabd
galitsky <eugen.galitsky@spryker.com>
2019-01-09 12:29:02 +0200

    DFIB-4: remove class, fix formatting, add comments

--- frontend/libs/tslint.js
+++ frontend/libs/tslint.js
@@ -1,52 +1,57 @@
 const path = require('path');
 const { Linter, Configuration } = require("tslint");
 const appSettings = require('../settings');
+const colors = require('colors');
 
-class TSSprykerLinter {
-    constructor() {
-        this.options = {
-            fix: false,
-            formatter: this.outputFormatter,
-        };
+/**
+ * List of output formatters for the tslint.
+ * https://palantir.github.io/tslint/formatters/
+ */
+const outputFormatter = () => {
+    const formatterName = process.argv.slice(2)[0];
+    const defaultFormatter = 'codeFrame';
+    return formatterName ? formatterName : defaultFormatter;
+};
 
-        this.program = Linter.createProgram("tsconfig.json", appSettings.context);
-        this.configurationFilename = path.join(appSettings.context, 'tslint.json');
-        this.linter = new Linter(this.options, this.program);
-        this.files = Linter.getFileNames(this.program);
+const linterOptions = {
+    fix: false,
+    formatter: outputFormatter(),
+};
+
+const runTSLint = () => {
+    const program = Linter.createProgram('tsconfig.json', appSettings.context);
+    const configurationFilename = path.join(appSettings.context, 'tslint.json');
+    const linter = new Linter(linterOptions, program);
+    const files = Linter.getFileNames(program);
+
+    lintFiles(program, configurationFilename, linter, files);
+};
+
+const lintFiles = (program, configurationFilename, linter, files) => {
+    files.forEach((file) => {
+        const fileContents = program.getSourceFile(file).getFullText();
+        const configuration = Configuration.findConfiguration(configurationFilename, file).results;
+        linter.lint(file, fileContents, configuration);
+    });
+
+    showLintOutput(linter);
+};
+
+const showLintOutput = (linter) => {
+    const lintingResult = linter.getResult();
+
+    console.log(
+        lintingResult.output,
+        `Errors count: ${colors.red.underline(lintingResult.errorCount)}\n`
+    );
+
+    exitProcess(lintingResult.errorCount);
+};
+
+const exitProcess = (errorCount) => {
+    if (errorCount > 0 && process.env.NODE_ENV !== 'development') {
+        process.exit(1);
     }
+};
 
-    lintFiles() {
-        this.files.forEach((file) => {
-            const fileContents = this.program.getSourceFile(file).getFullText();
-            const configuration = Configuration.findConfiguration(this.configurationFilename, file).results;
-            this.linter.lint(file, fileContents, configuration);
-        });
-
-        this.showLintOutput();
-    }
-
-    showLintOutput() {
-        const lintingResult = this.linter.getResult();
-
-        console.log(
-            lintingResult.output,
-            `\x1b[36mErrors count: ${lintingResult.errorCount}\x1b[0m`
-        );
-
-        this.exitProcess(lintingResult.errorCount);
-    }
-
-    exitProcess(errorCount) {
-        if(errorCount > 0 && process.env.NODE_ENV !== 'development') {
-            process.exit(1);
-        }
-    }
-
-    get outputFormatter() {
-        const formatterName = process.argv.slice(2)[0];
-        return formatterName ? formatterName : 'codeFrame';
-    }
-}
-
-const tsLint = new TSSprykerLinter();
-tsLint.lintFiles();
+runTSLint();
