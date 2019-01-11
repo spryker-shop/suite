7879d48dffdad846ca9e505fcec4f320b76277aa
Ivan Pugach <ivan.pugach@spryker.com>
2019-01-11 18:04:00 +0200

    Fix after CR

--- src/Pyz/Client/QuickOrder/QuickOrderDependencyProvider.php
+++ src/Pyz/Client/QuickOrder/QuickOrderDependencyProvider.php
@@ -7,10 +7,10 @@
 
 namespace Pyz\Client\QuickOrder;
 
-use Spryker\Client\PriceProductStorage\Plugin\QuickOrder\QuickOrderPriceValidationPlugin;
-use Spryker\Client\ProductDiscontinuedStorage\Plugin\QuickOrder\QuickOrderDiscontinuedValidationPlugin;
+use Spryker\Client\PriceProductStorage\Plugin\QuickOrder\QuickOrderProductPriceValidationPlugin;
+use Spryker\Client\ProductDiscontinuedStorage\Plugin\QuickOrder\QuickOrderProductDiscontinuedValidationPlugin;
 use Spryker\Client\ProductMeasurementUnitStorage\Plugin\QuickOrder\ProductConcreteTransferBaseMeasurementUnitExpanderPlugin;
-use Spryker\Client\ProductQuantityStorage\Plugin\QuickOrder\QuickOrderQuantityValidationPlugin;
+use Spryker\Client\ProductQuantityStorage\Plugin\QuickOrder\QuickOrderProductQuantityValidationPlugin;
 use Spryker\Client\QuickOrder\QuickOrderDependencyProvider as SprykerQuickOrderDependencyProvider;
 
 class QuickOrderDependencyProvider extends SprykerQuickOrderDependencyProvider
@@ -26,14 +26,14 @@ class QuickOrderDependencyProvider extends SprykerQuickOrderDependencyProvider
     }
 
     /**
-     * @return \Spryker\Client\QuickOrderExtension\Dependency\Plugin\QuickOrderValidatorPluginInterface[]
+     * @return \Spryker\Client\QuickOrderExtension\Dependency\Plugin\QuickOrderValidationPluginInterface[]
      */
     protected function getQuickOrderValidationPlugins(): array
     {
         return [
-            new QuickOrderDiscontinuedValidationPlugin(),
-            new QuickOrderPriceValidationPlugin(),
-            new QuickOrderQuantityValidationPlugin(),
+            new QuickOrderProductDiscontinuedValidationPlugin(),
+            new QuickOrderProductPriceValidationPlugin(),
+            new QuickOrderProductQuantityValidationPlugin(),
         ];
     }
 }
