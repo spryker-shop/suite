79a79456255b289ebd6f977f846e2681f901a281
Alexander Veselov <alexander.veselov@spryker.com>
2019-01-14 11:17:08 +0000

    CMS-84: CMS-86: updated composer.lock, added productListPlugin

---
+++ src/Pyz/Zed/ContentGui/ContentGuiDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ContentGui;
+
+use Spryker\Zed\ContentGui\ContentGuiDependencyProvider as SprykerContentGuiDependencyProvider;
+use Spryker\Zed\ContentProductConnector\Communication\Plugin\ContentProductConnectorPlugin;
+
+class ContentGuiDependencyProvider extends SprykerContentGuiDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\ContentGuiExtension\Plugin\ContentPluginInterface[]
+     */
+    protected function getContentItemPlugins(): array
+    {
+        return [
+            new ContentProductConnectorPlugin(),
+        ];
+    }
+}
