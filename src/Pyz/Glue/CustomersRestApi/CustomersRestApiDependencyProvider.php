10432d8d9ccd563b5fca29cbac2d576152d7c65d
voitov <tigran.voitov@spryker.com>
2019-01-24 13:10:54 +0200

    glue-2279 glue-2285

--- src/Pyz/Glue/CustomersRestApi/CustomersRestApiDependencyProvider.php
+++ src/Pyz/Glue/CustomersRestApi/CustomersRestApiDependencyProvider.php
@@ -7,17 +7,30 @@
 
 namespace Pyz\Glue\CustomersRestApi;
 
-use Spryker\Glue\CartsRestApi\Plugin\CustomerPostRegister\UpdateCartCreateCustomerReferencePlugin;
+use Spryker\Glue\CartsRestApi\Plugin\CustomerPostCreate\UpdateCartCreateCustomerReferencePlugin;
+use Spryker\Glue\CartsRestApi\Plugin\CustomerPostRegister\UpdateCartCustomerReferencePlugin;
 use Spryker\Glue\CustomersRestApi\CustomersRestApiDependencyProvider as SprykerCustomersRestApiDependencyProvider;
 
 class CustomersRestApiDependencyProvider extends SprykerCustomersRestApiDependencyProvider
 {
     /**
-     * @return \Spryker\Glue\CustomersRestApiExtension\Dependency\Plugin\CustomerPostCreatePluginInterface[]
+     * @deprecated use getCustomerPostCreatePlugins()
+     *
+     * @return \Spryker\Glue\CustomersRestApiExtension\Dependency\Plugin\CustomerPostRegisterPluginInterface[]
      */
     protected function getCustomerPostRegisterPlugins(): array
     {
         return array_merge(parent::getCustomerPostRegisterPlugins(), [
+            new UpdateCartCustomerReferencePlugin(),
+        ]);
+    }
+
+    /**
+     * @return \Spryker\Glue\CustomersRestApiExtension\Dependency\Plugin\CustomerPostCreatePluginInterface[]
+     */
+    protected function getCustomerPostCreatePlugins(): array
+    {
+        return array_merge(parent::getCustomerPostCreatePlugins(), [
             new UpdateCartCreateCustomerReferencePlugin(),
         ]);
     }
