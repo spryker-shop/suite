77bb22a960da999ed13bc0de667fb450c2458a89
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-01-29 10:53:52 +0200

    PS-4362: fix broken vouchers bug (PS-7419) amendments after TL review

--- src/Pyz/Zed/Calculation/CalculationDependencyProvider.php
+++ src/Pyz/Zed/Calculation/CalculationDependencyProvider.php
@@ -33,7 +33,7 @@ use Spryker\Zed\DiscountCalculationConnector\Communication\Plugin\DiscountCalcul
 use Spryker\Zed\DiscountPromotion\Communication\Plugin\Calculation\RemovePromotionItemsCalculatorPlugin;
 use Spryker\Zed\Kernel\Container;
 use Spryker\Zed\Payment\Communication\Plugin\Calculation\PaymentCalculatorPlugin;
-use Spryker\Zed\PersistentCart\Communication\Plugin\QuoteSaveAfterQuoteCalculatePlugin;
+use Spryker\Zed\PersistentCart\Communication\Plugin\Calculation\QuoteAttributesSavePostQuoteRecalculatePlugin;
 use Spryker\Zed\ProductBundle\Communication\Plugin\Calculation\CalculateBundlePricePlugin;
 use Spryker\Zed\ProductOption\Communication\Plugin\ProductOptionTaxRateCalculatorPlugin;
 use Spryker\Zed\SalesOrderThreshold\Communication\Plugin\Calculation\AddSalesOrderThresholdExpenseCalculatorPlugin;
@@ -270,12 +270,12 @@ class CalculationDependencyProvider extends SprykerCalculationDependencyProvider
     }
 
     /**
-     * @return \Spryker\Zed\CalculationExtension\Dependency\Plugin\QuoteAfterCalculatePluginInterface[]
+     * @return \Spryker\Zed\CalculationExtension\Dependency\Plugin\PostQuoteRecalculatePluginInterface[]
      */
-    protected function getQuoteAfterCalculatePlugins(): array
+    protected function getPostQuoteRecalculatePlugins(): array
     {
         return [
-            new QuoteSaveAfterQuoteCalculatePlugin(),
+            new QuoteAttributesSavePostQuoteRecalculatePlugin(),
         ];
     }
 }
