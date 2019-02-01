385323d86dc7442bc3c7a9e607927caa5d86109c
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-02-01 16:20:28 +0200

    CC-990 activated CustomerReorderItemCheckboxWidget

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -21,6 +21,7 @@ use SprykerShop\Yves\CompanyWidget\Widget\CompanyBusinessUnitAddressWidget;
 use SprykerShop\Yves\CompanyWidget\Widget\CompanyMenuItemWidget;
 use SprykerShop\Yves\CurrencyWidget\Widget\CurrencyWidget;
 use SprykerShop\Yves\CustomerPage\Widget\CustomerNavigationWidget;
+use SprykerShop\Yves\CustomerReorderWidget\Plugin\CustomerPage\CustomerReorderItemCheckboxWidget;
 use SprykerShop\Yves\DiscountPromotionWidget\Widget\CartDiscountPromotionProductListWidget;
 use SprykerShop\Yves\DiscountWidget\Widget\CheckoutVoucherFormWidget;
 use SprykerShop\Yves\DiscountWidget\Widget\DiscountVoucherFormWidget;
@@ -107,6 +108,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             CreateShoppingListFromCartWidget::class,
             CurrencyWidget::class,
             CustomerNavigationWidget::class,
+            CustomerReorderItemCheckboxWidget::class,
             DisplayProductAbstractReviewWidget::class,
             ExampleProductColorSelectorWidget::class,
             LanguageSwitcherWidget::class,
