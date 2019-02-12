5c9fdee0b042bcb249a127bb576ff3be961f0ad6
Karoly Gerner <30629375+gerner-spryker@users.noreply.github.com>
2019-02-12 17:44:46 +0100

    PS-3945 File upload concrete products (#1729)
    * PS-3945: File upload concrete products

--- src/Pyz/Yves/QuickOrderPage/QuickOrderPageDependencyProvider.php
+++ src/Pyz/Yves/QuickOrderPage/QuickOrderPageDependencyProvider.php
@@ -10,6 +10,10 @@ namespace Pyz\Yves\QuickOrderPage;
 use SprykerShop\Yves\ProductPackagingUnitWidget\Plugin\QuickOrder\QuickOrderItemDefaultPackagingUnitExpanderPlugin;
 use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrder\QuickOrderFormMeasurementUnitColumnPlugin;
 use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrderPage\ProductQuantityFilterPlugin;
+use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrderPage\QuickOrderCsvFileParserStrategyPlugin;
+use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrderPage\QuickOrderCsvFileProcessorStrategyPluginStrategyPluginInterface;
+use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrderPage\QuickOrderCsvFileTemplateStrategyPlugin;
+use SprykerShop\Yves\QuickOrderPage\Plugin\QuickOrderPage\QuickOrderCsvFileValidatorStrategyPlugin;
 use SprykerShop\Yves\QuickOrderPage\QuickOrderPageDependencyProvider as SprykerQuickOrderPageDependencyProvider;
 use SprykerShop\Yves\ShoppingListWidget\Plugin\QuickOrderPage\ShoppingListQuickOrderFormHandlerStrategyPlugin;
 
@@ -52,6 +56,36 @@ class QuickOrderPageDependencyProvider extends SprykerQuickOrderPageDependencyPr
     {
         return [
             new ProductQuantityFilterPlugin(),
+        ];
+    }
+
+    /**
+     * @return \SprykerShop\Yves\QuickOrderPageExtension\Dependency\Plugin\QuickOrderFileParserStrategyPluginInterface[]
+     */
+    protected function getQuickOrderFileParserPlugins(): array
+    {
+        return [
+            new QuickOrderCsvFileParserStrategyPlugin(),
+        ];
+    }
+
+    /**
+     * @return \SprykerShop\Yves\QuickOrderPageExtension\Dependency\Plugin\QuickOrderFileTemplateStrategyPluginInterface[]
+     */
+    protected function getQuickOrderFileTemplatePlugins(): array
+    {
+        return [
+            new QuickOrderCsvFileTemplateStrategyPlugin(),
+        ];
+    }
+
+    /**
+     * @return \SprykerShop\Yves\QuickOrderPageExtension\Dependency\Plugin\QuickOrderFileValidatorStrategyPluginInterface[]
+     */
+    protected function getQuickOrderFileValidatorPlugins(): array
+    {
+        return [
+            new QuickOrderCsvFileValidatorStrategyPlugin(),
         ];
     }
 }
