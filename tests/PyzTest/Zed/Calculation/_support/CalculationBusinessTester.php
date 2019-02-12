70a8541aa2c77f09e85c6f333bc94dfda6a693f8
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-02-12 18:46:47 +0200

    PS-4358: fix constants PLUGINS_POST_QUOTE_RECALCULATE => PLUGINS_QUOTE_POST_RECALCULATE

--- tests/PyzTest/Zed/Calculation/_support/CalculationBusinessTester.php
+++ tests/PyzTest/Zed/Calculation/_support/CalculationBusinessTester.php
@@ -160,7 +160,7 @@ class CalculationBusinessTester extends Actor
 
         $container = new Container();
         $container[CalculationDependencyProvider::QUOTE_CALCULATOR_PLUGIN_STACK] = $calculatorPlugins;
-        $container[CalculationDependencyProvider::PLUGINS_POST_QUOTE_RECALCULATE] = [];
+        $container[CalculationDependencyProvider::PLUGINS_QUOTE_POST_RECALCULATE] = [];
 
         $calculationBusinessFactory->setContainer($container);
         $calculationFacade->setFactory($calculationBusinessFactory);
