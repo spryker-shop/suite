7bb9b751cdb64b2859a7c6f6a0f7739e996fcee0
laktionova <helen.laktionova@spryker.com>
2019-02-13 11:43:43 +0200

    * MP-667: * Updated composer; * Fixed code sniff.

--- src/Pyz/Zed/Auth/AuthDependencyProvider.php
+++ src/Pyz/Zed/Auth/AuthDependencyProvider.php
@@ -1,16 +1,15 @@
 <?php
 
 /**
- * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
- * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
 namespace Pyz\Zed\Auth;
 
+use Spryker\Zed\Auth\AuthDependencyProvider as SprykerAuthDependencyProvider;
 use Spryker\Zed\AuthMailConnector\Communication\Plugin\AuthPasswordResetMailSenderPlugin;
 use Spryker\Zed\Kernel\Container;
-
-use Spryker\Zed\Auth\AuthDependencyProvider as SprykerAuthDependencyProvider;
 
 /**
  * @method \Spryker\Zed\Auth\AuthConfig getConfig()
