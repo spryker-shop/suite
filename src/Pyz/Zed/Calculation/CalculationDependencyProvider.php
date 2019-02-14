1a3dfc3e0f052b674d45ac76168d2e4a86a124a1
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-02-14 16:33:05 +0200

    PS-4358: refactor QuoteAttributesSaveQuotePostRecalculatePlugin => QuoteSaveQuotePostRecalculateStrategyPlugin

--- src/Pyz/Zed/Calculation/CalculationDependencyProvider.php
+++ src/Pyz/Zed/Calculation/CalculationDependencyProvider.php
@@ -33,7 +33,7 @@ use Spryker\Zed\DiscountCalculationConnector\Communication\Plugin\DiscountCalcul
 use Spryker\Zed\DiscountPromotion\Communication\Plugin\Calculation\RemovePromotionItemsCalculatorPlugin;
 use Spryker\Zed\Kernel\Container;
 use Spryker\Zed\Payment\Communication\Plugin\Calculation\PaymentCalculatorPlugin;
-use Spryker\Zed\PersistentCart\Communication\Plugin\Calculation\QuoteAttributesSaveQuotePostRecalculatePlugin;
+use Spryker\Zed\PersistentCart\Communication\Plugin\Calculation\QuoteSaveQuotePostRecalculateStrategyPlugin;
 use Spryker\Zed\ProductBundle\Communication\Plugin\Calculation\CalculateBundlePricePlugin;
 use Spryker\Zed\ProductOption\Communication\Plugin\ProductOptionTaxRateCalculatorPlugin;
 use Spryker\Zed\SalesOrderThreshold\Communication\Plugin\Calculation\AddSalesOrderThresholdExpenseCalculatorPlugin;
@@ -270,12 +270,12 @@ class CalculationDependencyProvider extends SprykerCalculationDependencyProvider
     }
 
     /**
-     * @return \Spryker\Zed\CalculationExtension\Dependency\Plugin\QuotePostRecalculatePluginInterface[]
+     * @return \Spryker\Zed\CalculationExtension\Dependency\Plugin\QuotePostRecalculatePluginStrategyInterface[]
      */
     protected function getQuotePostRecalculatePlugins(): array
     {
         return [
-            new QuoteAttributesSaveQuotePostRecalculatePlugin(),
+            new QuoteSaveQuotePostRecalculateStrategyPlugin(),
         ];
     }
 }
