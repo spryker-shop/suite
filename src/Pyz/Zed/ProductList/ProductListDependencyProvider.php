74b06231f2333107c45d31e120dbac6592f582c6
laktionova <helen.laktionova@spryker.com>
2019-02-28 17:13:58 +0200

    PS-7883: - Registered `ProductBundleProductListPreSaverPlugin`; - Updated composer.lock/composer.json

---
+++ src/Pyz/Zed/ProductList/ProductListDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ProductList;
+
+use Spryker\Zed\ProductBundleProductListConnector\Communication\Plugin\ProductBundleProductListPreSaverPlugin;
+use Spryker\Zed\ProductList\ProductListDependencyProvider as SprykerProductListDependencyProvider;
+
+class ProductListDependencyProvider extends SprykerProductListDependencyProvider
+{
+    /**
+     * @return \Spryker\Zed\ProductListExtension\Dependency\Plugin\ProductListPreSaverInterface[]
+     */
+    protected function getProductListPreSavePlugins(): array
+    {
+        return [
+            new ProductBundleProductListPreSaverPlugin(),
+        ];
+    }
+}
