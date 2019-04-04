afb83ff9d4204a0d31bb72f7c847982bfb2edc40
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-04-04 19:54:30 +0300

    PS-9056: disabled quite unlock after approval rejection when quote in process of quotation.

--- src/Pyz/Zed/Quote/QuoteDependencyProvider.php
+++ src/Pyz/Zed/Quote/QuoteDependencyProvider.php
@@ -16,6 +16,7 @@ use Spryker\Zed\MultiCart\Communication\Plugin\ResolveQuoteNameBeforeQuoteCreate
 use Spryker\Zed\Quote\QuoteDependencyProvider as SprykerQuoteDependencyProvider;
 use Spryker\Zed\QuoteApproval\Communication\Plugin\Quote\QuoteApprovalExpanderPlugin;
 use Spryker\Zed\QuoteApproval\Communication\Plugin\Quote\RemoveQuoteApprovalsBeforeQuoteDeletePlugin;
+use Spryker\Zed\QuoteRequest\Communication\Plugin\Quote\QuoteUnlockPreCheckPlugin;
 use Spryker\Zed\SharedCart\Communication\Plugin\CleanQuoteShareBeforeQuoteCreatePlugin;
 use Spryker\Zed\SharedCart\Communication\Plugin\DeactivateSharedQuotesBeforeQuoteSavePlugin;
 use Spryker\Zed\SharedCart\Communication\Plugin\MarkAsDefaultQuoteAfterSavePlugin;
@@ -103,6 +104,16 @@ class QuoteDependencyProvider extends SprykerQuoteDependencyProvider
     {
         return [
             new InitDefaultQuoteCustomerQuoteDeleteAfterPlugin(), #MultiCartFeature
+        ];
+    }
+
+    /**
+     * @return \Spryker\Zed\QuoteExtension\Dependency\Plugin\QuoteUnlockPreCheckPluginInterface[]
+     */
+    protected function getQuoteUnlockPreCheckPlugins(): array
+    {
+        return [
+            new QuoteUnlockPreCheckPlugin(),
         ];
     }
 }
