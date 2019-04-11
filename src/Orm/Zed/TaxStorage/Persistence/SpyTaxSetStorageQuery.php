4041cdd55551460ea212811cfe2c951ee6ab1eb1
Aleksandr Kovalchuk <avtop25495@gmail.com>
2019-04-11 12:39:40 +0300

    GLUE-3315

--- src/Orm/Zed/TaxStorage/Persistence/SpyTaxSetStorageQuery.php
+++ src/Orm/Zed/TaxStorage/Persistence/SpyTaxSetStorageQuery.php
@@ -1,10 +1,5 @@
 <?php
 
-/**
- * This file is part of the Spryker Suite.
- * For full license information, please view the LICENSE file that was distributed with this source code.
- */
-
 namespace Orm\Zed\TaxStorage\Persistence;
 
 use Orm\Zed\TaxStorage\Persistence\Base\SpyTaxSetStorageQuery as BaseSpyTaxSetStorageQuery;
