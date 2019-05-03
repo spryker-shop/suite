0b22b32685f00a4b3858106e221f3f7f655f7938
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-05-03 16:11:55 +0300

    PS-9127: implemented vault functionality

---
+++ src/Orm/Zed/Vault/Persistence/SpyVault.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\Vault\Persistence;
+
+use Orm\Zed\Vault\Persistence\Base\SpyVault as BaseSpyVault;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_vault' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyVault extends BaseSpyVault
+{
+}
---
+++ src/Orm/Zed/Vault/Persistence/SpyVaultQuery.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Orm\Zed\Vault\Persistence;
+
+use Orm\Zed\Vault\Persistence\Base\SpyVaultQuery as BaseSpyVaultQuery;
+
+/**
+ * Skeleton subclass for performing query and update operations on the 'spy_vault' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyVaultQuery extends BaseSpyVaultQuery
+{
+
+}
