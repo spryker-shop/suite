a4a17c674cb0b847db660a615fe14a94553491fd
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-05-03 16:40:55 +0300

    PS-9127: fix CS

--- src/Orm/Zed/Vault/Persistence/SpyVault.php
+++ src/Orm/Zed/Vault/Persistence/SpyVault.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\Vault\Persistence;
 
 use Orm\Zed\Vault\Persistence\Base\SpyVault as BaseSpyVault;
@@ -10,7 +15,7 @@ use Orm\Zed\Vault\Persistence\Base\SpyVault as BaseSpyVault;
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
--- src/Orm/Zed/Vault/Persistence/SpyVaultQuery.php
+++ src/Orm/Zed/Vault/Persistence/SpyVaultQuery.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\Vault\Persistence;
 
 use Orm\Zed\Vault\Persistence\Base\SpyVaultQuery as BaseSpyVaultQuery;
@@ -10,11 +15,10 @@ use Orm\Zed\Vault\Persistence\Base\SpyVaultQuery as BaseSpyVaultQuery;
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyVaultQuery extends BaseSpyVaultQuery
 {
-
 }
