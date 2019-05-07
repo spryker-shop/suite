ea968881c3cf2c4345f4eddd2bd1d09da883a756
René Klatt <rene.klatt@spryker.com>
2019-05-07 14:15:19 +0000

    TE-3458 Added composer-constrainer and module-finder.

--- src/Pyz/Zed/Console/ConsoleDependencyProvider.php
+++ src/Pyz/Zed/Console/ConsoleDependencyProvider.php
@@ -142,6 +142,7 @@ use Spryker\Zed\ZedNavigation\Communication\Console\BuildNavigationConsole;
 use SprykerSdk\Spryk\Console\SprykBuildConsole;
 use SprykerSdk\Spryk\Console\SprykDumpConsole;
 use SprykerSdk\Spryk\Console\SprykRunConsole;
+use SprykerSdk\Zed\ComposerConstrainer\Communication\Console\ComposerConstraintConsole;
 use Stecman\Component\Symfony\Console\BashCompletion\CompletionCommand;
 
 /**
@@ -347,6 +348,7 @@ class ConsoleDependencyProvider extends SprykerConsoleDependencyProvider
             $commands[] = new GenerateRestApiDocumentationConsole();
             $commands[] = new QueueDumpConsole();
             $commands[] = new EventTriggerListenerConsole();
+            $commands[] = new ComposerConstraintConsole();
         }
 
         return $commands;
