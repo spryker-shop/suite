6f82b20c07c165eda367400286ce2e4ce8a118d0
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2019-05-20 11:34:18 +0000

    TE-3561 Add CTE for ProductAbstract and ProductConcrete storage writer

---
+++ src/Pyz/Zed/ProductStorage/ProductStorageDependencyProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ProductStorage;
+
+use Spryker\Zed\Kernel\Container;
+use Spryker\Zed\ProductStorage\ProductStorageDependencyProvider as SprykerProductStorageDependencyProvider;
+
+class ProductStorageDependencyProvider extends SprykerProductStorageDependencyProvider
+{
+    public const SERVICE_SYNCHRONIZATION = 'SERVICE_SYNCHRONIZATION';
+    public const CLIENT_QUEUE = 'CLIENT_QUEUE';
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    public function provideBusinessLayerDependencies(Container $container)
+    {
+        $container = parent::provideBusinessLayerDependencies($container);
+        $container = $this->addSynchronizationService($container);
+        $container = $this->addQueueClient($container);
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addSynchronizationService(Container $container)
+    {
+        $container->set(static::SERVICE_SYNCHRONIZATION, function (Container $container) {
+            return $container->getLocator()->synchronization()->service();
+        });
+
+        return $container;
+    }
+
+    /**
+     * @param \Spryker\Zed\Kernel\Container $container
+     *
+     * @return \Spryker\Zed\Kernel\Container
+     */
+    protected function addQueueClient(Container $container)
+    {
+        $container->set(static::CLIENT_QUEUE, function (Container $container) {
+            return $container->getLocator()->queue()->client();
+        });
+
+        return $container;
+    }
+}
