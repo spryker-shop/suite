efd549ab8a3e1f6ac1b70cf3c9d902d315f92c29
Sergey Samoylov <sergey.samoylov@spryker.com>
2019-05-21 15:54:02 +0300

    cms-1919: cms-2140: add editor widget

--- src/Pyz/Zed/ContentGui/ContentGuiDependencyProvider.php
+++ src/Pyz/Zed/ContentGui/ContentGuiDependencyProvider.php
@@ -9,6 +9,7 @@ namespace Pyz\Zed\ContentGui;
 
 use Spryker\Zed\ContentBannerGui\Communication\Plugin\ContentGui\ContentBannerContentGuiEditorPlugin;
 use Spryker\Zed\ContentBannerGui\Communication\Plugin\ContentGui\ContentBannerFormPlugin;
+use Spryker\Zed\ContentFileGui\Communication\Plugin\ContentGui\ContentFileContentGuiEditorPlugin;
 use Spryker\Zed\ContentGui\ContentGuiDependencyProvider as SprykerContentGuiDependencyProvider;
 use Spryker\Zed\ContentProductGui\Communication\Plugin\ContentGui\ContentProductContentGuiEditorPlugin;
 use Spryker\Zed\ContentProductGui\Communication\Plugin\ContentGui\ProductAbstractListFormPlugin;
@@ -34,6 +35,7 @@ class ContentGuiDependencyProvider extends SprykerContentGuiDependencyProvider
         return [
             new ContentBannerContentGuiEditorPlugin(),
             new ContentProductContentGuiEditorPlugin(),
+            new ContentFileContentGuiEditorPlugin(),
         ];
     }
 }
