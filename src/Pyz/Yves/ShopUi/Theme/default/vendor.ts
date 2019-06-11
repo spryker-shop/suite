ac44150af13a2fffd52f4952763e94925027e832
danil.moroz <danil.moroz@spryker.com>
2019-06-11 11:06:44 +0300

    CC-4556: Yves, IE, Search field is cropped

--- src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
@@ -1,5 +1,6 @@
 // add webcomponents polyfill
 import '@webcomponents/webcomponentsjs/webcomponents-bundle';
+import 'string.prototype.startswith';
 import elementClosestPolyfill from 'element-closest';
 
 elementClosestPolyfill(window);
