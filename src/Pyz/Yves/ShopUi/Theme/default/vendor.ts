2f25270050f066455bbf19745400bcc437a1d37b
danil.moroz <danil.moroz@spryker.com>
2019-06-14 12:10:46 +0300

    CC-2075: Address form. Validation. Wrong label is displayed

--- src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/vendor.ts
@@ -1,6 +1,5 @@
 // add webcomponents polyfill
 import '@webcomponents/webcomponentsjs/webcomponents-bundle';
-import 'string.prototype.startswith';
 import elementClosestPolyfill from 'element-closest';
 
 elementClosestPolyfill(window);
