81af3f0e3561510d33e61c1c8a8dd1320e4419a4
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2019-07-05 15:24:08 +0300

    Tests

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -4,14 +4,6 @@ if (getenv('SPRYKER_TESTING_ENABLED')) {
     define('APPLICATION_ENV', getenv('APPLICATION_ENV'));
 }
 
-if (!getenv('SPRYKER_TEST_WEB_DRIVER_HOST')) {
-    define('SPRYKER_TEST_WEB_DRIVER_HOST', 'webdriver');
-}
-
-if (!getenv('SPRYKER_TEST_IN_BROWSER')) {
-    define('SPRYKER_TEST_IN_BROWSER', 'phantomjs');
-}
-
 define('APPLICATION_ROOT_DIR', dirname(__DIR__));
 
 putenv('APPLICATION_STORE=DE');
