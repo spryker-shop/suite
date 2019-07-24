6c4fd83c60aac0a5ea45977f85d6d3ade95568cf
Dima Aseev <dmitriy.aseev@spryker.com>
2019-07-24 15:04:11 +0300

    CC-5289: Introduced ProductConfigurableBundle module.

---
+++ src/Orm/Zed/ProductConfigurableBundle/Persistence/SpyProductConfigurableBundle.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Orm\Zed\ProductConfigurableBundle\Persistence;
+
+use Spryker\Zed\ProductConfigurableBundle\Persistence\Propel\AbstractSpyProductConfigurableBundle as BaseSpyProductConfigurableBundle;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_product_configurable_bundle' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements. This class will only be generated as
+ * long as it does not already exist in the output directory.
+ *
+ */
+class SpyProductConfigurableBundle extends BaseSpyProductConfigurableBundle
+{
+}
