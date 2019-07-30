22f6b72a1a18016787fe1f0f879f8f8cfdeca3ac
Anton Zubariev <anton.zubariev@spryker.com>
2019-07-30 08:06:27 +0300

    [CMS-2402] Composer update

--- src/Orm/Zed/CmsSlot/Persistence/SpyCmsSlotTemplate.php
+++ src/Orm/Zed/CmsSlot/Persistence/SpyCmsSlotTemplate.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\CmsSlot\Persistence;
 
 use Orm\Zed\CmsSlot\Persistence\Base\SpyCmsSlotTemplate as BaseSpyCmsSlotTemplate;
@@ -10,11 +15,10 @@ use Orm\Zed\CmsSlot\Persistence\Base\SpyCmsSlotTemplate as BaseSpyCmsSlotTemplat
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
  *
  */
 class SpyCmsSlotTemplate extends BaseSpyCmsSlotTemplate
 {
-
 }
