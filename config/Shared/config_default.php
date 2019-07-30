ecc1f360701f0998d2fc39c14117701b4bc0ba48
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2019-07-30 16:28:05 +0300

    CMS-2062 reveretd config changes, updated composer.

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -381,11 +381,6 @@ $config[KernelConstants::DEPENDENCY_INJECTOR_ZED] = [
         GiftCardConfig::PROVIDER_NAME,
     ],
 ];
-$config[KernelConstants::DEPENDENCY_INJECTOR_GLUE] = [
-    'GlueApplication' => [
-        'ProductsRestApi',
-    ],
-];
 
 $config[NopaymentConstants::NO_PAYMENT_METHODS] = [
     NopaymentConfig::PAYMENT_PROVIDER_NAME,
