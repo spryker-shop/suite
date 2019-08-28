32498f59412727116b52dc345827df1e2a6da913
artemkosatov <artem.kosatov@spryker.com>
2019-08-28 16:41:12 +0300

    PS-10020: Extended ProductReplacementForWidgetConfig at the project level.

---
+++ src/Pyz/Yves/ProductReplacementForWidget/ProductReplacementForWidgetConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\ProductReplacementForWidget;
+
+use SprykerShop\Yves\ProductReplacementForWidget\ProductReplacementForWidgetConfig as SprykerShopProductReplacementForWidgetConfig;
+
+class ProductReplacementForWidgetConfig extends SprykerShopProductReplacementForWidgetConfig
+{
+    /**
+     * @return bool
+     */
+    public function checkShowApplicableAlternativeProductsOnly(): bool
+    {
+        return true;
+    }
+}
