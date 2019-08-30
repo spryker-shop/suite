c12b42269d32d1586b3200e2afb2bd0ca185f714
Ehsan Zanjani <ehsan.zanjani@spryker.com>
2019-08-30 14:15:58 +0000

    TE-4013 update composer

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -440,6 +440,7 @@ $config[LogglyConstants::ERROR_QUEUE_NAME] = 'loggly-log-queue.error';
 
 // ---------- Event
 $config[EventConstants::EVENT_CHUNK] = 500;
+$config[EventConstants::EVENT_QUEUE_RETRY_ACTIVE] = true;
 
 // ---------- EventBehavior
 $config[EventBehaviorConstants::EVENT_BEHAVIOR_TRIGGERING_ACTIVE] = true;
