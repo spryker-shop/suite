9347627ddc3c1700737db7560fe6fa4e1c859875
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2019-09-02 18:47:56 +0300

    TE-3929 CR edits [skip ci]

--- src/Pyz/Client/ExampleProductSalePage/Plugin/Elasticsearch/Query/SaleSearchQueryPlugin.php
+++ src/Pyz/Client/ExampleProductSalePage/Plugin/Elasticsearch/Query/SaleSearchQueryPlugin.php
@@ -61,9 +61,7 @@ class SaleSearchQueryPlugin extends AbstractPlugin implements QueryInterface, Se
     public function getSearchContext(): SearchContextTransfer
     {
         $searchContextTransfer = new SearchContextTransfer();
-        $searchContextTransfer->setElasticsearchContext(
-            $this->createElasticsearchSearchContextTransfer()
-        );
+        $searchContextTransfer = $this->expandWithVendorContext($searchContextTransfer);
 
         return $searchContextTransfer;
     }
@@ -168,13 +166,16 @@ class SaleSearchQueryPlugin extends AbstractPlugin implements QueryInterface, Se
     }
 
     /**
-     * @return \Generated\Shared\Transfer\ElasticsearchSearchContextTransfer
+     * @param \Generated\Shared\Transfer\SearchContextTransfer $searchContextTransfer
+     *
+     * @return \Generated\Shared\Transfer\SearchContextTransfer
      */
-    protected function createElasticsearchSearchContextTransfer(): ElasticsearchSearchContextTransfer
+    protected function expandWithVendorContext(SearchContextTransfer $searchContextTransfer): SearchContextTransfer
     {
         $elasticsearchSearchContextTransfer = new ElasticsearchSearchContextTransfer();
         $elasticsearchSearchContextTransfer->setSourceName(static::SOURCE_NAME);
+        $searchContextTransfer->setElasticsearchContext($elasticsearchSearchContextTransfer);
 
-        return $elasticsearchSearchContextTransfer;
+        return $searchContextTransfer;
     }
 }
