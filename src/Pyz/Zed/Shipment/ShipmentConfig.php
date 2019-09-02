685853f9d0894156ce107c831ed310f53c90f1f5
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-09-02 19:52:54 +0300

    CC-6033. BE refactoring after Karoly review. Spryker-Spryker. Part 2.
    * Updated Spryker-Shop modules: CustomerPage.
    * Updated Spryker-Core modules: Oms, Sales, SalesOrderThresholdGui.
    * Updated Project modules: Shipment.

---
+++ src/Pyz/Zed/Shipment/ShipmentConfig.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Shared\Shipment;
+
+use Spryker\Zed\Shipment\ShipmentConfig as SprykerShipmentConfig;
+
+class ShipmentConfig extends SprykerShipmentConfig
+{
+   /**
+     * @return bool
+     */
+    public function isMultiShipmentSelectionEnabled(): bool
+    {
+        return true;
+    }
+}
