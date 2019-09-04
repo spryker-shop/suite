c51e893d5f31d5acaf0927ff248b2cd620f1047d
Artem Chub <artem.chub@spryker.com>
2019-09-04 16:20:35 +0000

    PS-10030: Created QuoteApprovalconfig

---
+++ src/Pyz/Shared/QuoteApproval/QuoteApprovalConfig.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Shared\QuoteApproval;
+
+use Generated\Shared\Transfer\QuoteTransfer;
+use Spryker\Zed\QuoteApproval\QuoteApprovalConfig as SprykerQuoteApprovalConfig;
+
+class QuoteApprovalConfig extends SprykerQuoteApprovalConfig
+{
+    /**
+     * @return array
+     */
+    public function getRequiredQuoteFields(): array
+    {
+        return [
+            QuoteTransfer::BILLING_ADDRESS,
+            QuoteTransfer::SHIPPING_ADDRESS,
+            QuoteTransfer::PAYMENTS,
+            QuoteTransfer::SHIPMENT,
+        ];
+    }
+}
