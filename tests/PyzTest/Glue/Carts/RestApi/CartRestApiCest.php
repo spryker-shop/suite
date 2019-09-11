5cbc2ab3379514a5a085deefbe53fb697bdbf466
voitov <tigran.voitov@spryker.com>
2019-09-11 18:34:12 +0300

    glue-7922 TL CR fixes

--- tests/PyzTest/Glue/Carts/RestApi/CartRestApiCest.php
+++ tests/PyzTest/Glue/Carts/RestApi/CartRestApiCest.php
@@ -26,7 +26,7 @@ use Spryker\Glue\CustomersRestApi\CustomersRestApiConfig;
  */
 class CartRestApiCest
 {
-    protected const ANONYMOUS_CUSTOMER_REFERENCE = '666';
+    protected const VALUE_FOR_ANONYMOUS = '666';
     protected const TEST_PASSWORD = 'test password';
     protected const NON_EXISTING_CUSTOMER_EMAIL = 'test_non_existing_email@spryker.com';
     protected const NON_EXISTING_CART_UUID = 'non-existing-cart-uuid';
@@ -55,17 +55,9 @@ class CartRestApiCest
      */
     public function requestGuestCartBecomesCustomerCartAfterCustomerLogin(CartsApiTester $I): void
     {
-        $customerEmail = sprintf('%s@test.local.com', uniqid('glue-', true));
+        $this->requestCustomerLoginWithXAnonymousCustomerUniqueIdHeader($I);
 
-        $this->requestCustomerCreate($I, $customerEmail);
-
-        $cartUuid = $this->requestCreateGuestCart($I);
-
-        $I->amUnauthorizedGlueUser(static::ANONYMOUS_CUSTOMER_REFERENCE);
-
-        $this->requestCustomerLogin($I, $customerEmail);
-
-        $this->requestFindCartByUuid($I, $cartUuid);
+        $this->requestFindCartByUuid($I, $this->fixtures->getQuoteTransfer()->getUuid());
 
         $this->requestGuestCartCollectionIsEmpty($I);
     }
@@ -77,341 +69,13 @@ class CartRestApiCest
      *
      * @return void
      */
-    public function requestCustomerCreateWithoutEmail(CartsApiTester $I): void
+    protected function requestCustomerLoginWithXAnonymousCustomerUniqueIdHeader(CartsApiTester $I): void
     {
-        // Arrange
-        $customerTransfer = $I->createCustomerTransfer('', '');
+        $I->amUnauthorizedGlueUser(static::VALUE_FOR_ANONYMOUS);
 
-        // Act
-        $I->sendPOST(CustomersRestApiConfig::RESOURCE_CUSTOMERS, [
-            'data' => [
-                'type' => CustomersRestApiConfig::RESOURCE_CUSTOMERS,
-                'attributes' => [
-                    'salutation' => $customerTransfer->getSalutation(),
-                    'firstName' => $customerTransfer->getFirstName(),
-                    'lastName' => $customerTransfer->getLastName(),
-                    'password' => $customerTransfer->getNewPassword(),
-                    'confirmPassword' => $customerTransfer->getNewPassword(),
-                    'acceptedTerms' => true,
-                ],
-            ],
-        ]);
+        $token = $I->haveAuthorizationToGlue($this->fixtures->getCustomerTransfer())['accessToken'];
 
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCustomerCreateWithoutPassword(CartsApiTester $I): void
-    {
-        // Arrange
-        $customerTransfer = $I->createCustomerTransfer(static::NON_EXISTING_CUSTOMER_EMAIL, '');
-
-        // Act
-        $I->sendPOST(CustomersRestApiConfig::RESOURCE_CUSTOMERS, [
-            'data' => [
-                'type' => CustomersRestApiConfig::RESOURCE_CUSTOMERS,
-                'attributes' => [
-                    'salutation' => $customerTransfer->getSalutation(),
-                    'firstName' => $customerTransfer->getFirstName(),
-                    'lastName' => $customerTransfer->getLastName(),
-                    'email' => $customerTransfer->getEmail(),
-                    'confirmPassword' => $customerTransfer->getNewPassword(),
-                    'acceptedTerms' => true,
-                ],
-            ],
-        ]);
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCustomerCreateWithNonAcceptedTerms(CartsApiTester $I): void
-    {
-        // Arrange
-        $customerTransfer = $I->createCustomerTransfer('', '');
-
-        // Act
-        $I->sendPOST(CustomersRestApiConfig::RESOURCE_CUSTOMERS, [
-            'data' => [
-                'type' => CustomersRestApiConfig::RESOURCE_CUSTOMERS,
-                'attributes' => [
-                    'salutation' => $customerTransfer->getSalutation(),
-                    'firstName' => $customerTransfer->getFirstName(),
-                    'lastName' => $customerTransfer->getLastName(),
-                    'email' => $customerTransfer->getEmail(),
-                    'password' => $customerTransfer->getNewPassword(),
-                    'confirmPassword' => $customerTransfer->getNewPassword(),
-                    'acceptedTerms' => false,
-                ],
-            ],
-        ]);
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCreateGuestCartWithoutAnonymousCustomerUniqueId(CartsApiTester $I): void
-    {
-        // Act
-        $I->sendPOST(
-            CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-            [
-                'data' => [
-                    'type' => CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-                    'attributes' => [
-                        'sku' => $this->fixtures->getProductConcreteTransfer()->getSku(),
-                        'quantity' => 1,
-                    ],
-                ],
-            ]
-        );
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::BAD_REQUEST);
-        $I->seeResponseIsJson();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCreateGuestCartWithoutSku(CartsApiTester $I): void
-    {
-        $I->amUnauthorizedGlueUser(static::ANONYMOUS_CUSTOMER_REFERENCE);
-
-        // Act
-        $I->sendPOST(
-            CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-            [
-                'data' => [
-                    'type' => CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-                    'attributes' => [
-                        'quantity' => 1,
-                    ],
-                ],
-            ]
-        );
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
-        $I->seeResponseIsJson();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCreateGuestCartWithoutQuantity(CartsApiTester $I): void
-    {
-        $I->amUnauthorizedGlueUser(static::ANONYMOUS_CUSTOMER_REFERENCE);
-
-        // Act
-        $I->sendPOST(
-            CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-            [
-                'data' => [
-                    'type' => CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-                    'attributes' => [
-                        'sku' => $this->fixtures->getProductConcreteTransfer()->getSku(),
-                    ],
-                ],
-            ]
-        );
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNPROCESSABLE_ENTITY);
-        $I->seeResponseIsJson();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    public function requestCustomerLoginWithNonExistingCustomer(CartsApiTester $I): void
-    {
-        // Arrange
-        $customerTransfer = (new CustomerTransfer())
-            ->setEmail(static::NON_EXISTING_CUSTOMER_EMAIL)
-            ->setNewPassword('');
-
-        // Act
-        $I->haveAuthorizationToGlue($customerTransfer);
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::UNAUTHORIZED);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return void
-     */
-    protected function requestFindCartByUuidWithNonExistingCartUuid(CartsApiTester $I): void
-    {
-        //act
-        $I->sendGET(
-            $I->formatUrl(
-                'carts/{cartUuid}',
-                [
-                    'cartUuid' => static::NON_EXISTING_CART_UUID,
-                ]
-            )
-        );
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::NOT_FOUND);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     *
-     * @return string
-     */
-    protected function requestCreateGuestCart(CartsApiTester $I): string
-    {
-        // Arrange
-        $I->amUnauthorizedGlueUser(static::ANONYMOUS_CUSTOMER_REFERENCE);
-
-        // Act
-        $I->sendPOST(
-            CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-            [
-                'data' => [
-                    'type' => CartsRestApiConfig::RESOURCE_GUEST_CARTS_ITEMS,
-                    'attributes' => [
-                        'sku' => $this->fixtures->getProductConcreteTransfer()->getSku(),
-                        'quantity' => 1,
-                    ],
-                ],
-            ]
-        );
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::CREATED);
-        $I->seeResponseIsJson();
-
-        $I->amSure('Returned resource is of type guest-carts')
-            ->whenI()
-            ->seeResponseDataContainsSingleResourceOfType('guest-carts');
-
-        return $I->grabDataFromResponseByJsonPath('$.data')[0]['id'];
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     * @param string $customerEmail
-     *
-     * @return string
-     */
-    protected function requestCustomerCreate(CartsApiTester $I, string $customerEmail): string
-    {
-        // Arrange
-        $customerTransfer = $I->createCustomerTransfer($customerEmail, static::TEST_PASSWORD);
-
-        // Act
-        $I->sendPOST(CustomersRestApiConfig::RESOURCE_CUSTOMERS, [
-            'data' => [
-                'type' => CustomersRestApiConfig::RESOURCE_CUSTOMERS,
-                'attributes' => [
-                    'salutation' => $customerTransfer->getSalutation(),
-                    'firstName' => $customerTransfer->getFirstName(),
-                    'lastName' => $customerTransfer->getLastName(),
-                    'email' => $customerTransfer->getEmail(),
-                    'password' => $customerTransfer->getNewPassword(),
-                    'confirmPassword' => $customerTransfer->getNewPassword(),
-                    'acceptedTerms' => true,
-                ],
-            ],
-        ]);
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::CREATED);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-
-        $I->amSure('Returned resource is of type customers')
-            ->whenI()
-            ->seeResponseDataContainsSingleResourceOfType(CustomersRestApiConfig::RESOURCE_CUSTOMERS);
-
-        return $I->grabDataFromResponseByJsonPath('$.data[0].id');
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
-     * @param string $customerEmail
-     *
-     * @return string
-     */
-    protected function requestCustomerLogin(CartsApiTester $I, string $customerEmail): string
-    {
-        // Arrange
-        $customerTransfer = (new CustomerTransfer())
-            ->setEmail($customerEmail)
-            ->setNewPassword(static::TEST_PASSWORD);
-
-        $token = $I->haveAuthorizationToGlue($customerTransfer)['accessToken'];
-
-        // Act
         $I->amAuthorizedGlueUser($token);
-
-        //assert
-        $I->seeResponseCodeIs(HttpCode::CREATED);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-
-        $I->amSure(sprintf('Returned resource is of type %s', AuthRestApiConfig::RESOURCE_ACCESS_TOKENS))
-            ->whenI()
-            ->seeResponseDataContainsSingleResourceOfType(AuthRestApiConfig::RESOURCE_ACCESS_TOKENS);
-
-        return $token;
     }
 
     /**
@@ -480,6 +144,6 @@ class CartRestApiCest
         $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseIsJson();
         $I->seeResponseMatchesOpenApiSchema();
-        $I->seeResponseDataIsEmpty();
+        $I->dontSeeResponseMatchesJsonPath('$.data[*]');
     }
 }
--- tests/PyzTest/Glue/Carts/RestApi/CartsRestApiFixtures.php
+++ tests/PyzTest/Glue/Carts/RestApi/CartsRestApiFixtures.php
@@ -7,10 +7,12 @@
 
 namespace PyzTest\Glue\Carts\RestApi;
 
-use Generated\Shared\Transfer\MoneyValueTransfer;
-use Generated\Shared\Transfer\PriceProductTransfer;
+use Generated\Shared\Transfer\CustomerTransfer;
+use Generated\Shared\Transfer\ItemTransfer;
 use Generated\Shared\Transfer\ProductConcreteTransfer;
-use Generated\Shared\Transfer\StockProductTransfer;
+use Generated\Shared\Transfer\QuoteTransfer;
+use Generated\Shared\Transfer\StoreTransfer;
+use Generated\Shared\Transfer\TotalsTransfer;
 use PyzTest\Glue\Carts\CartsApiTester;
 use SprykerTest\Shared\Testify\Fixtures\FixturesBuilderInterface;
 use SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface;
@@ -27,10 +29,23 @@ use SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface;
  */
 class CartsRestApiFixtures implements FixturesBuilderInterface, FixturesContainerInterface
 {
+    protected const TEST_PASSWORD = 'test password';
+    protected const ANONYMOUS_CUSTOMER_REFERENCE = 'anonymous:666';
+
     /**
      * @var \Generated\Shared\Transfer\ProductConcreteTransfer
      */
     protected $productConcreteTransfer;
+
+    /**
+     * @var \Generated\Shared\Transfer\CustomerTransfer
+     */
+    protected $customerTransfer;
+
+    /**
+     * @var \Generated\Shared\Transfer\QuoteTransfer
+     */
+    protected $quoteTransfer;
 
     /**
      * @return \Generated\Shared\Transfer\ProductConcreteTransfer
@@ -41,6 +56,22 @@ class CartsRestApiFixtures implements FixturesBuilderInterface, FixturesContaine
     }
 
     /**
+     * @return \Generated\Shared\Transfer\CustomerTransfer
+     */
+    public function getCustomerTransfer(): CustomerTransfer
+    {
+        return $this->customerTransfer;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function getQuoteTransfer(): QuoteTransfer
+    {
+        return $this->quoteTransfer;
+    }
+
+    /**
      * @param \PyzTest\Glue\Carts\CartsApiTester $I
      *
      * @return \SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface
@@ -48,6 +79,8 @@ class CartsRestApiFixtures implements FixturesBuilderInterface, FixturesContaine
     public function buildFixtures(CartsApiTester $I): FixturesContainerInterface
     {
         $this->createProduct($I);
+        $this->createCustomer($I);
+        $this->createQuote($I);
 
         return $this;
     }
@@ -60,19 +93,48 @@ class CartsRestApiFixtures implements FixturesBuilderInterface, FixturesContaine
     protected function createProduct(CartsApiTester $I): void
     {
         $this->productConcreteTransfer = $I->haveFullProduct();
-        $I->haveProductInStock([StockProductTransfer::SKU => $this->productConcreteTransfer->getSku()]);
+    }
 
-        $priceProductOverride = [
-            PriceProductTransfer::ID_PRICE_PRODUCT => $this->productConcreteTransfer->getFkProductAbstract(),
-            PriceProductTransfer::SKU_PRODUCT_ABSTRACT => $this->productConcreteTransfer->getAbstractSku(),
-            PriceProductTransfer::ID_PRODUCT => $this->productConcreteTransfer->getIdProductConcrete(),
-            PriceProductTransfer::PRICE_TYPE_NAME => 'DEFAULT',
-            PriceProductTransfer::MONEY_VALUE => [
-                MoneyValueTransfer::NET_AMOUNT => 666,
-                MoneyValueTransfer::GROSS_AMOUNT => 999,
+    /**
+     * @param \PyzTest\Glue\Carts\CartsApiTester $I
+     *
+     * @return void
+     */
+    protected function createCustomer(CartsApiTester $I): void
+    {
+        $customerTransfer = $I->haveCustomer([
+            'password' => static::TEST_PASSWORD,
+        ]);
+
+        $customerTransfer->setNewPassword(static::TEST_PASSWORD);
+
+        $this->customerTransfer = $customerTransfer;
+    }
+
+    /**
+     * @param CartsApiTester $I
+     *
+     * @return void
+     */
+    protected function createQuote(CartsApiTester $I): void
+    {
+        $totalsTransfer = new TotalsTransfer();
+        $totalsTransfer->setPriceToPay(random_int(1000, 10000));
+
+        $this->quoteTransfer = $I->havePersistentQuote([
+            QuoteTransfer::CUSTOMER => (new CustomerTransfer())->setCustomerReference(static::ANONYMOUS_CUSTOMER_REFERENCE),
+            QuoteTransfer::TOTALS => $totalsTransfer,
+            QuoteTransfer::ITEMS => [
+                [
+                    ItemTransfer::SKU => $this->productConcreteTransfer->getSku(),
+                    ItemTransfer::GROUP_KEY => $this->productConcreteTransfer->getSku(),
+                    ItemTransfer::ABSTRACT_SKU => $this->productConcreteTransfer->getAbstractSku(),
+                    ItemTransfer::ID => $this->productConcreteTransfer->getIdProductConcrete(),
+                    ItemTransfer::UNIT_PRICE => random_int(100, 1000),
+                    ItemTransfer::QUANTITY => 5,
+                ]
             ],
-        ];
-
-        $I->havePriceProduct($priceProductOverride);
+            QuoteTransfer::STORE => [StoreTransfer::NAME => 'DE'],
+        ]);
     }
 }
