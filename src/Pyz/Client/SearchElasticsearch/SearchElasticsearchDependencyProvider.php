d7a65a17cd9f2a25291e8379717f7bc3b2597347
Pavlo Asaulenko <asaulenko.pavel@gmail.com>
2019-10-08 19:09:45 +0300

    TE-3980 Temp commit

--- src/Pyz/Client/SearchElasticsearch/SearchElasticsearchDependencyProvider.php
+++ src/Pyz/Client/SearchElasticsearch/SearchElasticsearchDependencyProvider.php
@@ -7,46 +7,22 @@
 
 namespace Pyz\Client\SearchElasticsearch;
 
-use Spryker\Client\Catalog\Plugin\Config\FacetConfigPlugin;
-use Spryker\Client\Catalog\Plugin\Config\PaginationConfigPlugin;
-use Spryker\Client\Catalog\Plugin\Config\SortConfigPlugin;
+use Spryker\Catalog\src\Spryker\Client\Catalog\Plugin\Config\CatalogSearchConfigBuilderPlugin;
 use Spryker\Client\Kernel\Container;
 use Spryker\Client\ProductSearchConfigStorage\Plugin\Config\ProductSearchConfigExpanderPlugin;
 use Spryker\Client\SearchElasticsearch\SearchElasticsearchDependencyProvider as SprykerSearchElasticsearchDependencyProvider;
-use Spryker\Client\SearchExtension\Dependency\Plugin\FacetConfigPluginInterface;
-use Spryker\Client\SearchExtension\Dependency\Plugin\PaginationConfigPluginInterface;
-use Spryker\Client\SearchExtension\Dependency\Plugin\SortConfigPluginInterface;
+use Spryker\Client\SearchExtension\Dependency\Plugin\SearchConfigBuilderPluginInterface;
 
 class SearchElasticsearchDependencyProvider extends SprykerSearchElasticsearchDependencyProvider
 {
     /**
      * @param \Spryker\Client\Kernel\Container $container
      *
-     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\FacetConfigPluginInterface|null
+     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\SearchConfigBuilderPluginInterface|null
      */
-    protected function getFacetSearchConfigBuilderPlugin(Container $container): ?FacetConfigPluginInterface
+    protected function getSearchConfigBuilderPlugin(Container $container): ?SearchConfigBuilderPluginInterface
     {
-        return new FacetConfigPlugin();
-    }
-
-    /**
-     * @param \Spryker\Client\Kernel\Container $container
-     *
-     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\PaginationConfigPluginInterface|null
-     */
-    protected function getPaginationSearchConfigBuilderPlugin(Container $container): ?PaginationConfigPluginInterface
-    {
-        return new PaginationConfigPlugin();
-    }
-
-    /**
-     * @param \Spryker\Client\Kernel\Container $container
-     *
-     * @return \Spryker\Client\SearchExtension\Dependency\Plugin\SortConfigPluginInterface|null
-     */
-    protected function getSortSearchConfigBuilderPlugin(Container $container): ?SortConfigPluginInterface
-    {
-        return new SortConfigPlugin();
+        return new CatalogSearchConfigBuilderPlugin();
     }
 
     /**
