5e42f562a5e3d49eb4a4f27726deec986938ffa9
Oleksandr Myrnyi <oleksandr.myrnyi@spryker.com>
2019-10-18 14:32:55 +0300

    SC-2256: Docker Travis CI perfomance

--- tests/PyzTest/Glue/Products/RestApi/ProductsRestApiFixtures.php
+++ tests/PyzTest/Glue/Products/RestApi/ProductsRestApiFixtures.php
@@ -7,9 +7,7 @@
 
 namespace PyzTest\Glue\Products\RestApi;
 
-use Generated\Shared\Transfer\MoneyValueTransfer;
 use Generated\Shared\Transfer\PriceProductTransfer;
-use Generated\Shared\Transfer\PriceTypeTransfer;
 use Generated\Shared\Transfer\ProductConcreteTransfer;
 use PyzTest\Glue\Products\ProductsApiTester;
 use SprykerTest\Shared\Testify\Fixtures\FixturesBuilderInterface;
@@ -62,7 +60,6 @@ class ProductsRestApiFixtures implements FixturesBuilderInterface, FixturesConta
     public function buildFixtures(ProductsApiTester $I): FixturesContainerInterface
     {
         $this->createProductConcrete($I);
-        $this->createPriceProduct($I);
 
         return $this;
     }
@@ -75,28 +72,5 @@ class ProductsRestApiFixtures implements FixturesBuilderInterface, FixturesConta
     protected function createProductConcrete(ProductsApiTester $I): void
     {
         $this->productConcreteTransfer = $I->haveFullProduct();
-    }
-
-    /**
-     * @param \PyzTest\Glue\Products\ProductsApiTester $I
-     *
-     * @return void
-     */
-    protected function createPriceProduct(ProductsApiTester $I): void
-    {
-        $priceTypeTransfer = $I->havePriceType([PriceTypeTransfer::NAME => 'DEFAULT']);
-        $currencyTransfer = $I->getLocator()->currency()->facade()->getDefaultCurrencyForCurrentStore();
-        $this->priceProductTransfer = $I->havePriceProduct([
-            PriceProductTransfer::ID_PRODUCT => $this->productConcreteTransfer->getIdProductConcrete(),
-            PriceProductTransfer::SKU_PRODUCT => $this->productConcreteTransfer->getSku(),
-            PriceProductTransfer::ID_PRICE_PRODUCT => $this->productConcreteTransfer->getFkProductAbstract(),
-            PriceProductTransfer::SKU_PRODUCT_ABSTRACT => $this->productConcreteTransfer->getAbstractSku(),
-            PriceProductTransfer::PRICE_TYPE => $priceTypeTransfer,
-            PriceProductTransfer::MONEY_VALUE => [
-                MoneyValueTransfer::NET_AMOUNT => 100,
-                MoneyValueTransfer::GROSS_AMOUNT => 100,
-                MoneyValueTransfer::CURRENCY => $currencyTransfer,
-            ],
-        ]);
     }
 }
