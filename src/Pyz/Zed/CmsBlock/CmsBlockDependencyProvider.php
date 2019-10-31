ca93d89224e0cab29ac3ec28a5fe7f8c00f26981
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2019-10-31 11:20:09 +0200

    CMS-2642: fixed interface

--- src/Pyz/Zed/CmsBlock/CmsBlockDependencyProvider.php
+++ src/Pyz/Zed/CmsBlock/CmsBlockDependencyProvider.php
@@ -14,7 +14,7 @@ use Spryker\Zed\CmsBlockProductConnector\Communication\Plugin\CmsBlockProductAbs
 class CmsBlockDependencyProvider extends CmsBlockCmsBlockDependencyProvider
 {
     /**
-     * @return \Spryker\Zed\CmsBlock\Communication\Plugin\CmsBlockUpdatePluginInterface[]
+     * @return \Spryker\Zed\CmsBlockExtension\Dependency\Plugin\CmsBlockUpdatePluginInterface[]
      */
     protected function getCmsBlockUpdatePlugins(): array
     {
