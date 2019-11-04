01ae8a7ecafc59a487e2a184c95939f4ae58e667
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2019-11-04 06:58:06 +0000

    PS-10487: Added form widget.

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -31,6 +31,7 @@ use SprykerShop\Yves\CompanyPage\Plugin\ShopApplication\CompanyBusinessUnitContr
 use SprykerShop\Yves\CompanyPage\Plugin\ShopApplication\CompanyUserRestrictionHandlerPlugin;
 use SprykerShop\Yves\CompanyWidget\Widget\CompanyBusinessUnitAddressWidget;
 use SprykerShop\Yves\CompanyWidget\Widget\CompanyMenuItemWidget;
+use SprykerShop\Yves\ConfigurableBundleCartNoteWidget\Widget\ConfigurableBundleCartNoteFormWidget;
 use SprykerShop\Yves\ConfigurableBundleWidget\Widget\QuoteConfiguredBundleWidget;
 use SprykerShop\Yves\CurrencyWidget\Widget\CurrencyWidget;
 use SprykerShop\Yves\CustomerPage\Widget\CustomerNavigationWidget;
@@ -211,6 +212,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             CommentThreadWidget::class,
             QuoteConfiguredBundleWidget::class,
             OrderConfiguredBundleWidget::class,
+            ConfigurableBundleCartNoteFormWidget::class,
         ];
     }
 
