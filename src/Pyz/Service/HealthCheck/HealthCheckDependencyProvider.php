abcbd4c212b12f44f21da7835f6933ba2fff2e5a
Oleksandr <oleksandr.myrnyi@spryker.com>
2019-11-13 12:58:50 +0000

    SC-1262: HealthCheck Yves session plugins

--- src/Pyz/Service/HealthCheck/HealthCheckDependencyProvider.php
+++ src/Pyz/Service/HealthCheck/HealthCheckDependencyProvider.php
@@ -10,6 +10,7 @@ namespace Pyz\Service\HealthCheck;
 use Spryker\Service\HealthCheck\HealthCheckDependencyProvider as SprykerHealthCheckDependencyProvider;
 use Spryker\Service\Propel\Plugin\HealthCheck\DatabaseHealthCheckPlugin;
 use Spryker\Service\Search\Plugin\HealthCheck\SearchHealthCheckPlugin;
+use Spryker\Service\Session\Plugin\HealthCheck\YvesSessionHealthCheckPlugin;
 use Spryker\Service\Session\Plugin\HealthCheck\ZedSessionHealthCheckPlugin;
 use Spryker\Service\Storage\Plugin\HealthCheck\KeyValueStoreHealthCheckPlugin;
 use Spryker\Service\ZedRequest\Plugin\HealthCheck\ZedRequestHealthCheckPlugin;
@@ -24,6 +25,8 @@ class HealthCheckDependencyProvider extends SprykerHealthCheckDependencyProvider
         return [
             new KeyValueStoreHealthCheckPlugin(),
             new SearchHealthCheckPlugin(),
+            new YvesSessionHealthCheckPlugin(),
+            new ZedRequestHealthCheckPlugin(),
         ];
     }
 
