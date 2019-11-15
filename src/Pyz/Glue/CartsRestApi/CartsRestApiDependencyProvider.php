3ecf5311f3e295fb06023f03a320f9b2069624ce
Denis Gnusov <denis.gnusov@spryker.com>
2019-11-15 16:31:52 +0200

    GLUE-7301 Product options in cart items

--- src/Pyz/Glue/CartsRestApi/CartsRestApiDependencyProvider.php
+++ src/Pyz/Glue/CartsRestApi/CartsRestApiDependencyProvider.php
@@ -9,6 +9,7 @@ namespace Pyz\Glue\CartsRestApi;
 
 use Spryker\Glue\CartsRestApi\CartsRestApiDependencyProvider as SprykerCartsRestApiDependencyProvider;
 use Spryker\Glue\CompanyUsersRestApi\Plugin\CartsRestApi\CompanyUserCustomerExpanderPlugin;
+use Spryker\Glue\ProductOptionsRestApi\Plugin\CartsRestApiExtension\CartItemToRestCartItemsAttributesMapperPlugin;
 
 class CartsRestApiDependencyProvider extends SprykerCartsRestApiDependencyProvider
 {
@@ -19,6 +20,16 @@ class CartsRestApiDependencyProvider extends SprykerCartsRestApiDependencyProvid
     {
         return [
             new CompanyUserCustomerExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \Spryker\Glue\CartsRestApiExtension\Dependency\Plugin\RestCartItemsAttributesMapperPluginInterface[]
+     */
+    protected function getItemToRestCartItemsAttributesMapperPlugins(): array
+    {
+        return [
+            new CartItemToRestCartItemsAttributesMapperPlugin(),
         ];
     }
 }
