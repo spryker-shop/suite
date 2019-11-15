b678ba641d65e756dd410211f5e46c7f5a2496e2
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-11-15 19:30:15 +0200

    MP-1491: moved functionality from SalesMerchantConnector to MerchantSalesOrder module

--- src/Pyz/Zed/MerchantSalesOrder/Communication/Plugin/MerchantOrderSaverPlugin.php
+++ src/Pyz/Zed/MerchantSalesOrder/Communication/Plugin/MerchantOrderSaverPlugin.php
@@ -5,23 +5,23 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Pyz\Zed\SalesMerchantConnector\Communication\Plugin;
+namespace Pyz\Zed\MerchantSalesOrder\Communication\Plugin;
 
+use Generated\Shared\Transfer\MerchantSalesOrderTransfer;
 use Generated\Shared\Transfer\OfferTransfer;
 use Generated\Shared\Transfer\QuoteTransfer;
-use Generated\Shared\Transfer\SalesOrderMerchantTransfer;
 use Generated\Shared\Transfer\SaveOrderTransfer;
 use Spryker\Zed\CheckoutExtension\Dependency\Plugin\CheckoutDoSaveOrderInterface;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 
 /**
- * @method \Spryker\Zed\SalesMerchantConnector\Business\SalesMerchantConnectorFacadeInterface getFacade()
+ * @method \Spryker\Zed\MerchantSalesOrder\Business\MerchantSalesOrderFacadeInterface getFacade()
  */
 class MerchantOrderSaverPlugin extends AbstractPlugin implements CheckoutDoSaveOrderInterface
 {
     /**
      * {@inheritDoc}
-     * - For every item with merchant offer save related data to `spy_sales_order_merchant`.
+     * - For every item with merchant offer save related data to `spy_merchant_sales_order`.
      *
      * @api
      *
@@ -34,12 +34,13 @@ class MerchantOrderSaverPlugin extends AbstractPlugin implements CheckoutDoSaveO
     {
         foreach ($quoteTransfer->getItems() as $itemTransfer) {
             $offerTransfer = $itemTransfer->getOffer();
+
             if (!$offerTransfer) {
                 continue;
             }
 
-            $this->getFacade()->createSalesOrderMerchant(
-                $this->createSalesOrderMerchantTransfer($offerTransfer, $saveOrderTransfer)
+            $this->getFacade()->createMerchantSalesOrder(
+                $this->createMerchantSalesOrderTransfer($offerTransfer, $saveOrderTransfer)
             );
         }
     }
@@ -48,17 +49,17 @@ class MerchantOrderSaverPlugin extends AbstractPlugin implements CheckoutDoSaveO
      * @param \Generated\Shared\Transfer\OfferTransfer $offerTransfer
      * @param \Generated\Shared\Transfer\SaveOrderTransfer $saveOrderTransfer
      *
-     * @return \Generated\Shared\Transfer\SalesOrderMerchantTransfer
+     * @return \Generated\Shared\Transfer\MerchantSalesOrderTransfer
      */
-    protected function createSalesOrderMerchantTransfer(
+    protected function createMerchantSalesOrderTransfer(
         OfferTransfer $offerTransfer,
         SaveOrderTransfer $saveOrderTransfer
-    ): SalesOrderMerchantTransfer {
-        $salesOrderMerchantSaveTransfer = new SalesOrderMerchantTransfer();
-        $salesOrderMerchantSaveTransfer->setMerchantReference($offerTransfer->getMerchantReference());
-        $salesOrderMerchantSaveTransfer->setFkSalesOrder($saveOrderTransfer->getIdSalesOrder());
-        $salesOrderMerchantSaveTransfer->setOrderReference($saveOrderTransfer->getOrderReference());
+    ): MerchantSalesOrderTransfer {
+        $merchantSalesOrderTransfer = new MerchantSalesOrderTransfer();
+        $merchantSalesOrderTransfer->setMerchantReference($offerTransfer->getMerchantReference());
+        $merchantSalesOrderTransfer->setFkSalesOrder($saveOrderTransfer->getIdSalesOrder());
+        $merchantSalesOrderTransfer->setOrderReference($saveOrderTransfer->getOrderReference());
 
-        return $salesOrderMerchantSaveTransfer;
+        return $merchantSalesOrderTransfer;
     }
 }
