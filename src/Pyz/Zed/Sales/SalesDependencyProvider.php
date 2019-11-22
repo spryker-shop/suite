e3a0385d11b8885f693516c862bb14f9c7e69e2a
Aleksey Kotsuba <aleksey.kotsuba@spryker.com>
2019-11-22 15:31:24 +0000

    PS-10484: Updated widget name.

--- src/Pyz/Zed/Sales/SalesDependencyProvider.php
+++ src/Pyz/Zed/Sales/SalesDependencyProvider.php
@@ -25,7 +25,7 @@ use Spryker\Zed\ProductPackagingUnit\Communication\Plugin\Sales\AmountSalesUnitH
 use Spryker\Zed\ProductPackagingUnit\Communication\Plugin\SalesExtension\AmountSalesUnitOrderItemExpanderPreSavePlugin;
 use Spryker\Zed\ProductPackagingUnit\Communication\Plugin\SalesExtension\ProductPackagingUnitOrderItemExpanderPreSavePlugin;
 use Spryker\Zed\Sales\SalesDependencyProvider as SprykerSalesDependencyProvider;
-use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfigurableBundleItemPreTransformerPlugin;
+use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfiguredBundleItemPreTransformerPlugin;
 use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfiguredBundleOrderExpanderPlugin;
 use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfiguredBundlesOrderPostSavePlugin;
 use Spryker\Zed\SalesMerchantConnector\Communication\Plugin\OrderItemReferenceExpanderPreSavePlugin;
@@ -124,7 +124,7 @@ class SalesDependencyProvider extends SprykerSalesDependencyProvider
     protected function getItemPreTransformerPlugins(): array
     {
         return [
-            new ConfigurableBundleItemPreTransformerPlugin(),
+            new ConfiguredBundleItemPreTransformerPlugin(),
         ];
     }
 }
