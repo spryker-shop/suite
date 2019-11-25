51bef8217995c7ce4aa7d8d122f39238685f6495
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-11-25 19:02:20 +0200

    CC-7344. Product abstract card. BE part.
    * Created new `ProductColorGroupWidget` extended from `ProductGroupWidget`.

---
+++ src/Pyz/Yves/ProductGroupWidget/ProductGroupWidgetDependencyProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * Copyright © 2016-present Spryker Systems GmbH. All rights reserved.
+ * Use of this software requires acceptance of the Evaluation License Agreement. See LICENSE file.
+ */
+
+namespace Pyz\Yves\ProductGroupWidget;
+
+use SprykerShop\Yves\CartPage\Plugin\ProductColorGroupWidget\AddToCartUrlProductViewExpanderPlugin;
+use SprykerShop\Yves\ProductGroupWidget\ProductGroupWidgetDependencyProvider as SprykerShopProductGroupWidgetDependencyProvider;
+use SprykerShop\Yves\ProductReviewWidget\Plugin\ProductColorGroupWidget\ProductReviewSummaryProductViewExpanderPlugin;
+
+class ProductGroupWidgetDependencyProvider extends SprykerShopProductGroupWidgetDependencyProvider
+{
+    /**
+     * @return \SprykerShop\Yves\ProductColorGroupWidgetExtension\Dependency\Plugin\ProductViewExpanderPluginInterface[]
+     */
+    protected function getProductViewExpanderPlugins(): array
+    {
+        return [
+            new AddToCartUrlProductViewExpanderPlugin(),
+            new ProductReviewSummaryProductViewExpanderPlugin(),
+        ];
+    }
+}
