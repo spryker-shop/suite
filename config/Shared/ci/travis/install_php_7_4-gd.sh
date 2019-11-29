7f8ae23ee890f067624ea9810f0a0c3b2f340f14
Vladimir Voronin <vladimir.voronin@gmail.com>
2019-11-29 19:37:53 +0200

    Fix the source directory path

--- config/Shared/ci/travis/install_php_7_4-gd.sh
+++ config/Shared/ci/travis/install_php_7_4-gd.sh
@@ -2,7 +2,7 @@
 
 mkdir /home/travis/php74
 wget -O - https://www.php.net/distributions/php-7.4.0.tar.gz | tar xz --directory=/home/travis/php74 --strip-components=1 > /dev/null
-cd /home/travis/php74/php-7.4.0/ext/gd
+cd /home/travis/php74/ext/gd
 phpize
 ./configure --enable-gd
 make
