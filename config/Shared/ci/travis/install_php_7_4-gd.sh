d090404b26851c0a454a3458e53f4106dcf0203e
Vladimir Voronin <vladimir.voronin@gmail.com>
2019-12-03 17:24:32 +0200

    Fix PHP auth with the new Travis php configuration

--- config/Shared/ci/travis/install_php_7_4-gd.sh
+++ config/Shared/ci/travis/install_php_7_4-gd.sh
@@ -4,7 +4,7 @@ mkdir /home/travis/php74
 wget -O - https://www.php.net/distributions/php-7.4.0.tar.gz | tar xz --directory=/home/travis/php74 --strip-components=1 > /dev/null
 cd /home/travis/php74/ext/gd
 phpize
-./configure --enable-gd --enable-gd-native-ttf
+./configure --enable-gd --with-jpeg --with-freetype
 make
 sudo make install
 echo "extension = gd.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
