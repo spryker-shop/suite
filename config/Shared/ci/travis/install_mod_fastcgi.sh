46d2d6d8eeb6f7b288994b4c480850de4f194298
Vladimir Voronin <vladimir.voronin@gmail.com>
2019-12-05 17:16:44 +0200

    Install mod_fastcgi manually to Ubuntu bionic

---
+++ config/Shared/ci/travis/install_mod_fastcgi.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir /home/travis/fastcgi && cd /home/travis/fastcgi
+wget http://mirrors.kernel.org/ubuntu/pool/multiverse/liba/libapache-mod-fastcgi/libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
+sudo dpkg -i libapache2-mod-fastcgi_2.4.7~0910052141-1.2_amd64.deb
+sudo a2enmod fastcgi
+sudo service apache2 restart
