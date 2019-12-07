c47c4dbeac47ffe6124a4d4ee4bdc331a929d3cb
Dmitriy Svetlichniy <dmitriy.svetlichniy@spryker.com>
2019-12-07 12:23:42 +0200

    CC-7344. Product abstract card. BE part.
    * Updated `composer.lock` file.
    * Merge of other changes.

--- src/Pyz/Yves/ProductGroupWidget/ProductGroupWidgetDependencyProvider.php
+++ src/Pyz/Yves/ProductGroupWidget/ProductGroupWidgetDependencyProvider.php
@@ -7,14 +7,14 @@
 
 namespace Pyz\Yves\ProductGroupWidget;
 
-use SprykerShop\Yves\CartPage\Plugin\ProductColorGroupWidget\AddToCartUrlProductViewExpanderPlugin;
+use SprykerShop\Yves\CartPage\Plugin\ProductGroupWidget\AddToCartUrlProductViewExpanderPlugin;
 use SprykerShop\Yves\ProductGroupWidget\ProductGroupWidgetDependencyProvider as SprykerShopProductGroupWidgetDependencyProvider;
-use SprykerShop\Yves\ProductReviewWidget\Plugin\ProductColorGroupWidget\ProductReviewSummaryProductViewExpanderPlugin;
+use SprykerShop\Yves\ProductReviewWidget\Plugin\ProductGroupWidget\ProductReviewSummaryProductViewExpanderPlugin;
 
 class ProductGroupWidgetDependencyProvider extends SprykerShopProductGroupWidgetDependencyProvider
 {
     /**
-     * @return \SprykerShop\Yves\ProductColorGroupWidgetExtension\Dependency\Plugin\ProductViewExpanderPluginInterface[]
+     * @return \SprykerShop\Yves\ProductGroupWidgetExtension\Dependency\Plugin\ProductViewExpanderPluginInterface[]
      */
     protected function getProductViewExpanderPlugins(): array
     {
