40d215425ebd3d7697693b17a0249baad56f93b1
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-01-13 12:46:38 +0200

    Fix MySQL installation in Travis with the Ubuntu bionic packages

--- config/Shared/ci/travis/mysql/travis_mysql_5.7.sh
+++ config/Shared/ci/travis/mysql/travis_mysql_5.7.sh
@@ -4,11 +4,11 @@ sudo apt-get remove --purge mysql* > /dev/null
 sudo apt-get purge mysql* > /dev/null
 sudo apt-get autoremove > /dev/null
 sudo apt-get install libaio1 libmecab2 > /dev/null
-sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-common_5.7.28-1debian9_amd64.deb > /dev/null
-sudo dpkg -i mysql-common_5.7.28-1debian9_amd64.deb > /dev/null
-sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-client_5.7.28-1debian9_amd64.deb > /dev/null
-sudo dpkg -i mysql-community-client_5.7.28-1debian9_amd64.deb > /dev/null
-sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-client_5.7.28-1debian9_amd64.deb > /dev/null
-sudo dpkg -i mysql-client_5.7.28-1debian9_amd64.deb > /dev/null
-sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-server_5.7.28-1debian9_amd64.deb > /dev/null
-sudo dpkg -i mysql-community-server_5.7.28-1debian9_amd64.deb > /dev/null
+sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-common_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo dpkg -i mysql-common_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-client_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo dpkg -i mysql-community-client_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-client_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo dpkg -i mysql-client_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo wget https://downloads.mysql.com/archives/get/p/23/file/mysql-community-server_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
+sudo dpkg -i mysql-community-server_5.7.28-1ubuntu18.04_amd64.deb > /dev/null
