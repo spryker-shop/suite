63bfa74de49bd300625e037120ea7d1fd3412d72
artemka124 <artem.symynenko@spryker.com>
2020-01-21 10:37:59 +0200

    MR-25 add dependency provider

---
+++ src/Pyz/Zed/ZedNavigation/ZedNavigationDependencyProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\ZedNavigation;
+
+use Spryker\Zed\Acl\Communication\Plugin\Navigation\AclNavigationItemsFilterPlugin;
+use Spryker\Zed\ZedNavigation\ZedNavigationDependencyProvider as SprykerZedNavigationDependencyProvider;
+
+class ZedNavigationDependencyProvider extends SprykerZedNavigationDependencyProvider
+{
+    /**
+     * @return array
+     */
+    protected function getNavigationItemFilterPlugins(): array
+    {
+        return [
+            new AclNavigationItemsFilterPlugin(),
+        ];
+    }
+}
