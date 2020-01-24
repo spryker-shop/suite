d252856a7dfe796c1a5231f92fa9a82d0d174118
vhrychenko <aptah199494@gmail.com>
2020-01-24 16:15:50 +0200

    MP-1701 Created config value

---
+++ src/Pyz/Shared/MerchantSwitcherWidget/MerchantSwitcherWidgetConfig.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Shared\MerchantSwitcherWidget;
+
+use SprykerShop\Shared\MerchantSwitcherWidget\MerchantSwitcherWidgetConfig as SprykerMerchantSwitcherWidgetConfig;
+
+class MerchantSwitcherWidgetConfig extends SprykerMerchantSwitcherWidgetConfig
+{
+    public const ENABLE_MERCHANT_SWITCHER = false;
+}
