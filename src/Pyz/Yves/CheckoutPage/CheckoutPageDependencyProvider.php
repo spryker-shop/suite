0140a7ac38b862ad28cf029638a46e8b643eefd2
ZakirovDamir <damir.zakirov.93@gmail.com>
2020-01-27 15:30:06 +0200

    PS:9965 Added a plugin call and glossary changes

--- src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
+++ src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
@@ -28,6 +28,7 @@ use SprykerShop\Yves\CustomerPage\Plugin\CheckoutPage\CustomerAddressExpanderPlu
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutAddressStepEnterPreCheckPlugin;
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutPaymentStepEnterPreCheckPlugin;
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutShipmentStepEnterPreCheckPlugin;
+use SprykerShop\Yves\QuoteRequestAgentWidget\Plugin\Shipment\QuoteRequestAgentWidgetCheckoutShipmentPostExecuteStrategyPlugin;
 use SprykerShop\Yves\SalesOrderThresholdWidget\Plugin\CheckoutPage\SalesOrderThresholdWidgetPlugin;
 
 /**
@@ -204,6 +205,16 @@ class CheckoutPageDependencyProvider extends SprykerShopCheckoutPageDependencyPr
         return [
             new CustomerAddressExpanderPlugin(),
             new CompanyUnitAddressExpanderPlugin(),
+        ];
+    }
+
+    /**
+     * @return \SprykerShop\Yves\QuoteRequestAgentPageExtension\Dependency\Plugin\CheckoutShipmentPostExecuteStrategyPluginInterface[]
+     */
+    protected function getCheckoutShipmentPostExecuteStrategyPlugins(): array
+    {
+        return [
+            new QuoteRequestAgentWidgetCheckoutShipmentPostExecuteStrategyPlugin(),
         ];
     }
 }
