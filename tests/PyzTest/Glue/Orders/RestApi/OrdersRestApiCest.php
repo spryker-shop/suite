e1d948f8c57f650ddef52d638ebc06caf1c6f144
Kotiuk <andrii.kotiuk@spryker.com>
2020-02-05 15:27:00 +0000

    glue 9044 added cests for orders

--- tests/PyzTest/Glue/Orders/RestApi/OrdersRestApiCest.php
+++ tests/PyzTest/Glue/Orders/RestApi/OrdersRestApiCest.php
@@ -8,80 +8,254 @@
 namespace PyzTest\Glue\Orders\RestApi;
 
 use Codeception\Util\HttpCode;
-use PyzTest\Glue\Carts\CartsApiTester;
+use Generated\Shared\Transfer\CustomerTransfer;
 use PyzTest\Glue\Orders\OrdersApiTester;
-use PyzTest\Glue\Orders\RestApi\OrdersRestApiFixtures;
-use Spryker\Glue\CartsRestApi\CartsRestApiConfig;
-use Spryker\Glue\GlueApplication\Rest\RequestConstantsInterface;
-use Spryker\Shared\Calculation\CalculationPriceMode;
 
 /**
  * Auto-generated group annotations
  *
  * @group PyzTest
  * @group Glue
- * @group Carts
+ * @group Orders
  * @group RestApi
- * @group CartsRestApiCest
+ * @group OrdersRestApiCest
  * Add your own group annotations below this line
  * @group EndToEnd
  */
 class OrdersRestApiCest
 {
     /**
-     * @var  \PyzTest\Glue\Orders\RestApi\OrdersRestApiFixtures
+     * @var \PyzTest\Glue\Orders\RestApi\OrdersRestApiFixtures
      */
     protected $fixtures;
 
     /**
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
      *
      * @return void
      */
-    public function loadFixtures(CartsApiTester $I): void
+    public function loadFixtures(OrdersApiTester $I): void
     {
         $this->fixtures = $I->loadFixtures(OrdersRestApiFixtures::class);
     }
 
-
     /**
      * @depends loadFixtures
      *
-     * @param \PyzTest\Glue\Products\ProductsApiTester $I
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
      *
      * @return void
      */
-    public function requestExistingProductConcrete(OrdersApiTester $I): void
+    public function requestGetEmptyListOfOrders(OrdersApiTester $I): void
     {
         //arrange
-        $this->authorizeCustomer($I);
+        $this->authorizeCustomer($I, $this->fixtures->getCustomerTransfer1());
 
         //act
         $I->sendGET(
-            $I->formatUrl(
-                'orders/{orderId}',
-                [
-                    'orderId' => $this->fixtures->getOrderTransfer()->getIdSalesOrder(),
-                ]
-            )
+            $I->formatUrl('orders')
         );
 
         //assert
         $I->seeResponseCodeIs(HttpCode::OK);
         $I->seeResponseIsJson();
+        $I->seeResponseMatchesOpenApiSchema();
 
+        $I->amSure('The returned resource has correct self-link')
+            ->whenI()
+            ->seeResponseDataContainsEmptyCollection();
+
+        $I->seeResponseLinksContainsSelfLink(
+            $I->formatFullUrl('orders')
+        );
     }
 
     /**
-     * @param \PyzTest\Glue\Carts\CartsApiTester $I
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
      *
      * @return void
      */
-    protected function authorizeCustomer(OrdersApiTester $I): void
+    public function requestGetListOfOrdersWithSingleOrder(OrdersApiTester $I): void
     {
-        $token = $I->haveAuthorizationToGlue($this->fixtures->getCustomerTransfer())->getAccessToken();
+        //arrange
+        $this->authorizeCustomer($I, $this->fixtures->getCustomerTransfer2());
+
+        //act
+        $I->sendGET(
+            $I->formatUrl('orders')
+        );
+
+        //assert
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesOpenApiSchema();
+
+        $I->amSure('The returned resource has correct self-link')
+            ->whenI()
+            ->seeResponseLinksContainsSelfLink(
+                $I->formatFullUrl('orders')
+            );
+
+        $I->amSure('The returned resource has not empty collection')
+            ->whenI()
+            ->seeResponseDataContainsNonEmptyCollection();
+
+        $I->amSure('The returned resource has correct resource collection type')
+            ->whenI()
+            ->seeResponseDataContainsResourceCollectionOfType('orders');
+
+        $I->amSure('The returned resource has correct size')
+            ->whenI()
+            ->seeResponseDataContainsResourceCollectionOfTypeWithSizeOf('orders', 1);
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return void
+     */
+    public function requestGetListOfOrdersWithMultipleOrders(OrdersApiTester $I): void
+    {
+        //arrange
+        $this->authorizeCustomer($I, $this->fixtures->getCustomerTransfer3());
+
+        //act
+        $I->sendGET(
+            $I->formatUrl('orders')
+        );
+        //assert
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesOpenApiSchema();
+
+        $I->amSure('The returned resource has correct self-link')
+            ->whenI()
+            ->seeResponseLinksContainsSelfLink(
+                $I->formatFullUrl('orders')
+            );
+
+        $I->amSure('The returned resource has not empty collection')
+            ->whenI()
+            ->seeResponseDataContainsNonEmptyCollection();
+
+        $I->amSure('The returned resource has correct resource collection type')
+            ->whenI()
+            ->seeResponseDataContainsResourceCollectionOfType('orders');
+
+        $I->amSure('The returned resource has correct size')
+            ->whenI()
+            ->seeResponseDataContainsResourceCollectionOfTypeWithSizeOf('orders', 2);
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return void
+     */
+    public function requestGetOrderDetails(OrdersApiTester $I): void
+    {
+        //arrange
+        $this->authorizeCustomer($I, $this->fixtures->getCustomerTransfer2());
+        $orderTransfer = $this->fixtures->getOrderTransfer();
+
+        //act
+        $I->sendGET(
+            $I->formatUrl(
+                'orders/{customerOrderReference}',
+                [
+                    'customerOrderReference' => $orderTransfer->getOrderReference(),
+                ]
+            )
+        );
+        //assert
+        $I->seeResponseCodeIs(HttpCode::OK);
+        $I->seeResponseIsJson();
+        $I->seeResponseMatchesOpenApiSchema();
+
+        $I->amSure('The returned resource has not empty collection')
+            ->whenI()
+            ->seeResponseDataContainsNonEmptyCollection();
+
+        $I->amSure('The returned resource is of correct type')
+            ->whenI()
+            ->seeResponseDataContainsSingleResourceOfType('orders');
+
+        $I->amSure('The returned resource has correct id')
+            ->whenI()
+            ->seeSingleResourceIdEqualTo($orderTransfer->getOrderReference());
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return void
+     */
+    public function requestGetListOfOrderWithoutAuthorizationToken(OrdersApiTester $I): void
+    {
+        //act
+        $I->sendGET(
+            $I->formatUrl('orders')
+        );
+        //assert
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return void
+     */
+    public function requestGetOrderDetailsWithoutAuthorizationToken(OrdersApiTester $I): void
+    {
+        //arrange
+        $orderTransfer = $this->fixtures->getOrderTransfer();
+
+        //act
+        $I->sendGET(
+            $I->formatUrl(
+                'orders/{customerOrderReference}',
+                [
+                    'customerOrderReference' => $orderTransfer->getOrderReference(),
+                ]
+            )
+        );
+        //assert
+        $I->seeResponseCodeIs(HttpCode::FORBIDDEN);
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return void
+     */
+    public function requestGetOrderDetailsWithIncorrectOrderReference(OrdersApiTester $I): void
+    {
+        //arrange
+        $this->authorizeCustomer($I, $this->fixtures->getCustomerTransfer2());
+
+        //act
+        $I->sendGET(
+            $I->formatUrl(
+                'orders/{customerOrderReference}',
+                [
+                    'customerOrderReference' => 'test',
+                ]
+            )
+        );
+        //assert
+        $I->seeResponseCodeIs(HttpCode::NOT_FOUND);
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
+     *
+     * @return void
+     */
+    protected function authorizeCustomer(OrdersApiTester $I, CustomerTransfer $customerTransfer): void
+    {
+        $token = $I->haveAuthorizationToGlue($customerTransfer)->getAccessToken();
 
         $I->amBearerAuthenticated($token);
     }
-
 }
--- tests/PyzTest/Glue/Orders/RestApi/OrdersRestApiFixtures.php
+++ tests/PyzTest/Glue/Orders/RestApi/OrdersRestApiFixtures.php
@@ -7,16 +7,18 @@
 
 namespace PyzTest\Glue\Orders\RestApi;
 
+use Generated\Shared\DataBuilder\QuoteBuilder;
 use Generated\Shared\Transfer\CustomerTransfer;
-use Generated\Shared\Transfer\ItemTransfer;
 use Generated\Shared\Transfer\SaveOrderTransfer;
-use PyzTest\Glue\Carts\CartsApiTester;
+use Generated\Shared\Transfer\TotalsTransfer;
 use PyzTest\Glue\Orders\OrdersApiTester;
+use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use SprykerTest\Shared\Testify\Fixtures\FixturesBuilderInterface;
 use SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface;
 
 /**
  * Auto-generated group annotations
+ *
  * @group PyzTest
  * @group Glue
  * @group Orders
@@ -26,32 +28,100 @@ use SprykerTest\Shared\Testify\Fixtures\FixturesContainerInterface;
  */
 class OrdersRestApiFixtures implements FixturesBuilderInterface, FixturesContainerInterface
 {
-
     protected const TEST_USERNAME = 'test username';
     protected const TEST_PASSWORD = 'test password';
+
+    protected const TEST_GRAND_TOTAL = 1;
+
     /**
      * @var \Generated\Shared\Transfer\SaveOrderTransfer
      */
     protected $saveOrderTransfer;
+
     /**
      * @var \Generated\Shared\Transfer\CustomerTransfer
      */
-    protected $customerTransfer;
+    protected $customerTransfer1;
 
     /**
-     * @return \Generated\Shared\Transfer\SaveOrderTransfer
+     * @var \Generated\Shared\Transfer\CustomerTransfer
      */
-    public function getOrderTransfer(): SaveOrderTransfer
+    protected $customerTransfer2;
+
+    /**
+     * @var \Generated\Shared\Transfer\CustomerTransfer
+     */
+    protected $customerTransfer3;
+
+    /**
+     * @var \Spryker\Shared\Kernel\Transfer\AbstractTransfer
+     */
+    protected $quoteTransfer;
+
+    /**
+     * @var \Generated\Shared\Transfer\ProductConcreteTransfer[]
+     */
+    protected $productTransfers;
+
+    /**
+     * @var string
+     */
+    protected $stateMachineProcessName;
+
+    /**
+     * @return string
+     */
+    protected function getStateMachineProcessName(): string
     {
-        return $this->saveOrderTransfer;
+        return $this->stateMachineProcessName;
     }
 
     /**
      * @return \Generated\Shared\Transfer\CustomerTransfer
      */
-    public function getCustomerTransfer(): CustomerTransfer
+    public function getCustomerTransfer1(): CustomerTransfer
     {
-        return $this->customerTransfer;
+        return $this->customerTransfer1;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\CustomerTransfer
+     */
+    public function getCustomerTransfer2(): CustomerTransfer
+    {
+        return $this->customerTransfer2;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\CustomerTransfer
+     */
+    public function getCustomerTransfer3(): CustomerTransfer
+    {
+        return $this->customerTransfer3;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\ProductConcreteTransfer[]
+     */
+    protected function getProductTransfers()
+    {
+        return $this->productTransfers;
+    }
+
+    /**
+     * @return \Spryker\Shared\Kernel\Transfer\AbstractTransfer
+     */
+    protected function getQuoteTransfer()
+    {
+        return $this->quoteTransfer;
+    }
+
+    /**
+     * @return \Generated\Shared\Transfer\SaveOrderTransfer
+     */
+    public function getOrderTransfer()
+    {
+        return $this->saveOrderTransfer;
     }
 
     /**
@@ -61,8 +131,23 @@ class OrdersRestApiFixtures implements FixturesBuilderInterface, FixturesContain
      */
     public function buildFixtures(OrdersApiTester $I): FixturesContainerInterface
     {
-        $this->createCustomer($I);
-        $this->createSaveOrderTransfer($I);
+        $this->createStateMachine($I);
+        $this->productTransfers[] = $this->createProduct($I);
+
+        //prepare data for empty order list response
+        $this->customerTransfer1 = $this->createCustomer($I, static::TEST_USERNAME, static::TEST_PASSWORD);
+
+        //prepare data for single order list response
+        $this->customerTransfer2 = $this->createCustomer($I, static::TEST_USERNAME, static::TEST_PASSWORD);
+        $this->quoteTransfer = $this->createQuote($this->customerTransfer2);
+        $this->saveOrderTransfer = $this->createOrderFromQuote($I);
+
+        //prepare data for multiply order list response
+        $this->customerTransfer3 = $this->createCustomer($I, static::TEST_USERNAME, static::TEST_PASSWORD);
+        $this->quoteTransfer = $this->createQuote($this->customerTransfer3);
+        $this->createOrderFromQuote($I);
+        $this->quoteTransfer = $this->createQuote($this->customerTransfer3);
+        $this->createOrderFromQuote($I);
 
         return $this;
     }
@@ -72,31 +157,67 @@ class OrdersRestApiFixtures implements FixturesBuilderInterface, FixturesContain
      *
      * @return void
      */
-    protected function createSaveOrderTransfer(OrdersApiTester $I): void
+    protected function createStateMachine(OrdersApiTester $I): void
     {
-        $testStateMachineProcessName = 'Test01';
+        $testStateMachineProcessName = 'DummyPayment01';
         $I->configureTestStateMachine([$testStateMachineProcessName]);
-        $saveOrderTransfer = $I->haveOrder([
-            ItemTransfer::UNIT_PRICE => 100,
-            ItemTransfer::SUM_PRICE => 100,
-        ], $testStateMachineProcessName);
-        $this->saveOrderTransfer = $saveOrderTransfer;
+
+        $this->stateMachineProcessName = $testStateMachineProcessName;
     }
 
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     * @param string $name
+     * @param string $password
+     *
+     * @return \Generated\Shared\Transfer\CustomerTransfer
+     */
+    protected function createCustomer(OrdersApiTester $I, string $name, string $password): CustomerTransfer
+    {
+        return $I->haveCustomer([
+            CustomerTransfer::USERNAME => $name,
+            CustomerTransfer::PASSWORD => $password,
+            CustomerTransfer::NEW_PASSWORD => $password,
+        ]);
+    }
 
     /**
      * @param \PyzTest\Glue\Orders\OrdersApiTester $I
      *
-     * @return void
+     * @return \Generated\Shared\Transfer\ProductConcreteTransfer
      */
-    protected function createCustomer(OrdersApiTester $I): void
+    public function createProduct(OrdersApiTester $I)
     {
-        $customerTransfer = $I->haveCustomer([
-            CustomerTransfer::USERNAME => static::TEST_USERNAME,
-            CustomerTransfer::PASSWORD => static::TEST_PASSWORD,
-            CustomerTransfer::NEW_PASSWORD => static::TEST_PASSWORD,
-        ]);
+        $productTransfer = $I->haveProduct();
 
-        $this->customerTransfer = $customerTransfer;
+        return $productTransfer;
+    }
+
+    /**
+     * @param \Generated\Shared\Transfer\CustomerTransfer $customerTransfer
+     *
+     * @return \Spryker\Shared\Kernel\Transfer\AbstractTransfer
+     */
+    public function createQuote(CustomerTransfer $customerTransfer): AbstractTransfer
+    {
+        return (new QuoteBuilder())
+            ->withItem($this->getProductTransfers())
+            ->withCustomer([CustomerTransfer::CUSTOMER_REFERENCE => $customerTransfer->getCustomerReference()])
+            ->withTotals([TotalsTransfer::GRAND_TOTAL => static::TEST_GRAND_TOTAL])
+            ->withShippingAddress()
+            ->withBillingAddress()
+            ->withCurrency()
+            ->withPayment()
+            ->build();
+    }
+
+    /**
+     * @param \PyzTest\Glue\Orders\OrdersApiTester $I
+     *
+     * @return \Generated\Shared\Transfer\SaveOrderTransfer
+     */
+    public function createOrderFromQuote(OrdersApiTester $I): SaveOrderTransfer
+    {
+        return $I->haveOrderFromQuote($this->getQuoteTransfer(), $this->getStateMachineProcessName());
     }
 }
