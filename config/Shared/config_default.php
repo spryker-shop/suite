4254d8642bdac18571d9c797ad2659e217922de0
René Klatt <rene.klatt@spryker.com>
2020-02-06 10:50:02 +0000

    TE-4987 Updated spryker/spryker.

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -70,6 +70,8 @@ $CURRENT_STORE = Store::getInstance()->getStoreName();
 
 // ---------- General environment
 $config[KernelConstants::SPRYKER_ROOT] = APPLICATION_ROOT_DIR . '/vendor/spryker/spryker/Bundles';
+$config[KernelConstants::AUTO_LOADER_CACHE_FILE_PATH] = APPLICATION_ROOT_DIR . '/data/' . $CURRENT_STORE . '/cache/' . APPLICATION . '/unresolvable.cache';
+
 $config[ApplicationConstants::PROJECT_TIMEZONE] = 'UTC';
 $config[ApplicationConstants::ENABLE_WEB_PROFILER] = false;
 
