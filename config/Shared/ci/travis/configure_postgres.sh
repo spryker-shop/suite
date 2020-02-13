8692846580e30881ab4c5518de9c76153662dcd7
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-02-13 20:47:35 +0200

    Add Postgres start/stop commands after configuration in Travis

--- config/Shared/ci/travis/configure_postgres.sh
+++ config/Shared/ci/travis/configure_postgres.sh
@@ -5,4 +5,5 @@ sudo sed -ie 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
 sudo sed -ie 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
 
 # Restart Postgres service
-##sudo /etc/init.d/postgresql restart 12
+sudo /etc/init.d/postgresql stop 12
+sudo /etc/init.d/postgresql start 12
