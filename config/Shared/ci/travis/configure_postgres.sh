d91d4a61217e9cca4dfbf17b3aa38772b240cc3b
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-02-14 15:10:11 +0200

    Change devtest propel configuration manualy for passing untracked git files checking

--- config/Shared/ci/travis/configure_postgres.sh
+++ config/Shared/ci/travis/configure_postgres.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Change port for Propel
-sudo sed -ie 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
+##sudo sed -ie 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
 
 # Set poor access to the Postgres for Travis testing needs
 sudo sed -ie 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
