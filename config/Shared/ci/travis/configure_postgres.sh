d053bfbfa4c18b8ecf44d1f8b7b41e0173144db9
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-02-14 14:04:47 +0200

    Refactored update to the Postgres 12 in Travis

--- config/Shared/ci/travis/configure_postgres.sh
+++ config/Shared/ci/travis/configure_postgres.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Change port for Propel
+sudo sed -ie 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
+
 # Set poor access to the Postgres for Travis testing needs
 sudo sed -ie 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
 sudo sed -ie 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
