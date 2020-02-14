c955d0c163a5cd3c4edd57ecaddc6f9a51b36e23
Vladimir Voronin <vladimir.voronin@gmail.com>
2020-02-14 16:27:53 +0200

    Update sed syntax in Travis to not creating additional backup file

--- config/Shared/ci/travis/configure_postgres.sh
+++ config/Shared/ci/travis/configure_postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Change port for Propel
-sudo sed -ie 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
+sudo sed -i 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
 
 # Set poor access to the Postgres for Travis testing needs
-sudo sed -ie 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
-sudo sed -ie 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
+sudo sed -i 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
+sudo sed -i 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
 
 # Restart Postgres service
 sudo pg_ctlcluster --skip-systemctl-redirect 12 main restart
