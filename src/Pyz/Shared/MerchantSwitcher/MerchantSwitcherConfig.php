71901c347a876a2e247e0a9071734b24bc17b999
vhrychenko <aptah199494@gmail.com>
2020-02-27 17:38:14 +0200

    MP-1724 Fix for widget

--- src/Pyz/Shared/MerchantSwitcher/MerchantSwitcherConfig.php
+++ src/Pyz/Shared/MerchantSwitcher/MerchantSwitcherConfig.php
@@ -11,5 +11,5 @@ use Spryker\Shared\MerchantSwitcher\MerchantSwitcherConfig as SprykerMerchantSwi
 
 class MerchantSwitcherConfig extends SprykerMerchantSwitcherConfig
 {
-    public const ENABLE_MERCHANT_SWITCHER = true;
+    public const ENABLE_MERCHANT_SWITCHER = false;
 }
