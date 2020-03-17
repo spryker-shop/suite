2f3d7c5a7fd7d670a8130a45e2d0161fe4be8870
Dima Aseev <dmitriy.aseev@spryker.com>
2020-03-17 13:53:52 +0200

    PS-12807: Added metadata for order items.

--- src/Pyz/Zed/Sales/SalesDependencyProvider.php
+++ src/Pyz/Zed/Sales/SalesDependencyProvider.php
@@ -36,6 +36,7 @@ use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfiguredBun
 use Spryker\Zed\SalesConfigurableBundle\Communication\Plugin\Sales\ConfiguredBundlesOrderPostSavePlugin;
 use Spryker\Zed\SalesMerchantConnector\Communication\Plugin\OrderItemReferenceExpanderPreSavePlugin;
 use Spryker\Zed\SalesProductConnector\Communication\Plugin\Sales\ItemMetadataHydratorPlugin;
+use Spryker\Zed\SalesProductConnector\Communication\Plugin\Sales\MetadataOrderItemExpanderPlugin;
 use Spryker\Zed\SalesProductConnector\Communication\Plugin\Sales\ProductIdHydratorPlugin;
 use Spryker\Zed\SalesQuantity\Communication\Plugin\SalesExtension\IsQuantitySplittableOrderItemExpanderPreSavePlugin;
 use Spryker\Zed\SalesQuantity\Communication\Plugin\SalesExtension\NonSplittableItemTransformerStrategyPlugin;
@@ -157,6 +158,7 @@ class SalesDependencyProvider extends SprykerSalesDependencyProvider
         return [
             new ProductOptionsOrderItemExpanderPlugin(),
             new StateHistoryOrderItemExpanderPlugin(),
+            new MetadataOrderItemExpanderPlugin(),
         ];
     }
 }
