042fc5cbf09b468d0192c889221abc8464673a59
Sergey Samoylov <sergey.samoylov@spryker.com>
2020-03-20 04:01:08 +0200

    mp-1664: updated namespase for plugin

--- src/Pyz/Client/PriceProductOfferStorage/PriceProductOfferStorageDependencyProvider.php
+++ src/Pyz/Client/PriceProductOfferStorage/PriceProductOfferStorageDependencyProvider.php
@@ -8,7 +8,7 @@
 namespace Pyz\Client\PriceProductOfferStorage;
 
 use Spryker\Client\PriceProductOfferStorage\PriceProductOfferStorageDependencyProvider as SprykerPriceProductOfferStorageDependencyProvider;
-use Spryker\Client\PriceProductOfferVolume\Plugin\PriceProductOfferStorageExtension\PriceProductOfferVolumeExtractorPlugin;
+use Spryker\Client\PriceProductOfferVolume\Plugin\PriceProductOfferStorage\PriceProductOfferVolumeExtractorPlugin;
 
 class PriceProductOfferStorageDependencyProvider extends SprykerPriceProductOfferStorageDependencyProvider
 {
