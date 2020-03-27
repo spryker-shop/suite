09d0f5033378ebef561edb247beab35405effa5e
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-03-27 11:30:24 +0200

    CC-8730 tmp commit

---
+++ src/SprykerCodeBucketContext.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use Spryker\Shared\Kernel\CodeBucket\Context\AbstractCodeBucketContext;
+use Spryker\Shared\Kernel\CodeBucket\Context\CodeBucketContextInterface;
+
+class SprykerCodeBucketContext extends AbstractCodeBucketContext implements CodeBucketContextInterface
+{
+    /**
+     * @return string[]
+     */
+    public function getCodeBuckets(): array
+    {
+        return [
+            'DE',
+            'AT',
+            'US',
+        ];
+    }
+
+    /**
+     * @deprecated This method implementation will be removed when environment configs are cleaned up.
+     *
+     * @return string
+     */
+    public function getDefaultCodeBucket(): string
+    {
+        return APPLICATION_STORE;
+    }
+}
