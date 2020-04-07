c2455c4f99808cf22e42d8c9b31f6367d6270b0d
Kotiuk <andrii.kotiuk@spryker.com>
2020-04-07 14:25:12 +0300

    glue-9460 fix travis errors

--- src/Orm/Zed/OauthRevoke/Persistence/SpyOauthRefreshToken.php
+++ src/Orm/Zed/OauthRevoke/Persistence/SpyOauthRefreshToken.php
@@ -1,5 +1,10 @@
 <?php
 
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace Orm\Zed\OauthRevoke\Persistence;
 
 use Orm\Zed\OauthRevoke\Persistence\Base\SpyOauthRefreshToken as BaseSpyOauthRefreshToken;
@@ -10,11 +15,9 @@ use Orm\Zed\OauthRevoke\Persistence\Base\SpyOauthRefreshToken as BaseSpyOauthRef
  *
  *
  * You should add additional methods to this class to meet the
- * application requirements.  This class will only be generated as
+ * application requirements. This class will only be generated as
  * long as it does not already exist in the output directory.
- *
  */
 class SpyOauthRefreshToken extends BaseSpyOauthRefreshToken
 {
-
 }
