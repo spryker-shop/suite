32e955e14ed4b7bde989d46095f4c5cba327d9e8
artemkosatov <artem.kosatov@spryker.com>
2020-04-09 19:19:41 +0300

    MP-1989: BE refactoring #1 after AA review.

--- src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
+++ src/Pyz/Yves/CheckoutPage/CheckoutPageDependencyProvider.php
@@ -26,7 +26,7 @@ use SprykerShop\Yves\CustomerPage\Form\RegisterForm;
 use SprykerShop\Yves\CustomerPage\Plugin\CheckoutPage\CheckoutAddressFormDataProviderPlugin;
 use SprykerShop\Yves\CustomerPage\Plugin\CheckoutPage\CustomerAddressExpanderPlugin;
 use SprykerShop\Yves\CustomerPage\Plugin\CustomerStepHandler;
-use SprykerShop\Yves\MerchantProductOfferWidget\Plugin\CheckoutPage\MerchantCheckoutStepEnginePreRenderPlugin;
+use SprykerShop\Yves\MerchantProductOfferWidget\Plugin\CheckoutPage\MerchantCheckoutPageStepEnginePreRenderPlugin;
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutAddressStepEnterPreCheckPlugin;
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutPaymentStepEnterPreCheckPlugin;
 use SprykerShop\Yves\QuoteApprovalWidget\Plugin\CheckoutPage\QuoteApprovalCheckerCheckoutShipmentStepEnterPreCheckPlugin;
@@ -199,10 +199,10 @@ class CheckoutPageDependencyProvider extends SprykerShopCheckoutPageDependencyPr
     /**
      * @return \SprykerShop\Yves\CheckoutPageExtension\Dependency\Plugin\StepEngine\CheckoutPageStepEnginePreRenderPluginInterface[]
      */
-    protected function getCheckoutStepEnginePreRenderPlugins(): array
+    protected function getCheckoutPageStepEnginePreRenderPlugins(): array
     {
         return [
-            new MerchantCheckoutStepEnginePreRenderPlugin(),
+            new MerchantCheckoutPageStepEnginePreRenderPlugin(),
         ];
     }
 }
