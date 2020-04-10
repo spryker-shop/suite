6d68e62eb5299e68e5becb7ef557525078eee7b1
Artem Symynenko <artem.symynenko@spryker.com>
2020-04-10 11:06:24 +0300

    MR-580 update json file

--- src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
+++ src/Pyz/Yves/ShopApplication/ShopApplicationDependencyProvider.php
@@ -45,6 +45,7 @@ use SprykerShop\Yves\DiscountPromotionWidget\Widget\CartDiscountPromotionProduct
 use SprykerShop\Yves\LanguageSwitcherWidget\Widget\LanguageSwitcherWidget;
 use SprykerShop\Yves\MerchantProductOfferWidget\Widget\MerchantProductOfferWidget;
 use SprykerShop\Yves\MerchantProductOfferWidget\Widget\ProductOfferSoldByMerchantWidget;
+use SprykerShop\Yves\MerchantSalesOrderWidget\Widget\MerchantOrderReferenceForItemsWidget;
 use SprykerShop\Yves\MerchantSwitcherWidget\Widget\MerchantSwitcherSelectorFormWidget;
 use SprykerShop\Yves\MultiCartWidget\Widget\AddToMultiCartWidget;
 use SprykerShop\Yves\MultiCartWidget\Widget\CartOperationsWidget;
@@ -235,6 +236,7 @@ class ShopApplicationDependencyProvider extends SprykerShopApplicationDependency
             MerchantProductOfferWidget::class,
             ConfiguredBundleNoteWidget::class,
             OrderCustomReferenceWidget::class,
+            MerchantOrderReferenceForItemsWidget::class,
         ];
     }
 
