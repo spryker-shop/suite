c4416774b93060dbd69d5c2ae4cb88b5d94c7e33
artemkosatov <artem.kosatov@spryker.com>
2020-04-15 15:21:48 +0300

    MP-2100: BE refactoring #1 after BE review.

--- src/Pyz/Zed/MerchantGui/MerchantGuiDependencyProvider.php
+++ src/Pyz/Zed/MerchantGui/MerchantGuiDependencyProvider.php
@@ -7,6 +7,7 @@
 
 namespace Pyz\Zed\MerchantGui;
 
+use Spryker\Zed\Kernel\Communication\Form\FormTypeInterface;
 use Spryker\Zed\MerchantGui\MerchantGuiDependencyProvider as SprykerMerchantGuiDependencyProvider;
 use Spryker\Zed\MerchantProfileGui\Communication\Plugin\MerchantGui\MerchantProfileFormExpanderPlugin;
 use Spryker\Zed\MerchantProfileGui\Communication\Plugin\MerchantGui\Tabs\MerchantProfileContactPersonFormTabExpanderPlugin;
@@ -15,6 +16,7 @@ use Spryker\Zed\MerchantProfileGui\Communication\Plugin\MerchantGui\Tabs\Merchan
 use Spryker\Zed\MerchantStockGui\Communication\Plugin\MerchantGui\MerchantStockMerchantFormExpanderPlugin;
 use Spryker\Zed\MerchantUserGui\Communication\Plugin\MerchantGui\MerchantUserTabMerchantFormTabExpanderPlugin;
 use Spryker\Zed\MerchantUserGui\Communication\Plugin\MerchantGui\MerchantUserViewMerchantUpdateFormViewExpanderPlugin;
+use Spryker\Zed\Store\Communication\Plugin\Form\StoreRelationToggleFormTypePlugin;
 
 class MerchantGuiDependencyProvider extends SprykerMerchantGuiDependencyProvider
 {
@@ -50,5 +52,13 @@ class MerchantGuiDependencyProvider extends SprykerMerchantGuiDependencyProvider
         return [
             new MerchantUserViewMerchantUpdateFormViewExpanderPlugin(),
         ];
+    }
+
+    /**
+     * @return \Spryker\Zed\Kernel\Communication\Form\FormTypeInterface
+     */
+    protected function getStoreRelationFormTypePlugin(): FormTypeInterface
+    {
+        return new StoreRelationToggleFormTypePlugin();
     }
 }
