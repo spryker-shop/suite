f967c02275ebe04ae4b307895451d0ae0ae93f8b
Alexey Kravchenko <alexey.kravchenko@spryker.com>
2020-04-16 10:04:30 +0300

    CC-8730 fix travis

--- tests/PyzTest/Zed/Console/_data/console_runner.php
+++ tests/PyzTest/Zed/Console/_data/console_runner.php
@@ -2,6 +2,7 @@
 
 const APPLICATION_ENV = 'development';
 const APPLICATION_STORE = 'DE';
+const APPLICATION_CODE_BUCKET = '';
 const APPLICATION_ROOT_DIR = __DIR__ . '/cli_sandbox';
 const APPLICATION_VENDOR_DIR = __DIR__ . '/../../../../../vendor';
 const APPLICATION_SOURCE_DIR = APPLICATION_ROOT_DIR . '/src/';
