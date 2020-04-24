7741a562a628ba7b5ee86f434c9507b504bff670
Eugenia Poidenko <eugenia.poidenko@spryker.com>
2020-04-24 14:14:37 +0300

    GLUE-10680 fixed tests.

--- tests/PyzTest/Glue/Checkout/RestApi/Fixtures/CheckoutDataRestApiFixtures.php
+++ tests/PyzTest/Glue/Checkout/RestApi/Fixtures/CheckoutDataRestApiFixtures.php
@@ -120,13 +120,13 @@ class CheckoutDataRestApiFixtures implements FixturesBuilderInterface, FixturesC
         ]);
         $I->havePaymentMethodWithStore([
             PaymentMethodTransfer::IS_ACTIVE => true,
-            PaymentMethodTransfer::METHOD_NAME => 'invoice',
+            PaymentMethodTransfer::METHOD_NAME => 'Invoice',
             PaymentMethodTransfer::NAME => 'dummyPaymentInvoice',
             PaymentMethodTransfer::ID_PAYMENT_PROVIDER => $paymentProviderTransfer->getIdPaymentProvider(),
         ]);
         $I->havePaymentMethodWithStore([
             PaymentMethodTransfer::IS_ACTIVE => true,
-            PaymentMethodTransfer::METHOD_NAME => 'credit card',
+            PaymentMethodTransfer::METHOD_NAME => 'Credit Card',
             PaymentMethodTransfer::NAME => 'dummyPaymentCreditCard',
             PaymentMethodTransfer::ID_PAYMENT_PROVIDER => $paymentProviderTransfer->getIdPaymentProvider(),
         ]);
