cbd849b5f19784548820ae9f10ef57d1514e9796
Alex Malkevich <malkevich.alex@gmail.com>
2020-04-30 20:52:36 +0200

    refactor(console): update Merchant Portal command classes

--- src/Pyz/Zed/Console/ConsoleDependencyProvider.php
+++ src/Pyz/Zed/Console/ConsoleDependencyProvider.php
@@ -131,8 +131,8 @@ use Spryker\Zed\Session\Communication\Console\SessionRemoveLockConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\CleanUpDependenciesConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\InstallPackageManagerConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\InstallProjectDependenciesConsole;
-use Spryker\Zed\SetupFrontend\Communication\Console\MpBuildFrontendConsole;
-use Spryker\Zed\SetupFrontend\Communication\Console\MpInstallDependenciesConsole;
+use Spryker\Zed\SetupFrontend\Communication\Console\MerchantPortalBuildFrontendConsole;
+use Spryker\Zed\SetupFrontend\Communication\Console\MerchantPortalInstallDependenciesConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\Npm\RunnerConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\YvesBuildFrontendConsole;
 use Spryker\Zed\SetupFrontend\Communication\Console\YvesInstallDependenciesConsole;
@@ -317,8 +317,8 @@ class ConsoleDependencyProvider extends SprykerConsoleDependencyProvider
             new ZedInstallDependenciesConsole(),
             new ZedBuildFrontendConsole(),
 
-            new MpInstallDependenciesConsole(),
-            new MpBuildFrontendConsole(),
+            new MerchantPortalInstallDependenciesConsole(),
+            new MerchantPortalBuildFrontendConsole(),
 
             new DeleteAllQueuesConsole(),
             new PurgeAllQueuesConsole(),
