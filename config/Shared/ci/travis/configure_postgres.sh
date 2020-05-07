e1416eac36b590d52d95cf5788018f21a18394c8
Vasily Rodin <vasily.rodin@spryker.com>
2020-05-07 09:19:04 +0000

    OPS-823 Try without JSON files

--- config/Shared/ci/travis/configure_postgres.sh
+++ config/Shared/ci/travis/configure_postgres.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Change port for Propel
-sudo sed -i 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
-
 # Set poor access to the Postgres for Travis testing needs
 sudo sed -i 's/md5/trust/g' /etc/postgresql/12/main/pg_hba.conf
 sudo sed -i 's/peer/trust/g' /etc/postgresql/12/main/pg_hba.conf
--- tests/travis_check_untracked.sh
+++ tests/travis_check_untracked.sh
@@ -18,17 +18,11 @@ else
 fi
 cd -
 
-# Avoid untracked files check fail because of Postgres port changing
-sudo sed -i 's/port=5433/port=5432/g' src/Orm/Propel/DE/Config/devtest/propel.json
-
 if [ -z "$(git add src tests -n -A)" ] ; then
     echo "no untracked files in src/tests, all good" ;
 else
     git add src tests -n -A
     exit 1
 fi
-
-# Avoid untracked files check fail because of Postgres port changing
-sudo sed -i 's/port=5432/port=5433/g' src/Orm/Propel/DE/Config/devtest/propel.json
 
 exit 0
