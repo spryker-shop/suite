d4bb5add8a5a8ca96f6d2caa5e7032e60db12be7
voitov <tigran.voitov@spryker.com>
2020-06-02 10:04:37 +0300

    cc-9577 small fix

--- src/Pyz/Zed/DataImport/Business/Model/Navigation/NavigationWriterStep.php
+++ src/Pyz/Zed/DataImport/Business/Model/Navigation/NavigationWriterStep.php
@@ -20,6 +20,7 @@ class NavigationWriterStep extends PublishAwareStep implements DataImportStepInt
 
     public const NAME = 'name';
     public const KEY = 'key';
+    public const KEY_IS_ACTIVE = 'is_active';
 
     /**
      * @param \Spryker\Zed\DataImport\Business\Model\DataSet\DataSetInterface $dataSet
