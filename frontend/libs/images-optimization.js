4d226dd842d0ff2bd5e846b3bd8310ccf5d43c9d
maslov <dev.maslov.a@gmail.com>
2020-06-11 16:01:18 +0300

    TE-5975: finished image optimization

--- frontend/libs/images-optimization.js
+++ frontend/libs/images-optimization.js
@@ -4,8 +4,10 @@ const imageminPngquant = require('imagemin-pngquant');
 const imageminSvgo = require('imagemin-svgo');
 const imageminGifsicle = require('imagemin-gifsicle');
 
-const { lstatSync, readdirSync } = require('fs');
-const { join } = require('path');
+const { lstatSync, readdirSync, existsSync } = require('fs');
+const { join, normalize } = require('path');
+
+let isGlobalImagesOptimized = false;
 
 const imagesOptimization = (appSettings) => {
     let isPublicOutput = false;
@@ -21,25 +23,61 @@ const imagesOptimization = (appSettings) => {
 
     if (isOptimize) {
         try {
-            Object.values(appSettings.paths.assets).reduce((copyConfig, assetsPath) => {
-                const assetPattern = `${assetsPath}/images/**/*.{jpg,png,gif,svg}`;
-                let outputPath = `${assetsPath}/images/optimized-images/`;
+            Object.values(appSettings.paths.assets).map(assetsPath => {
+                const assetsImagePath = normalize(join(assetsPath, '/images/'));
+                const assetsImagePattern = '/*.{jpg,png,svg,gif}';
+                let outputPath = normalize(join(assetsPath, '/images/optimized-images/'));
 
                 if (isPublicOutput) {
-                    outputPath = `${appSettings.paths.public}/images/`;
+                    outputPath = normalize(join(appSettings.paths.public, '/images/'));
                 }
 
-                imagemin([assetPattern], {
-                    destination: outputPath,
-                    plugins: [
-                        imageminMozjpeg(appSettings.imageOptimizationOptions.jpg),
-                        imageminPngquant(appSettings.imageOptimizationOptions.png),
-                        imageminSvgo({
-                            plugins: [appSettings.imageOptimizationOptions.svg],
-                        }),
-                        imageminGifsicle(appSettings.imageOptimizationOptions.gif),
-                    ],
-                })
+                const isGlobalImages = assetsPath === appSettings.paths.assets.globalAssets;
+
+                if (isGlobalImages && isGlobalImagesOptimized && !isPublicOutput) {
+                    return;
+                }
+
+                if (existsSync(assetsImagePath)) {
+                    const isDirectory = source => lstatSync(source).isDirectory();
+
+                    const getDirectories = source =>
+                        readdirSync(source)
+                            .map(name => join(source, name))
+                            .filter(isDirectory);
+
+                    const getDirectoriesRecursive = source => [
+                        source,
+                        ...getDirectories(source)
+                            .map(getDirectoriesRecursive)
+                            .reduce((a, b) => a.concat(b), [])
+                    ];
+
+                    const assetsImageFolders = getDirectoriesRecursive(assetsImagePath)
+                        .map(imagePath => normalize(imagePath));
+
+                    const outputImageFolders = assetsImageFolders
+                        .map(imagePath => imagePath.replace(assetsImagePath,''))
+                        .map(imageInnerFolder => join(outputPath, imageInnerFolder));
+
+                    assetsImageFolders.forEach((dir, i) => {
+                        imagemin([`${dir}${assetsImagePattern}`], {
+                            destination: outputImageFolders[i],
+                            plugins: [
+                                imageminMozjpeg(appSettings.imageOptimizationOptions.jpg),
+                                imageminPngquant(appSettings.imageOptimizationOptions.png),
+                                imageminSvgo({
+                                    plugins: [appSettings.imageOptimizationOptions.svg],
+                                }),
+                                imageminGifsicle(appSettings.imageOptimizationOptions.gif),
+                            ]
+                        });
+                    });
+
+                    if (isGlobalImages) {
+                        isGlobalImagesOptimized = true;
+                    }
+                }
             }, []);
 
             console.info(`${appSettings.namespaceConfig.namespace} (${appSettings.theme} theme) --> images successfully compressed!`);
