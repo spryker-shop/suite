161fc1d08d3f15bba285c49db9414836b1e6fa54
AntonKhabiuk <brotherosirus@gmail.com>
2020-06-17 17:15:46 +0300

    SC-3253 Revert propel config, fix environment in deploy config.

--- config/Shared/config_propel.php
+++ config/Shared/config_propel.php
@@ -7,29 +7,27 @@ use Spryker\Zed\PropelOrm\Business\Builder\ExtensionQueryBuilder;
 use Spryker\Zed\PropelOrm\Business\Builder\ObjectBuilder;
 use Spryker\Zed\PropelOrm\Business\Builder\QueryBuilder;
 
-$ENGINE = $config[PropelConstants::ZED_DB_ENGINE] ?? strtolower(getenv('SPRYKER_DB_ENGINE') ?: PropelConfig::DB_ENGINE_MYSQL);
-
 $DSN = sprintf(
     '%s:host=%s;port=%d;dbname=%s',
-    $ENGINE,
-    $config[PropelConstants::ZED_DB_HOST] ?? getenv('SPRYKER_DB_HOST'),
-    $config[PropelConstants::ZED_DB_PORT] ?? getenv('SPRYKER_DB_PORT'),
-    $config[PropelConstants::ZED_DB_DATABASE] ?? getenv('SPRYKER_DB_DATABASE')
+    $config[PropelConstants::ZED_DB_ENGINE],
+    $config[PropelConstants::ZED_DB_HOST],
+    $config[PropelConstants::ZED_DB_PORT],
+    $config[PropelConstants::ZED_DB_DATABASE]
 );
 
 $connections = [
     'pgsql' => [
         'adapter' => PropelConfig::DB_ENGINE_PGSQL,
         'dsn' => $DSN,
-        'user' => $config[PropelConstants::ZED_DB_USERNAME] ?? getenv('SPRYKER_DB_USERNAME'),
-        'password' => $config[PropelConstants::ZED_DB_PASSWORD] ?? getenv('SPRYKER_DB_PASSWORD'),
+        'user' => $config[PropelConstants::ZED_DB_USERNAME],
+        'password' => $config[PropelConstants::ZED_DB_PASSWORD],
         'settings' => [],
     ],
     'mysql' => [
         'adapter' => PropelConfig::DB_ENGINE_MYSQL,
         'dsn' => $DSN,
-        'user' => $config[PropelConstants::ZED_DB_USERNAME] ?? getenv('SPRYKER_DB_USERNAME'),
-        'password' => $config[PropelConstants::ZED_DB_PASSWORD] ?? getenv('SPRYKER_DB_PASSWORD'),
+        'user' => $config[PropelConstants::ZED_DB_USERNAME],
+        'password' => $config[PropelConstants::ZED_DB_PASSWORD],
         'settings' => [
             'charset' => 'utf8',
             'queries' => [
@@ -65,10 +63,11 @@ $config[PropelConstants::PROPEL] = [
     'paths' => [
         'phpDir' => APPLICATION_ROOT_DIR,
         'sqlDir' => APPLICATION_ROOT_DIR . '/src/Orm/Propel/Sql',
-        'migrationDir' => APPLICATION_ROOT_DIR . '/src/Orm/Propel/Migration_' . $ENGINE,
+        'migrationDir' => APPLICATION_ROOT_DIR . '/src/Orm/Propel/Migration_' . $config[PropelConstants::ZED_DB_ENGINE],
         'schemaDir' => APPLICATION_ROOT_DIR . '/src/Orm/Propel/Schema',
     ],
 ];
 
+$ENGINE = $config[PropelConstants::ZED_DB_ENGINE];
 $config[PropelConstants::PROPEL]['database']['connections']['default'] = $connections[$ENGINE];
 $config[PropelConstants::PROPEL]['database']['connections']['zed'] = $connections[$ENGINE];
