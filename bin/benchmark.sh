4ff71ac0829e32e59e1723525531af9acf68db93
Mike Kalinin <mike.kalinin@spryker.com>
2020-06-18 16:25:52 +0300

    Cleanup: Reverting accidentially committed files

--- bin/benchmark.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo 'Warming up...'
-php src/Benchmark/src/Benchmark/GlueAddToCart.php 5 > /dev/null
-
-time php src/Benchmark/src/Benchmark/GlueAddToCart.php ${1:-100}
-
-cd src/Benchmark
-time vendor/bin/phpbench run src/Benchmark --iterations=${1:-100} --report=total --report=summary --time-unit=seconds
--- src/Benchmark/bootstrap.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-define('YVES_BASE_URL', 'http://yves.de.spryker.local');
-define('GLUE_BASE_URL', 'http://glue.de.spryker.local');
