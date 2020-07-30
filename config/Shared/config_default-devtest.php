d8562943a1026ae1a7ed14cd4c3a37749f98a244
mscherer <mark.scherer@spryker.com>
2020-07-30 12:57:54 +0200

    TE-5887 Upgrade oauth2 server to version 8

--- config/Shared/config_default-devtest.php
+++ config/Shared/config_default-devtest.php
@@ -50,7 +50,6 @@ $config[KernelConstants::SPRYKER_ROOT] = APPLICATION_ROOT_DIR . '/vendor/spryker
 // ---------- ZedRequest
 $config[ZedRequestConstants::TRANSFER_DEBUG_SESSION_FORWARD_ENABLED] = true;
 $config[ZedRequestConstants::SET_REPEAT_DATA] = true;
-$config[ZedRequestConstants::YVES_REQUEST_REPEAT_DATA_PATH] = APPLICATION_ROOT_DIR . '/data/cache/codeBucket/yves-requests';
 
 // ---------- Testify
 $config[TestifyConstants::BOOTSTRAP_CLASS_YVES] = YvesBootstrap::class;
