249ea73c02c81bbb6f1a21408e615c811a3bd7b7
Aleksey Belan <alexey.belan@spryker.com>
2021-05-12 14:19:58 +0300

    MP-4661 Fix aliases for mp modules (#5286)
    MP-4661 Incorrect aliases for mp modules

---
+++ frontend/merchant-portal/mp-paths.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+const ROOT_DIR = path.resolve(__dirname, "../..");
+
+const SPRYKER_CORE_DIR = "vendor/spryker/spryker/Bundles";
+const SPRYKER_PROJECT_DIR = "src/Pyz/Zed";
+
+const ROOT_SPRYKER_CORE_DIR = path.join(ROOT_DIR, SPRYKER_CORE_DIR);
+const ROOT_SPRYKER_PROJECT_DIR = path.join(ROOT_DIR, SPRYKER_PROJECT_DIR);
+
+const MP_CORE_ENTRY_POINT_FILE =
+    "*/src/Spryker/Zed/*/Presentation/Components/entry.ts";
+const MP_PROJECT_ENTRY_POINT_FILE =
+    "*/Presentation/Components/entry.ts";
+
+const MP_PUBLIC_API_FILE = 'mp.public-api.ts';
+
+module.exports = {
+    ROOT_DIR,
+    SPRYKER_CORE_DIR,
+    SPRYKER_PROJECT_DIR,
+    ROOT_SPRYKER_CORE_DIR,
+    ROOT_SPRYKER_PROJECT_DIR,
+    MP_CORE_ENTRY_POINT_FILE,
+    MP_PROJECT_ENTRY_POINT_FILE,
+    MP_PUBLIC_API_FILE,
+};
