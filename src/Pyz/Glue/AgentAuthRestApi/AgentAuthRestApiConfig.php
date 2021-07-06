b9d8cc441b98df80991dd08827ba5e9147c05fb5
sergeshin-spryker <74545642+sergeshin-spryker@users.noreply.github.com>
2021-07-06 16:31:28 +0300

    GLUE-10972: API: Create Request for Quote (#5000)
    GLUE-10972 API: Create Request for Quote

---
+++ src/Pyz/Glue/AgentAuthRestApi/AgentAuthRestApiConfig.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Glue\AgentAuthRestApi;
+
+use Spryker\Glue\AgentAuthRestApi\AgentAuthRestApiConfig as SprykerAgentAuthRestApiConfig;
+use Spryker\Glue\QuoteRequestAgentsRestApi\QuoteRequestAgentsRestApiConfig;
+
+class AgentAuthRestApiConfig extends SprykerAgentAuthRestApiConfig
+{
+    /**
+     * {@inheritDoc}
+     *
+     * @api
+     *
+     * @return string[]
+     */
+    public function getAgentResources(): array
+    {
+        return [
+            static::RESOURCE_AGENT_CUSTOMER_IMPERSONATION_ACCESS_TOKENS,
+            static::RESOURCE_AGENT_CUSTOMER_SEARCH,
+            QuoteRequestAgentsRestApiConfig::RESOURCE_AGENT_QUOTE_REQUESTS,
+        ];
+    }
+}
