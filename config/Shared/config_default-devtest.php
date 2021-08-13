4ed40bbcbc3c6b5567db9a5b1007992380539d82
Oleksandr Zakharkin <24879203+az-joss@users.noreply.github.com>
2021-08-13 15:51:27 +0300

    MP-5519 Fixed password reset link in merchant user email. (#5727)
    MP-5519 When creating a new Merchant and user, the user receives a link to the backoffice instead of a link to merchant portal

--- config/Shared/config_default-devtest.php
+++ config/Shared/config_default-devtest.php
@@ -13,6 +13,7 @@ use Spryker\Shared\Http\HttpConstants;
 use Spryker\Shared\Kernel\KernelConstants;
 use Spryker\Shared\Kernel\Store;
 use Spryker\Shared\Log\LogConstants;
+use Spryker\Shared\MerchantPortalApplication\MerchantPortalConstants;
 use Spryker\Shared\Newsletter\NewsletterConstants;
 use Spryker\Shared\ProductManagement\ProductManagementConstants;
 use Spryker\Shared\Propel\PropelConstants;
@@ -36,6 +37,7 @@ $stores = array_combine(Store::getInstance()->getAllowedStores(), Store::getInst
 $backendApiHost = sprintf('backend-api-test.%s.%s.local', $storeLowerCase, $domain);
 $backendGatewayHost = sprintf('backend-gateway-test.%s.%s.local', $storeLowerCase, $domain);
 $backofficeHost = sprintf('backoffice-test.%s.%s.local', $storeLowerCase, $domain);
+$merchantPortalHost = sprintf('mp-test.%s.%s.local', $storeLowerCase, $domain);
 $glueHost = sprintf('glue-test.de.%s.local', $domain);
 $yvesHost = sprintf('www-test.%s.%s.local', $storeLowerCase, $domain);
 
@@ -169,6 +171,15 @@ $config[ZedRequestConstants::BASE_URL_SSL_ZED_API] = sprintf(
 $config[ApplicationConstants::BASE_URL_ZED] = sprintf(
     'http://%s',
     $backofficeHost
+);
+
+// ----------------------------------------------------------------------------
+// ------------------------------ MERCHANT PORTAL -----------------------------
+// ----------------------------------------------------------------------------
+
+$config[MerchantPortalConstants::BASE_URL_MP] = sprintf(
+    'http://%s',
+    $merchantPortalHost
 );
 
 // ----------------------------------------------------------------------------
