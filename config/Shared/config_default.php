7de553ab06fab405ba1c0dbd6c25f43bbb063786
Stanislav Matveyev <stanislav.matveyev@spryker.com>
2022-04-15 12:42:10 +0300

    Fixed payment module path for split repos

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -591,7 +591,7 @@ $config[GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN] = getenv('
 $config[OmsConstants::PROCESS_LOCATION] = [
     OmsConfig::DEFAULT_PROCESS_LOCATION,
     APPLICATION_ROOT_DIR . '/vendor/spryker-eco/payone/config/Zed/Oms',
-    APPLICATION_ROOT_DIR . '/vendor/spryker/spryker/Bundles/Payment/config/Zed/Oms',
+    APPLICATION_ROOT_DIR . '/vendor/spryker/payment/config/Zed/Oms',
 ];
 $config[OmsConstants::ACTIVE_PROCESSES] = [
     'PayoneCreditCardPartialOperations',
