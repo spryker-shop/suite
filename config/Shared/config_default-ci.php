d523936396076d651b80cd492abdbb278a9d2aa0
Svyatoslav Varpikhovsky <varpihovsky@gmail.com>
2022-07-14 15:36:18 +0300

    FRW-42 Trigger build

--- config/Shared/config_default-ci.php
+++ config/Shared/config_default-ci.php
@@ -109,9 +109,7 @@ $config[OauthCryptographyConstants::PUBLIC_KEY_PATH] = 'file://' . APPLICATION_R
 $config[OauthConstants::ENCRYPTION_KEY] = 'lxZFUEsBCJ2Yb14IF2ygAHI5N4+ZAUXXaSeeJm6+twsUmIen';
 $config[OauthConstants::OAUTH_CLIENT_IDENTIFIER] = 'frontend';
 $config[OauthConstants::OAUTH_CLIENT_SECRET] = 'abc123';
-
 $config[SecuritySystemUserConstants::AUTH_DEFAULT_CREDENTIALS]['yves_system']['token'] = 'JDJ5JDEwJFE0cXBwYnVVTTV6YVZXSnVmM2l1UWVhRE94WkQ4UjBUeHBEWTNHZlFRTEd4U2F6QVBqejQ2';
-
 $config[HttpConstants::URI_SIGNER_SECRET_KEY] = 'JDJ5JDEwJFE0cXBwYnVVTTV6YVZXSnVmM2l1UWVhRE94WkQ4UjBUeHBEWTNHZlFRTEd4U2F6QVBqejQ2';
 
 // ----------------------------------------------------------------------------
