3bba59b6a9b2f6ffd1b9ec78152f0f43d06d4600
Mark Scherer <dereuromark@users.noreply.github.com>
2022-08-09 14:01:57 +0200

    Improve local setup (#7028)

--- config/Shared/config_local.dist.php
+++ config/Shared/config_local.dist.php
@@ -1,5 +1,9 @@
 <?php
 
+use Pyz\Shared\Console\ConsoleConstants;
+
 /*
  * Here go your local configs which should not be version controlled (tokens, passwords, keys, ...)
  */
+
+$config[ConsoleConstants::ENABLE_DEVELOPMENT_CONSOLE_COMMANDS] = true;
