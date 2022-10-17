d808d0879daeedff48c4efabc06caf96c4490e07
Yaroslav Krutikov <yaroslav.krutikov@spryker.com>
2022-10-17 18:24:49 +0200

    FRW-443 Fixed failed tests for suite. Ignored false-positive style check fail

--- tests/PyzTest/Zed/NavigationGui/Presentation/NavigationCRUDCest.php
+++ tests/PyzTest/Zed/NavigationGui/Presentation/NavigationCRUDCest.php
@@ -42,7 +42,7 @@ class NavigationCRUDCest
         $this->update($i, $idNavigation);
 
         # Somehow $this->update() influences on this $this->delete() so the test becomes flickery.
-        $i->wait(1);
+        $i->wait(3);
 
         $this->delete($i, $idNavigation);
     }
