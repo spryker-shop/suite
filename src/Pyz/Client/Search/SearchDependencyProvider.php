1365cbc8a1cdbf04986bcadac326f327fd7dc0cb
Maksym Ohnianikov <89031985+Maksym-Ohnianikov@users.noreply.github.com>
2023-02-20 12:22:03 +0200

    PBC-334: HTTP adapter for Search and integration with CatalogRestApi module (#7581)
    PBC-334: HTTP adapter for Search and integration with CatalogRestApi module

--- src/Pyz/Client/Search/SearchDependencyProvider.php
+++ src/Pyz/Client/Search/SearchDependencyProvider.php
@@ -16,6 +16,8 @@ use Spryker\Client\Search\Dependency\Plugin\SearchConfigBuilderInterface;
 use Spryker\Client\Search\SearchDependencyProvider as SprykerSearchDependencyProvider;
 use Spryker\Client\SearchElasticsearch\Plugin\ElasticsearchSearchAdapterPlugin;
 use Spryker\Client\SearchElasticsearch\Plugin\ElasticsearchSearchContextExpanderPlugin;
+use Spryker\Client\SearchHttp\Plugin\Search\SearchHttpSearchAdapterPlugin;
+use Spryker\Client\SearchHttp\Plugin\Search\SearchHttpSearchContextExpanderPlugin;
 
 class SearchDependencyProvider extends SprykerSearchDependencyProvider
 {
@@ -51,6 +53,7 @@ class SearchDependencyProvider extends SprykerSearchDependencyProvider
     protected function getClientAdapterPlugins(): array
     {
         return [
+            new SearchHttpSearchAdapterPlugin(),
             new ElasticsearchSearchAdapterPlugin(),
         ];
     }
@@ -61,6 +64,7 @@ class SearchDependencyProvider extends SprykerSearchDependencyProvider
     protected function getSearchContextExpanderPlugins(): array
     {
         return [
+            new SearchHttpSearchContextExpanderPlugin(),
             new ElasticsearchSearchContextExpanderPlugin(),
         ];
     }
--- src/Pyz/Zed/Synchronization/SynchronizationDependencyProvider.php
+++ src/Pyz/Zed/Synchronization/SynchronizationDependencyProvider.php
@@ -71,6 +71,7 @@ use Spryker\Zed\ProductSetStorage\Communication\Plugin\Synchronization\ProductSe
 use Spryker\Zed\ProductStorage\Communication\Plugin\Synchronization\ProductAbstractSynchronizationDataPlugin;
 use Spryker\Zed\ProductStorage\Communication\Plugin\Synchronization\ProductConcreteSynchronizationDataPlugin;
 use Spryker\Zed\SalesReturnSearch\Communication\Plugin\Synchronization\ReturnReasonSynchronizationDataBulkRepositoryPlugin;
+use Spryker\Zed\SearchHttp\Communication\Plugin\Synchronization\SearchHttpSynchronizationDataPlugin;
 use Spryker\Zed\ShoppingListStorage\Communication\Plugin\Synchronization\ShoppingListSynchronizationDataBulkPlugin;
 use Spryker\Zed\Synchronization\Communication\Plugin\Synchronization\SynchronizationDataQueryExpanderWhereBetweenStrategyPlugin;
 use Spryker\Zed\Synchronization\SynchronizationDependencyProvider as SprykerSynchronizationDependencyProvider;
@@ -158,6 +159,7 @@ class SynchronizationDependencyProvider extends SprykerSynchronizationDependency
             new ProductConfigurationSynchronizationDataRepositoryPlugin(),
             new MerchantSynchronizationDataBulkRepositoryPlugin(),
             new AssetStorageSynchronizationDataPlugin(),
+            new SearchHttpSynchronizationDataPlugin(),
         ];
     }
 
