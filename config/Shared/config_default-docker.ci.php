1218b243d7f5a9aa24e5f03a20b67f46d04f49a6
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2023-03-15 13:11:51 +0100

    FRW-1733: Travis CI job optimisation (#7604)
    FRW-1733 Travis CI job optimisation

--- config/Shared/config_default-docker.ci.php
+++ config/Shared/config_default-docker.ci.php
@@ -4,7 +4,11 @@
 // ############################## CI CONFIGURATION ############################
 // ############################################################################
 
+use Spryker\Shared\MessageBroker\MessageBrokerConstants;
+use Spryker\Shared\MessageBrokerAws\MessageBrokerAwsConstants;
 use Spryker\Shared\Product\ProductConstants;
+use Spryker\Shared\StorageDatabase\StorageDatabaseConstants;
+use Spryker\Zed\Propel\PropelConfig;
 
 require 'config_default-docker.dev.php';
 
@@ -19,3 +23,25 @@ require 'common/config_logs-ci-info.php';
 //----------------------------------- ACP -------------------------------------
 //-----------------------------------------------------------------------------
 $config[ProductConstants::PUBLISHING_TO_MESSAGE_BROKER_ENABLED] = false;
+
+//-----------------------------------------------------------------------------
+//------------------------------- RDS Storage ---------------------------------
+//-----------------------------------------------------------------------------
+$config[StorageDatabaseConstants::DB_ENGINE] = strtolower(getenv('SPRYKER_DB_ENGINE') ?: '') ?: PropelConfig::DB_ENGINE_MYSQL;
+$config[StorageDatabaseConstants::DB_HOST] = getenv('SPRYKER_DB_HOST');
+$config[StorageDatabaseConstants::DB_PORT] = getenv('SPRYKER_DB_PORT');
+$config[StorageDatabaseConstants::DB_DATABASE] = getenv('SPRYKER_DB_DATABASE');
+$config[StorageDatabaseConstants::DB_USERNAME] = getenv('SPRYKER_DB_USERNAME');
+$config[StorageDatabaseConstants::DB_PASSWORD] = getenv('SPRYKER_DB_PASSWORD');
+$config[StorageDatabaseConstants::DB_DEBUG] = false;
+
+// ----------------------------------------------------------------------------
+// ------------------------------ MessageBroker -----------------------------------------
+// ----------------------------------------------------------------------------
+$config[MessageBrokerConstants::CHANNEL_TO_TRANSPORT_MAP] =
+$config[MessageBrokerAwsConstants::CHANNEL_TO_RECEIVER_TRANSPORT_MAP] = [
+    'payment' => 'in-memory',
+    'assets' => 'in-memory',
+    'product' => 'in-memory',
+    'reviews' => 'in-memory',
+];
