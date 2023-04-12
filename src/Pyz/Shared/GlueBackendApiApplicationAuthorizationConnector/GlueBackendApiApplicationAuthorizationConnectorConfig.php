d25c0de490a148ea4c3d4b70ed952542b842c590
Dmitry Lymarenko <37305577+DmitryLymarenko@users.noreply.github.com>
2023-04-12 16:24:51 +0300

    CC-24927: Service Point (Physical Locations) - Data import & BAPI (#7722)
    CC-24927 Service Point (Physical Locations) - Data import & BAPI

--- src/Pyz/Shared/GlueBackendApiApplicationAuthorizationConnector/GlueBackendApiApplicationAuthorizationConnectorConfig.php
+++ src/Pyz/Shared/GlueBackendApiApplicationAuthorizationConnector/GlueBackendApiApplicationAuthorizationConnectorConfig.php
@@ -77,6 +77,9 @@ class GlueBackendApiApplicationAuthorizationConnectorConfig extends SprykerGlueB
                     'patch',
                 ],
             ],
+            '/\/service-points(?:\/[^\/]+)?\/?$/' => [
+                'isRegularExpression' => true,
+            ],
         ];
     }
 }
