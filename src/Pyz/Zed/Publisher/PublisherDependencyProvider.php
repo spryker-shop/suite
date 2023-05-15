fcbb2e1883fea0c984a2334943e4c5cb5cf2ca31
Artem Siminenko <artem.siminenko@gmail.com>
2023-05-15 12:58:05 +0200

    CC-17164 Fix product offer availability when product offer is not assigned to store. (#7797)
    CC-17164 Fix product offer availability when product offer is not assigned to store.

--- src/Pyz/Zed/Publisher/PublisherDependencyProvider.php
+++ src/Pyz/Zed/Publisher/PublisherDependencyProvider.php
@@ -109,6 +109,7 @@ use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Publisher\ProductLabelD
 use Spryker\Zed\ProductLabelStorage\Communication\Plugin\Publisher\ProductLabelProductAbstract\ProductLabelProductAbstractWritePublisherPlugin as ProductLabelProductAbstractStorageWritePublisherPlugin;
 use Spryker\Zed\ProductListSearch\Communication\Plugin\Publisher\ProductListSearchPublisherTriggerPlugin;
 use Spryker\Zed\ProductListStorage\Communication\Plugin\Publisher\ProductListPublisherTriggerPlugin;
+use Spryker\Zed\ProductOfferAvailabilityStorage\Communication\Plugin\Publisher\ProductOfferAvailability\ProductOfferAvailabilityProductOfferStoreStoragePublisherPlugin;
 use Spryker\Zed\ProductOfferStorage\Communication\Plugin\Publisher\ProductConcreteOffers\ProductConcreteProductOffersDeletePublisherPlugin;
 use Spryker\Zed\ProductOfferStorage\Communication\Plugin\Publisher\ProductConcreteOffers\ProductConcreteProductOffersStoreDeletePublisherPlugin;
 use Spryker\Zed\ProductOfferStorage\Communication\Plugin\Publisher\ProductConcreteOffers\ProductConcreteProductOffersStoreWritePublisherPlugin;
@@ -191,6 +192,7 @@ class PublisherDependencyProvider extends SprykerPublisherDependencyProvider
             $this->getProductExportPlugins(),
             $this->getServicePointSearchPlugins(),
             $this->getServicePointStoragePlugins(),
+            $this->getProductOfferAvailabilityStoragePlugins(),
         );
     }
 
@@ -501,6 +503,16 @@ class PublisherDependencyProvider extends SprykerPublisherDependencyProvider
     {
         return [
             new PriceProductMerchantWritePublisherPlugin(),
+        ];
+    }
+
+    /**
+     * @return array<\Spryker\Zed\PublisherExtension\Dependency\Plugin\PublisherPluginInterface>
+     */
+    protected function getProductOfferAvailabilityStoragePlugins(): array
+    {
+        return [
+            new ProductOfferAvailabilityProductOfferStoreStoragePublisherPlugin(),
         ];
     }
 
