a36ebae90562b63ba162a63ceaec0e8a94c1f052
Dmitry Tsemma <dmitry.tsemma@spryker.com>
2023-09-05 20:26:43 +0300

    FRW-5841 Dynamic Entity configuration installer. (#8135)
    FRW-5841 Dynamic Entity configuration installer.

--- src/Pyz/Zed/Installer/InstallerDependencyProvider.php
+++ src/Pyz/Zed/Installer/InstallerDependencyProvider.php
@@ -11,6 +11,7 @@ use Spryker\Zed\Acl\Communication\Plugin\AclInstallerPlugin;
 use Spryker\Zed\CompanyUserInvitation\Communication\Plugin\CompanyUserInvitationStatusInstallerPlugin;
 use Spryker\Zed\Country\Communication\Plugin\CountryInstallerPlugin;
 use Spryker\Zed\CustomerAccess\Communication\Plugin\CustomerAccessInstallerPlugin;
+use Spryker\Zed\DynamicEntity\Communication\Plugin\Installer\DynamicEntityInstallerPlugin;
 use Spryker\Zed\Glossary\Communication\Plugin\GlossaryInstallerPlugin;
 use Spryker\Zed\Installer\InstallerDependencyProvider as SprykerInstallerDependencyProvider;
 use Spryker\Zed\Locale\Communication\Plugin\LocaleInstallerPlugin;
@@ -65,6 +66,7 @@ class InstallerDependencyProvider extends SprykerInstallerDependencyProvider
             new AgentOauthScopeInstallerPlugin(),
             new PushNotificationWebPushPhpProviderInstallerPlugin(),
             new OauthWarehouseInstallerPlugin(),
+            new DynamicEntityInstallerPlugin(),
         ];
     }
 }
