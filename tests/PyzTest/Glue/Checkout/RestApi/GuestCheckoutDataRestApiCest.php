3d3e64db42d27112e35ef34ce4fc77283cab1caa
Dmitriy Aseev <32021063+dmiseev@users.noreply.github.com>
2023-09-07 14:53:59 +0300

    CC-31153: Reverted Codeception GLUE API Tests  (#8199)
    Reverted Codeception GLUE API Tests

--- tests/PyzTest/Glue/Checkout/RestApi/GuestCheckoutDataRestApiCest.php
+++ tests/PyzTest/Glue/Checkout/RestApi/GuestCheckoutDataRestApiCest.php
@@ -9,8 +9,7 @@ namespace PyzTest\Glue\Checkout\RestApi;
 
 use Codeception\Util\HttpCode;
 use PyzTest\Glue\Checkout\CheckoutApiTester;
-use PyzTest\Glue\Checkout\RestApi\Fixtures\CheckoutDataRestApiFixtures;
-use PyzTest\Glue\Checkout\RestApi\Fixtures\PaymentMethodsFixtures;
+use PyzTest\Glue\Checkout\RestApi\Fixtures\GuestCheckoutDataRestApiFixtures;
 use Spryker\Glue\CheckoutRestApi\CheckoutRestApiConfig;
 
 /**
@@ -20,14 +19,19 @@ use Spryker\Glue\CheckoutRestApi\CheckoutRestApiConfig;
  * @group Glue
  * @group Checkout
  * @group RestApi
- * @group CheckoutDataRestApiCest
+ * @group GuestCheckoutDataRestApiCest
  * Add your own group annotations below this line
  * @group EndToEnd
  */
-class CheckoutDataRestApiCest
+class GuestCheckoutDataRestApiCest
 {
     /**
-     * @var \PyzTest\Glue\Checkout\RestApi\Fixtures\CheckoutDataRestApiFixtures
+     * @var string
+     */
+    protected const HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID = 'X-Anonymous-Customer-Unique-Id';
+
+    /**
+     * @var \PyzTest\Glue\Checkout\RestApi\Fixtures\GuestCheckoutDataRestApiFixtures
      */
     protected $fixtures;
 
@@ -38,9 +42,8 @@ class CheckoutDataRestApiCest
      */
     public function loadFixtures(CheckoutApiTester $I): void
     {
-        $I->loadFixtures(PaymentMethodsFixtures::class);
-        /** @var \PyzTest\Glue\Checkout\RestApi\Fixtures\CheckoutDataRestApiFixtures $fixtures */
-        $fixtures = $I->loadFixtures(CheckoutDataRestApiFixtures::class);
+        /** @var \PyzTest\Glue\Checkout\RestApi\Fixtures\GuestCheckoutDataRestApiFixtures $fixtures */
+        $fixtures = $I->loadFixtures(GuestCheckoutDataRestApiFixtures::class);
         $this->fixtures = $fixtures;
     }
 
@@ -51,12 +54,14 @@ class CheckoutDataRestApiCest
      *
      * @return void
      */
-    public function requestEmptyRequestWithOneItemInQuoteAndEmptyBody(CheckoutApiTester $I): void
+    public function requestEmptyRequestWithOneItemInQuote(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
 
         $url = $I->buildCheckoutDataUrl();
         $requestPayload = [
@@ -90,54 +95,15 @@ class CheckoutDataRestApiCest
      *
      * @return void
      */
-    public function requestWithOneItemInQuoteAndBillingAddress(CheckoutApiTester $I): void
+    public function requestWithOneItemInQuoteAndBillingAndShippingAddresses(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
-
-        $url = $I->buildCheckoutDataUrl();
-        $requestPayload = [
-            'data' => [
-                'type' => CheckoutRestApiConfig::RESOURCE_CHECKOUT_DATA,
-                'attributes' => [
-                    'idCart' => $quoteTransfer->getUuid(),
-                    'billingAddress' => $I->getAddressRequestPayload($quoteTransfer->getBillingAddress()),
-                ],
-            ],
-        ];
-
-        // Act
-        $I->sendPOST($url, $requestPayload);
-
-        // Assert
-        $I->seeResponseCodeIs(HttpCode::OK);
-        $I->seeResponseIsJson();
-        $I->seeResponseMatchesOpenApiSchema();
-
-        $I->assertCheckoutDataResponseResourceHasCorrectData();
-
-        $I->amSure('The returned resource has correct self link')
-            ->whenI()
-            ->seeSingleResourceHasSelfLink($url);
-    }
-
-    /**
-     * @depends loadFixtures
-     *
-     * @param \PyzTest\Glue\Checkout\CheckoutApiTester $I
-     *
-     * @return void
-     */
-    public function requestWithOneItemInQuoteWithoutPayment(CheckoutApiTester $I): void
-    {
-        // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
         $shippingAddressTransfer = $quoteTransfer->getItems()[0]->getShipment()->getShippingAddress();
-        $idShipmentMethod = $this->fixtures->getShipmentMethodTransfer()->getIdShipmentMethod();
 
         $url = $I->buildCheckoutDataUrl();
         $requestPayload = [
@@ -147,7 +113,6 @@ class CheckoutDataRestApiCest
                     'idCart' => $quoteTransfer->getUuid(),
                     'billingAddress' => $I->getAddressRequestPayload($quoteTransfer->getBillingAddress()),
                     'shippingAddress' => $I->getAddressRequestPayload($shippingAddressTransfer),
-                    'shipment' => $I->getShipmentRequestPayload($idShipmentMethod),
                 ],
             ],
         ];
@@ -177,9 +142,11 @@ class CheckoutDataRestApiCest
     public function requestWithOneItemInQuoteWithoutShipment(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
         $shippingAddressTransfer = $quoteTransfer->getItems()[0]->getShipment()->getShippingAddress();
 
         $url = $I->buildCheckoutDataUrl();
@@ -217,12 +184,14 @@ class CheckoutDataRestApiCest
      *
      * @return void
      */
-    public function requestWithOneItemInQuoteWithoutVoucherCode(CheckoutApiTester $I): void
+    public function requestWithOneItemInQuoteWithoutPayment(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
         $shippingAddressTransfer = $quoteTransfer->getItems()[0]->getShipment()->getShippingAddress();
         $idShipmentMethod = $this->fixtures->getShipmentMethodTransfer()->getIdShipmentMethod();
 
@@ -234,7 +203,6 @@ class CheckoutDataRestApiCest
                     'idCart' => $quoteTransfer->getUuid(),
                     'billingAddress' => $I->getAddressRequestPayload($quoteTransfer->getBillingAddress()),
                     'shippingAddress' => $I->getAddressRequestPayload($shippingAddressTransfer),
-                    'payments' => [],
                     'shipment' => $I->getShipmentRequestPayload($idShipmentMethod),
                 ],
             ],
@@ -265,9 +233,11 @@ class CheckoutDataRestApiCest
     public function requestWithOneItemInQuoteAndCustomerAndBillingAndShippingAddressesAndCart(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
         $shippingAddressTransfer = $quoteTransfer->getItems()[0]->getShipment()->getShippingAddress();
 
         $url = $I->buildCheckoutDataUrl();
@@ -278,7 +248,7 @@ class CheckoutDataRestApiCest
                     'idCart' => $quoteTransfer->getUuid(),
                     'billingAddress' => $I->getAddressRequestPayload($quoteTransfer->getBillingAddress()),
                     'shippingAddress' => $I->getAddressRequestPayload($shippingAddressTransfer),
-                    'customer' => $I->getCustomerRequestPayload($this->fixtures->getCustomerTransfer()),
+                    'customer' => $I->getCustomerRequestPayload($this->fixtures->getGuestCustomerTransfer()),
                 ],
             ],
         ];
@@ -308,9 +278,11 @@ class CheckoutDataRestApiCest
     public function requestWithOneItemInQuoteAndFullBody(CheckoutApiTester $I): void
     {
         // Arrange
-        $I->authorizeCustomerToGlue($this->fixtures->getCustomerTransfer());
-
-        $quoteTransfer = $this->fixtures->getQuoteTransfer();
+        $quoteTransfer = $this->fixtures->getGuestQuoteTransfer();
+        $I->haveHttpHeader(
+            static::HEADER_ANONYMOUS_CUSTOMER_UNIQUE_ID,
+            $this->fixtures->getGuestCustomerReference(),
+        );
         $shippingAddressTransfer = $quoteTransfer->getItems()[0]->getShipment()->getShippingAddress();
         $idShipmentMethod = $this->fixtures->getShipmentMethodTransfer()->getIdShipmentMethod();
 
@@ -322,7 +294,7 @@ class CheckoutDataRestApiCest
                     'idCart' => $quoteTransfer->getUuid(),
                     'billingAddress' => $I->getAddressRequestPayload($quoteTransfer->getBillingAddress()),
                     'shippingAddress' => $I->getAddressRequestPayload($shippingAddressTransfer),
-                    'customer' => $I->getCustomerRequestPayload($this->fixtures->getCustomerTransfer()),
+                    'customer' => $I->getCustomerRequestPayload($this->fixtures->getGuestCustomerTransfer()),
                     'payments' => $I->getPaymentRequestPayload(),
                     'shipment' => $I->getShipmentRequestPayload($idShipmentMethod),
                 ],
