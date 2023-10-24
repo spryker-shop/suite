2f476d16d7eff4fd017e0d1e9fe9930a95119367
Dmitriy Aseev <32021063+dmiseev@users.noreply.github.com>
2023-10-24 12:41:26 +0300

    CC-31565: Fixed Click&Collect checkout flow with product bundles in the cart (#8327)
    Fixed Click&Collect checkout flow with product bundles in the cart

---
+++ src/Pyz/Yves/ServicePointWidget/ServicePointWidgetConfig.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Yves\ServicePointWidget;
+
+use Generated\Shared\Transfer\ItemTransfer;
+use SprykerShop\Yves\ServicePointWidget\ServicePointWidgetConfig as SprykerServicePointWidgetConfig;
+
+class ServicePointWidgetConfig extends SprykerServicePointWidgetConfig
+{
+    /**
+     * @return list<string>
+     */
+    public function getNotApplicableServicePointAddressStepFormItemPropertiesForHydration(): array
+    {
+        return [
+            ItemTransfer::BUNDLE_ITEM_IDENTIFIER,
+            ItemTransfer::RELATED_BUNDLE_ITEM_IDENTIFIER,
+        ];
+    }
+}
