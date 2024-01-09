5764a77898ef0c251986c52eac644464808e7e78
Artem Symynenko <artem.siminenko@gmail.com>
2024-01-09 11:37:26 +0100

    CC-26450 Added password validation during merchant email update. (#8559)
    CC-26450 Added password validation during merchant email update.

---
+++ src/Pyz/Zed/UserMerchantPortalGui/UserMerchantPortalGuiConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * This file is part of the Spryker Suite.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\UserMerchantPortalGui;
+
+use Spryker\Zed\UserMerchantPortalGui\UserMerchantPortalGuiConfig as SprykerUserMerchantPortalGuiConfig;
+
+class UserMerchantPortalGuiConfig extends SprykerUserMerchantPortalGuiConfig
+{
+    /**
+     * @var bool
+     */
+    protected const IS_EMAIL_UPDATE_PASSWORD_VERIFICATION_ENABLED = true;
+}
