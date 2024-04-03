bf41f55e7167f44045aa68d8f9ed704f8f9a7f6d
Oleksii Bilan <alexey.belan@spryker.com>
2024-04-03 16:31:22 +0300

    CC-33168 Update Angular to v17 (#8764)
    Update Angular to v17

--- frontend/merchant-portal/entry-points.js
+++ frontend/merchant-portal/entry-points.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const { readFileSync } = require('fs');
 const {
     ROOT_SPRYKER_CORE_DIR,
     MP_CORE_ENTRY_POINT_FILE,
@@ -6,26 +7,33 @@ const {
     MP_PROJECT_ENTRY_POINT_FILE,
 } = require('./mp-paths');
 const { getMPEntryPoints, entryPointPathToName } = require('./utils');
+const MP_SINGLE_ENTRY = 'spy/merchant-portal';
+const MP_SINGLE_ENTRY_MARKER = `${MP_SINGLE_ENTRY}:single-entry-marker`;
 
 async function getMPEntryPointsMap() {
+    const singleEntryNames = new Map();
+
     const entryPointsMap = async (dir, entryPath) => {
         const entryPoints = await getMPEntryPoints(dir, entryPath);
 
-        return entryPoints.reduce(
-            (acc, entryPoint) => ({
-                ...acc,
-                [entryPointPathToName('spy/', entryPoint)]: path.join(dir, entryPoint),
-            }),
-            {},
-        );
-    };
-    const coreEntryPoints = await entryPointsMap(ROOT_SPRYKER_CORE_DIR, MP_CORE_ENTRY_POINT_FILE);
-    const projectEntryPoints = await entryPointsMap(ROOT_SPRYKER_PROJECT_DIR, MP_PROJECT_ENTRY_POINT_FILE);
+        return entryPoints.reduce((acc, entryPoint) => {
+            const fullPath = path.join(dir, entryPoint);
+            const isSingleEntry = readFileSync(fullPath, { encoding: 'utf8' }).includes(MP_SINGLE_ENTRY_MARKER);
+            const name = entryPointPathToName('spy/', entryPoint);
 
-    return {
-        ...coreEntryPoints,
-        ...projectEntryPoints,
+            if (isSingleEntry || singleEntryNames.has(name)) {
+                singleEntryNames.set(name, fullPath);
+
+                return acc;
+            }
+
+            return { ...acc, [name]: fullPath };
+        }, {});
     };
+    const core = await entryPointsMap(ROOT_SPRYKER_CORE_DIR, MP_CORE_ENTRY_POINT_FILE);
+    const project = await entryPointsMap(ROOT_SPRYKER_PROJECT_DIR, MP_PROJECT_ENTRY_POINT_FILE);
+
+    return { ...core, ...project, [MP_SINGLE_ENTRY]: [...singleEntryNames.values()] };
 }
 
 module.exports = {
