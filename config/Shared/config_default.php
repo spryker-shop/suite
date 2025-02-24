e8a5dec8e66085c95a6a94fd8c5cdb57e0ccc26f
Dmitriy Krainiy <dmitriy.krainiy@spryker.com>
2025-02-24 10:27:41 +0100

    Added TreviPay App host to trusted hosts

--- config/Shared/config_default.php
+++ config/Shared/config_default.php
@@ -240,7 +240,8 @@ $config[KernelConstants::DOMAIN_WHITELIST] = array_merge($trustedHosts, [
     'threedssvc.pay1.de', // trusted Payone domain
     'www.sofort.com', // trusted Payone domain
     'connect.stripe.com', // trusted Stripe domain
-    'checkout.b2b.credit', // trusted Trevipay checkout domain
+    'checkout.b2b.credit', // trusted TreviPay checkout domain
+    'trevipay-staging.acp.demo-spryker.com',  // trusted TreviPay App domain
 ]);
 $config[KernelConstants::STRICT_DOMAIN_REDIRECT] = true;
 
