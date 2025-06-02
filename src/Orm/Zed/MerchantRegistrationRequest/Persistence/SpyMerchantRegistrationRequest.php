23cf6d928ac2c11efe4cef8e7d3a846f03a2cfe5
Denis Gnusov <denis.gnusov@spryker.com>
2025-06-02 15:50:47 +0300

    CC-35709: Merchant registration request form

---
+++ src/Orm/Zed/MerchantRegistrationRequest/Persistence/SpyMerchantRegistrationRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Orm\Zed\MerchantRegistrationRequest\Persistence;
+
+use Orm\Zed\MerchantRegistrationRequest\Persistence\Base\SpyMerchantRegistrationRequest as BaseSpyMerchantRegistrationRequest;
+
+/**
+ * Skeleton subclass for representing a row from the 'spy_merchant_registration_request' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class SpyMerchantRegistrationRequest extends BaseSpyMerchantRegistrationRequest
+{
+
+}
--- src/Pyz/Yves/Router/RouterDependencyProvider.php
+++ src/Pyz/Yves/Router/RouterDependencyProvider.php
@@ -12,6 +12,7 @@ namespace Pyz\Yves\Router;
 use Pyz\Yves\ExampleProductSalePage\Plugin\Router\ExampleProductSaleRouteProviderPlugin;
 use Spryker\Yves\CustomerDataChangeRequest\Plugin\Router\CustomerDataChangeRequestRouteProviderPlugin;
 use Spryker\Yves\HealthCheck\Plugin\Router\HealthCheckRouteProviderPlugin;
+use Spryker\Yves\MerchantRegistrationRequest\Plugin\Router\MerchantRegistrationRequestRouteProviderPlugin;
 use Spryker\Yves\MultiFactorAuth\Plugin\Router\Customer\MultiFactorAuthCustomerRouteProviderPlugin;
 use Spryker\Yves\Router\Plugin\RouteManipulator\LanguageDefaultPostAddRouteManipulatorPlugin;
 use Spryker\Yves\Router\Plugin\RouteManipulator\SslPostAddRouteManipulatorPlugin;
@@ -201,6 +202,7 @@ class RouterDependencyProvider extends SprykerRouterDependencyProvider
             new SspAssetRouteProviderPlugin(), #SspAssetManagementFeature
             new SspAssetManagementRouteProviderPlugin(), #SspAssetManagementFeature
             new SspServiceManagementPageRouteProviderPlugin(),
+            new MerchantRegistrationRequestRouteProviderPlugin(),
         ];
 
         if (class_exists(LoadTestingRouterProviderPlugin::class)) {
